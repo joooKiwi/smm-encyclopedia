8386d7674240bbe714869a7828e12d661f48d241
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-03-28 18:48:18 -0400

    Change from direct text to translation text template

--- src/navigation/Footer.tsx
+++ src/navigation/Footer.tsx
@@ -7,7 +7,7 @@ export default class Footer extends Component {
         return <footer id="footer_container" className="bg-dark pt-4 pb-3 mb-0">
             <div className="container">
                 <div className="row">
-                    <span id="copyright" className="text-sm-center text-light small">Copyright <sup>©</sup> Nintendo<sup>TM</sup></span>
+                    <span id="copyright" className="text-sm-center text-light small">{__('Copyright')} <sup>©</sup> Nintendo<sup>TM</sup></span>
                     <span id="copyright_madeBy" className="text-sm-center text-light small">{__('Web application made by')}: JóôòKiwi</span>
                     <span id="copyright_lastEdited" className="text-sm-center text-light small">{__('Last update')}: {Languages.currentLanguage.newDateInstanceCreator.createDate(28, 3, 2021)}</span>
                 </div>
