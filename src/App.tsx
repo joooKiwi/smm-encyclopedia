fd4b971a1ba108b0af0738125c71d6e42942fa3b
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-03-27 22:56:42 -0400

    Removal of the React template to change the default template.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
 import './App.scss';
 import React, {Component} from 'react';
-import logo from "./logo.svg";
-import GroupTextButton from "./buttons/GroupTextButton";
 import Navigation from "./navigation/Navigation";
 import Footer from "./navigation/Footer";
 
 export default class App
     extends Component {
 
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    <GroupTextButton elements={[{text: 'test', isActive: true,}]}/>
-                </header>
-            </div>
-        );
+    public render() {
+        return (<>
             <Navigation/>
+            <main className="pt-3 pb-5 align-bottom container-fluid">
+            </main>
             <Footer/>
+        </>)
     }
 
 }
