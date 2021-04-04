6cf92452dedb9278ba716d9f202c842d9bec3421
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-04-04 00:02:10 -0400

    Change from a direct to the proper trademark logo TM → ™

--- src/navigation/Footer.tsx
+++ src/navigation/Footer.tsx
@@ -8,7 +8,7 @@ export default class Footer
         return <footer id="footer_container" className="bg-dark pt-4 pb-3 mb-0">
             <div className="container">
                 <div className="row">
-                    <span id="copyright" className="text-center text-light small">Copyright <sup>©</sup> Nintendo<sup>TM</sup></span>
+                    <span id="copyright" className="text-center text-light small">Copyright <sup>©</sup> Nintendo™</span>
                     <span id="copyright_madeBy" className="text-center text-light small">{__('Web application made by')}: JóôòKiwi</span>
                     <span id="copyright_lastEdited" className="text-center text-light small">{__('Last update')}: {Languages.currentLanguage.newDateInstanceCreator.createDate(3, 4, 2021)}</span>
                 </div>
