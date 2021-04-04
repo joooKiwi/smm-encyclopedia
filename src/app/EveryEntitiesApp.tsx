4605dfa8b4455780b998339694d53ccbbfa3dd2e
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-04-04 15:02:26 -0400

    Change from a basic template to the content for every apps

---
+++ src/app/EveryEntitiesApp.tsx
@@ -0,0 +1,11 @@
+import AbstractApp from "./AbstractApp";
+import React from "react";
+
+export class EveryEntitiesApp
+    extends AbstractApp {
+
+    protected _mainContent(): JSX.Element {
+        return <>every entities</>;
+    }
+
+}
---
+++ src/app/EveryLimitsApp.tsx
@@ -0,0 +1,11 @@
+import AbstractApp from "./AbstractApp";
+import React from "react";
+
+export class EveryLimitsApp
+    extends AbstractApp {
+
+    protected _mainContent(): JSX.Element {
+        return <>every limits</>;
+    }
+
+}
