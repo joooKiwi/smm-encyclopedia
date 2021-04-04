2c3fc0e61b35029bec236afbdadc78eccc625053
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-04-03 23:01:12 -0400

    Test on using the routes with different languages

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,29 +1,38 @@
 import App from "../App";
 import {Languages} from "../lang/Languages";
-import {BrowserRouter, Redirect, Route, Switch, useParams} from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch, useLocation, useParams} from "react-router-dom";
 import React from "react";
 
 export default function Routes() {
     return <BrowserRouter>
         <Switch>
-            <Route path="/:lang">
-                {/*<Route path="/home"><LanguageRedirector/></Route>*/}
-                <LanguageRedirector/>
-                <Route path="/every/entity"/>
-                <Route path="/every/limit"/>
-            </Route>
-            <Route path="/"><LanguageRedirector/></Route>
+            <Route exact path="/"><Redirect to={`/${Languages.defaultLanguage}/home`}/></Route>
+            <Route path="/home"><Redirect to={`/${Languages.defaultLanguage}/home`}/></Route>
+            <Route path="/every/entity"><Redirect to={`/${Languages.defaultLanguage}/every/entity`}/></Route>
+            <Route path="/every/limit"><Redirect to={`/${Languages.defaultLanguage}/every/limit`}/></Route>
+            <Route path="/:lang"><DirectRoutes/></Route>
         </Switch>
     </BrowserRouter>;
 }
 
-function LanguageRedirector() {
+function DirectRoutes() {
     const params: { lang?: string } = useParams();
-    if ('lang' in params && params.lang != null) {
+    const location = useLocation();
+    if ('lang' in params && typeof params.lang === 'string') {
         Languages.setCurrentLanguage(params.lang);
-        let currentLanguage = Languages.getValue(params.lang);
-        if (currentLanguage != null)
-            return <App/>;
+        const currentLanguage = Languages.getValue(params.lang);
+        if (currentLanguage != null) {
+            switch (location.pathname) {
+                case '/' + currentLanguage.acronym + '/home':
+                    return <App/>;
+                case '/' + currentLanguage.acronym + '/every/entity':
+                    return <>every entities</>;
+                case '/' + currentLanguage.acronym + '/every/limit':
+                    return <>every limit</>;
+                default:
+                    return <Redirect to={`/${currentLanguage.acronym}/home`}/>;
+            }
+        }
     }
     return <Redirect to={`/${Languages.defaultLanguage}/home`}/>;
 }
