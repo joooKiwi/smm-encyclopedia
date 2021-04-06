2174a7a826e3897c871fa340982054558a741693
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-04-05 23:32:47 -0400

    Change == to === to avoid any warning

--- src/loader/converter/GenericStringToAnyNullableConverter.ts
+++ src/loader/converter/GenericStringToAnyNullableConverter.ts
@@ -4,13 +4,13 @@ export class GenericStringToAnyNullableConverter<T>
     extends GenericStringToAnyConverter<T | null> {
 
     protected _convertTheValue(validValue: string): T | null {
-        return validValue == ''
+        return validValue === ''
             ? null
             : super._convertTheValue(validValue);
     }
 
     public isValueValid(value: string): boolean {
-        return value == '' || super.isValueValid(value);
+        return value === '' || super.isValueValid(value);
     }
 
 }
