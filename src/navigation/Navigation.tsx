fa28b3cd0e5c05b15037d9c1e602c83dc1c9604e
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-04-11 15:00:20 -0400

    Addition of the tab for the entity group app

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -20,6 +20,7 @@ export default class Navigation
                             <span className="nav-link dropdown-toggle" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">{__('Display')}...</span>
                             <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
                                 <li className="dropdown-item"><Link key="navigationEveryEntities" className="nav-link active" to={`/${Languages.currentLanguage.acronym}/every/entity`}>{__('Display every entities')}</Link></li>
+                                <li className="dropdown-item"><Link key="navigationEveryLimit" className="nav-link active" to={`/${Languages.currentLanguage.acronym}/every/group`}>{__('Display every entity group')}</Link></li>
                                 <li className="dropdown-item"><Link key="navigationEveryLimit" className="nav-link active" to={`/${Languages.currentLanguage.acronym}/every/limit`}>{__('Display every limits')}</Link></li>
                             </ul>
                         </li>
