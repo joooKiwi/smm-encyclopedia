bdbb5279998a06d3695970935f1e314445e87afd
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-04-17 17:52:46 -0400

    Addition of a forgotten file for the dependency inversion on the ClassWithEveryLanguages.tsx file

---
+++ src/lang/ClassWithEveryLanguages.ts
@@ -0,0 +1,26 @@
+export interface ClassWithEveryLanguages {
+
+    japanese: string
+
+    americanEnglish: string
+    europeanEnglish: string
+
+    americanSpanish: string
+    europeanSpanish: string
+
+    canadianFrench: string
+    europeanFrench: string
+
+    dutch: string
+
+    german: string
+
+    italian: string
+
+    russian: string
+
+    korean: string
+
+    simplifiedChinese: string
+    traditionalChinese: string
+}
