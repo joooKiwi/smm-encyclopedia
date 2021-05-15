ec2d1d803ecbbd16950067fc24b90f1428187f00
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-05-15 17:29:45 -0400

    Revert of deleted files (from the removal of duplicate files)

---
+++ src/buttons/elements/ActivatableElement.tsx
@@ -0,0 +1,3 @@
+export interface ActivatableElement {
+    isActive: boolean
+}
---
+++ src/buttons/elements/ActivatableTextElement.tsx
@@ -0,0 +1,6 @@
+import {ActivatableElement} from "./ActivatableElement";
+
+export interface ActivatableTextElement
+    extends ActivatableElement {
+    text: string
+}
