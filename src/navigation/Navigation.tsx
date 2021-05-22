f95711c94673ac8eb0e6c3d84294d924e47af482
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-05-22 16:38:52 -0400

    Addition of the navigation translation. And removal of the template method.

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,10 +1,14 @@
-import React, {ReactNode} from 'react';
-import {Link}             from 'react-router-dom';
+import React            from 'react';
+import {Link}           from 'react-router-dom';
+import {useTranslation} from 'react-i18next';
 
-import {__, Languages}  from '../lang/Languages';
-import LanguageChanger  from './LanguageChanger';
+import {Languages}     from '../lang/Languages';
+import LanguageChanger from './LanguageChanger';
 
-export default function Navigation(): ReactNode {
+export default function Navigation(): JSX.Element {
+    const t_content = useTranslation('content').t;
+    const t_game = useTranslation('game').t;
+
     return (<nav className="navbar navbar-expand-md navbar-light bg-light">
         <div className="container-fluid">
             <Link key='navigationHome' className="navbar-brand" aria-current="page" to={`/${Languages.currentLanguage.acronym}/home`}>Home</Link>
@@ -14,13 +18,13 @@ export default function Navigation(): ReactNode {
             <div id="navbar-container" className="collapse navbar-collapse">
                 <ul id="left-navbar-container" className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item dropdown">
-                        <span className="nav-link dropdown-toggle" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">{__('Display')}...</span>
+                        <span className="nav-link dropdown-toggle" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">{t_content('Display')}...</span>
                         <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                            <li className="dropdown-item"><Link key="navigationEveryEntities" className="nav-link active" to={`/${Languages.currentLanguage.acronym}/every/entity`}>{__('Display every entities')}</Link></li>
-                            <li className="dropdown-item"><Link key="navigationEveryCategories" className="nav-link active" to={`/${Languages.currentLanguage.acronym}/every/category`}>{__('Display every entity categories')}</Link></li>
-                            <li className="dropdown-item"><Link key="navigationEveryGroups" className="nav-link active" to={`/${Languages.currentLanguage.acronym}/every/group`}>{__('Display every entity groups')}</Link></li>
-                            <li className="dropdown-item"><Link key="navigationEveryLimit" className="nav-link active" to={`/${Languages.currentLanguage.acronym}/every/limit`}>{__('Display every limits')}</Link></li>
-                            <li className="dropdown-item"><Link key="navigationEveryTheme" className="nav-link active" to={`/${Languages.currentLanguage.acronym}/every/theme`}>{__('Display every themes')}</Link></li>
+                            <li className="dropdown-item"><Link key="navigationEveryEntities" className="nav-link active" to={`/${Languages.currentLanguage.acronym}/every/entity`}>{t_game('Display every entities')}</Link></li>
+                            <li className="dropdown-item"><Link key="navigationEveryCategories" className="nav-link active" to={`/${Languages.currentLanguage.acronym}/every/category`}>{t_game('Display every entity categories')}</Link></li>
+                            <li className="dropdown-item"><Link key="navigationEveryGroups" className="nav-link active" to={`/${Languages.currentLanguage.acronym}/every/group`}>{t_game('Display every entity groups')}</Link></li>
+                            <li className="dropdown-item"><Link key="navigationEveryLimit" className="nav-link active" to={`/${Languages.currentLanguage.acronym}/every/limit`}>{t_game('Display every limits')}</Link></li>
+                            <li className="dropdown-item"><Link key="navigationEveryTheme" className="nav-link active" to={`/${Languages.currentLanguage.acronym}/every/theme`}>{t_game('Display every themes')}</Link></li>
                         </ul>
                     </li>
                 </ul>
