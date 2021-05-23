a0462c4923b3e096047c74baa4f882f794ab1a38
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-05-22 21:22:55 -0400

    Relocation of the method "getLanguageToSetTheList" to be inside the class and without the parameter since it is useless to have it.

--- src/entity/lang/SMM2NameComponent.tsx
+++ src/entity/lang/SMM2NameComponent.tsx
@@ -7,7 +7,7 @@ import {EveryLanguages}                             from '../../lang/EveryLangua
 import {Name}                                       from '../../lang/name/Name';
 
 interface SMM2NameProperty
-extends ContentAndLanguageTranslationElement{
+    extends ContentAndLanguageTranslationElement {
     popoverOrientation?: 'auto' | 'top' | 'bottom' | 'left' | 'right'
     id: string
     name: Name
@@ -28,9 +28,38 @@ class SMM2NameComponent
         return this.props.name;
     }
 
+    private get languagesToDisplay(): readonly EveryLanguages[] {
+        const returnedLanguages = [] as EveryLanguages[];
+
+        if (!EveryLanguages.ENGLISH.isCurrentLanguage)
+            returnedLanguages.push(...(typeof EveryLanguages.ENGLISH.original(this.name) === 'string' ? [EveryLanguages.ENGLISH] : [EveryLanguages.AMERICAN_ENGLISH, EveryLanguages.EUROPEAN_ENGLISH]));
+        if (!EveryLanguages.FRENCH.isCurrentLanguage)
+            returnedLanguages.push(...(typeof EveryLanguages.FRENCH.original(this.name) === 'string' ? [EveryLanguages.FRENCH] : [EveryLanguages.CANADIAN_FRENCH, EveryLanguages.EUROPEAN_FRENCH]));
+        if (!EveryLanguages.GERMAN.isCurrentLanguage)
+            returnedLanguages.push(EveryLanguages.GERMAN);
+        if (!EveryLanguages.SPANISH.isCurrentLanguage)
+            returnedLanguages.push(...(typeof EveryLanguages.SPANISH.original(this.name) === 'string' ? [EveryLanguages.SPANISH] : [EveryLanguages.AMERICAN_SPANISH, EveryLanguages.EUROPEAN_SPANISH]));
+        if (!EveryLanguages.ITALIAN.isCurrentLanguage)
+            returnedLanguages.push(EveryLanguages.ITALIAN);
+        if (!EveryLanguages.DUTCH.isCurrentLanguage)
+            returnedLanguages.push(EveryLanguages.DUTCH);
+        if (!EveryLanguages.PORTUGUESE.isCurrentLanguage)
+            returnedLanguages.push(...(typeof EveryLanguages.PORTUGUESE.original(this.name) === 'string' ? [EveryLanguages.PORTUGUESE] : [EveryLanguages.AMERICAN_PORTUGUESE, EveryLanguages.EUROPEAN_PORTUGUESE]));
+        if (!EveryLanguages.RUSSIAN.isCurrentLanguage)
+            returnedLanguages.push(EveryLanguages.RUSSIAN);
+        if (!EveryLanguages.JAPANESE.isCurrentLanguage)
+            returnedLanguages.push(EveryLanguages.JAPANESE);
+        if (!EveryLanguages.CHINESE.isCurrentLanguage)
+            returnedLanguages.push(...(typeof EveryLanguages.CHINESE.original(this.name) === 'string' ? [EveryLanguages.CHINESE] : [EveryLanguages.CHINESE_SIMPLIFIED, EveryLanguages.CHINESE_TRADITIONAL]));
+        if (!EveryLanguages.KOREAN.isCurrentLanguage)
+            returnedLanguages.push(EveryLanguages.KOREAN);
+
+        return returnedLanguages;
+    }
+
     public render(): JSX.Element {
         const id = this.id + '_' + this.name.english.toLowerCase().replace(' ', '_');
-        const languagesToDisplay = getLanguagesToSetTheList(this.name);
+        const languagesToDisplay = this.languagesToDisplay;
         let content = '<ol>';
         this.name.toNameMap().forEach((value, language) => {
             if (languagesToDisplay.includes(language)) {
@@ -50,35 +79,6 @@ class SMM2NameComponent
         return <span id={id} data-bs-toggle="popover">{EveryLanguages.currentLanguage.get(this.name)}</span>;
     }
 
-}
-
-function getLanguagesToSetTheList(name: Name): readonly EveryLanguages[] {
-    const returnedLanguages = [] as EveryLanguages[];
-
-    if (!EveryLanguages.ENGLISH.isCurrentLanguage)
-        returnedLanguages.push(...(typeof EveryLanguages.ENGLISH.original(name) === 'string' ? [EveryLanguages.ENGLISH] : [EveryLanguages.AMERICAN_ENGLISH, EveryLanguages.EUROPEAN_ENGLISH]));
-    if (!EveryLanguages.FRENCH.isCurrentLanguage)
-        returnedLanguages.push(...(typeof EveryLanguages.FRENCH.original(name) === 'string' ? [EveryLanguages.FRENCH] : [EveryLanguages.CANADIAN_FRENCH, EveryLanguages.EUROPEAN_FRENCH]));
-    if (!EveryLanguages.GERMAN.isCurrentLanguage)
-        returnedLanguages.push(EveryLanguages.GERMAN);
-    if (!EveryLanguages.SPANISH.isCurrentLanguage)
-        returnedLanguages.push(...(typeof EveryLanguages.SPANISH.original(name) === 'string' ? [EveryLanguages.SPANISH] : [EveryLanguages.AMERICAN_SPANISH, EveryLanguages.EUROPEAN_SPANISH]));
-    if (!EveryLanguages.ITALIAN.isCurrentLanguage)
-        returnedLanguages.push(EveryLanguages.ITALIAN);
-    if (!EveryLanguages.DUTCH.isCurrentLanguage)
-        returnedLanguages.push(EveryLanguages.DUTCH);
-    if (!EveryLanguages.PORTUGUESE.isCurrentLanguage)
-        returnedLanguages.push(...(typeof EveryLanguages.PORTUGUESE.original(name) === 'string' ? [EveryLanguages.PORTUGUESE] : [EveryLanguages.AMERICAN_PORTUGUESE, EveryLanguages.EUROPEAN_PORTUGUESE]));
-    if (!EveryLanguages.RUSSIAN.isCurrentLanguage)
-        returnedLanguages.push(EveryLanguages.RUSSIAN);
-    if (!EveryLanguages.JAPANESE.isCurrentLanguage)
-        returnedLanguages.push(EveryLanguages.JAPANESE);
-    if (!EveryLanguages.CHINESE.isCurrentLanguage)
-        returnedLanguages.push(...(typeof EveryLanguages.CHINESE.original(name) === 'string' ? [EveryLanguages.CHINESE] : [EveryLanguages.CHINESE_SIMPLIFIED, EveryLanguages.CHINESE_TRADITIONAL]));
-    if (!EveryLanguages.KOREAN.isCurrentLanguage)
-        returnedLanguages.push(EveryLanguages.KOREAN);
-
-    return returnedLanguages;
 }
 
 export default withTranslation(['content', 'language',])(SMM2NameComponent);
