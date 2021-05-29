133002a2b626c96e77aa2f7a57bb1d56edf1ca87
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-05-29 16:08:15 -0400

    Fix on the possible values to include more every possibilities. And simplification of the anme to only include the acronym of the game title.

--- src/entity/properties/IsInGameStylePropertyContainer.ts
+++ src/entity/properties/IsInGameStylePropertyContainer.ts
@@ -10,18 +10,22 @@ import {GameStyles}                    from '../gameStyle/GameStyles';
 export class IsInGameStylePropertyContainer
     implements IsInGameStyleProperty {
 
-    private static readonly __IS_IN_EXCLUSIVE_TO_SUPER_MARIO_BROS_IN_SUPER_MARIO_MAKER_1_PROPERTY = new IsInGameStylePropertyContainer(true, false, false, false, null,) as IsInOnlySMM1GameStyleProperty;
-    private static readonly __IS_IN_EXCLUSIVE_TO_SUPER_MARIO_BROS_IN_SUPER_MARIO_MAKER_2_PROPERTY = new IsInGameStylePropertyContainer(true, false, false, false, false,) as IsInOnlySMM2GameStyleProperty;
-    private static readonly __IS_IN_EXCLUSIVE_TO_SUPER_MARIO_BROS_3_PROPERTY = new IsInGameStylePropertyContainer(false, true, false, false, false,) as IsInOnlySMM2GameStyleProperty;
-    private static readonly __IS_IN_EXCLUSIVE_TO_SUPER_MARIO_WORLD_PROPERTY = new IsInGameStylePropertyContainer(false, false, true, false, false,) as IsInOnlySMM2GameStyleProperty;
-    private static readonly __IS_IN_EXCLUSIVE_TO_SUPER_MARIO_BROS_AND_SUPER_MARIO_BROS_3_AND_NEW_SUPER_MARIO_BROS_U = new IsInGameStylePropertyContainer(true, true, true, false, false,) as IsInOnlySMM2GameStyleProperty;
-    private static readonly __IS_IN_EXCLUSIVE_TO_NEW_SUPER_MARIO_BROS_U_PROPERTY = new IsInGameStylePropertyContainer(false, false, false, true, false,) as IsInOnlySMM2GameStyleProperty;
-    private static readonly __IS_IN_EXCLUSIVE_TO_SUPER_MARIO_3D_WORLD_PROPERTY = new IsInGameStylePropertyContainer(false, false, false, false, true,) as IsInOnlySMM2GameStyleProperty;
-    private static readonly __IS_IN_EXCLUSIVE_TO_SUPER_MARIO_BROS_AND_SUPER_MARIO_BROS_3_PROPERTY = new IsInGameStylePropertyContainer(true, true, false, false, false,) as IsInOnlySMM2GameStyleProperty;
-    private static readonly __IS_IN_EXCLUSIVE_TO_SUPER_MARIO_WORLD_AND_NEW_SUPER_MARIO_BROS_U_PROPERTY = new IsInGameStylePropertyContainer(false, false, true, true, false,) as IsInOnlySMM2GameStyleProperty;
-    private static readonly __IS_IN_ORIGINAL_4_STYLES_PROPERTY = new IsInGameStylePropertyContainer(true, true, true, true, false,) as IsInOnlySMM2GameStyleProperty;
-    private static readonly __IS_IN_EVERY_GAME_STYLES_PROPERTY = new IsInGameStylePropertyContainer(true, true, true, true, true,) as IsInOnlySMM2GameStyleProperty;
-    private static readonly __IS_IN_NO_GAME_STYLES_PROPERTY = new IsInGameStylePropertyContainer(false, false, false, false, null,);
+    private static readonly __IS_IN_EXCLUSIVE_TO_SMB_IN_SMM1_PROPERTY =         new IsInGameStylePropertyContainer(true,  false, false, false, null, ) as IsInOnlySMM1GameStyleProperty;
+    private static readonly __IS_IN_EXCLUSIVE_TO_SMB_IN_SMM2_PROPERTY =         new IsInGameStylePropertyContainer(true,  false, false, false, false,) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_EXCLUSIVE_TO_SMB3_PROPERTY =                new IsInGameStylePropertyContainer(false, true,  false, false, false,) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_EXCLUSIVE_TO_SMW_PROPERTY =                 new IsInGameStylePropertyContainer(false, false, true,  false, false,) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_EXCLUSIVE_TO_SMB_AND_SMB3_AND_SMW =         new IsInGameStylePropertyContainer(true,  true,  true,  false, false,) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_EXCLUSIVE_TO_SMB_AND_SMB3_AND_NSMBU =       new IsInGameStylePropertyContainer(true,  true,  false, true,  false,) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_EXCLUSIVE_TO_SMB_AND_SMB3_NSMBU_AND_SM3DW = new IsInGameStylePropertyContainer(true,  true,  true,  false, true, ) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_EXCLUSIVE_TO_NSMBU_PROPERTY =               new IsInGameStylePropertyContainer(false, false, false, true,  false,) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_EXCLUSIVE_TO_SM3DW_PROPERTY =               new IsInGameStylePropertyContainer(false, false, false, false, true, ) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_EXCLUSIVE_TO_SMB_AND_SMB3_PROPERTY =        new IsInGameStylePropertyContainer(true,  true,  false, false, false,) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_EXCLUSIVE_TO_SMW_AND_NSMBU_PROPERTY =       new IsInGameStylePropertyContainer(false, false, true,  true,  false,) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_EXCLUSIVE_TO_SMW_AND_SM3DW_PROPERTY =       new IsInGameStylePropertyContainer(false, false, true,  false, true, ) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_EXCLUSIVE_TO_SMW_NSMBU_AND_SM3DW_PROPERTY = new IsInGameStylePropertyContainer(false, false, true,  true,  true, ) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_ORIGINAL_4_STYLES_PROPERTY =                new IsInGameStylePropertyContainer(true,  true,  true,  true,  false,) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_EVERY_GAME_STYLES_PROPERTY =                new IsInGameStylePropertyContainer(true,  true,  true,  true,  true, ) as IsInOnlySMM2GameStyleProperty;
+    private static readonly __IS_IN_NO_GAME_STYLES_PROPERTY =                   new IsInGameStylePropertyContainer(false, false, false, false, null, );
 
     readonly #isInSuperMarioBrosStyle;
     readonly #isInSuperMarioBros3Style;
@@ -83,12 +87,15 @@ export class IsInGameStylePropertyContainer
      *     <tr><td>false true  false false & false</td><td>A property only in {@link GameStyles.SUPER_MARIO_BROS_3}</td></tr>
      *     <tr><td>false false true  false & false</td><td>A property only in {@link GameStyles.SUPER_MARIO_WORLD}</td></tr>
      *     <tr><td>false false false true  & false</td><td>A property only in {@link GameStyles.NEW_SUPER_MARIO_BROS_U}</td></tr>
-     *     <tr><td>false false false false & true</td><td>A property only in {@link GameStyles.SUPER_MARIO_3D_WORLD}</td></tr>
+     *     <tr><td>false false false false & true</td> <td>A property only in {@link GameStyles.SUPER_MARIO_3D_WORLD}</td></tr>
      *     <tr><td>true  true  true  true  & false</td><td>A property in the original 4 styles ({@link GameStyles.SUPER_MARIO_BROS}, {@link GameStyles.SUPER_MARIO_BROS_3}, {@link GameStyles.SUPER_MARIO_WORLD} & {@link GameStyles.NEW_SUPER_MARIO_BROS_U})</td></tr>
      *     <tr><td>true  true  false false & false</td><td>A property only in {@link GameStyles.SUPER_MARIO_BROS} & {@link GameStyles.SUPER_MARIO_BROS_3}</td></tr>
      *     <tr><td>false false true  true  & false</td><td>A property only in {@link GameStyles.SUPER_MARIO_WORLD} & {@link GameStyles.NEW_SUPER_MARIO_BROS_U}</td></tr>
+     *     <tr><td>false false true  false & true</td> <td>A property only in {@link GameStyles.SUPER_MARIO_WORLD} & {@link GameStyles.SUPER_MARIO_3D_WORLD}</td></tr>
      *     <tr><td>true  true  false true  & false</td><td>A property only in {@link GameStyles.SUPER_MARIO_BROS}, {@link GameStyles.SUPER_MARIO_BROS_3} & {@link GameStyles.NEW_SUPER_MARIO_BROS_U}</td></tr>
-     *     <tr><td>false false false false & null</td><td>A property no game styles. It also catch every other cases, but it should not happen.</td></tr>
+     *     <tr><td>true  true  true  false & false</td><td>A property only in {@link GameStyles.SUPER_MARIO_BROS}, {@link GameStyles.SUPER_MARIO_BROS_3} & {@link GameStyles.SUPER_MARIO_WORLD}</td></tr>
+     *     <tr><td>true  true  false true  & true</td> <td>A property only in {@link GameStyles.SUPER_MARIO_BROS}, {@link GameStyles.SUPER_MARIO_BROS_3}, {@link GameStyles.NEW_SUPER_MARIO_BROS_U} & {@link GameStyles.SUPER_MARIO_3D_WORLD}</td></tr>
+     *     <tr><td>false false false false & null</td> <td>A property no game styles. It also catch every other cases, but it should not happen.</td></tr>
      * </table>
      *
      * @param isInSuperMarioBrosStyle
@@ -105,29 +112,39 @@ export class IsInGameStylePropertyContainer
         }
         //region ----- Exclusive for 1 game style -----
         if (isInSuperMarioBrosStyle && !isInSuperMarioBros3Style && !isInSuperMarioWorldStyle && !isInNewSuperMarioBrosUStyle) {
-            if (isInSuperMario3DWorldStyle == null)
-                return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SUPER_MARIO_BROS_IN_SUPER_MARIO_MAKER_1_PROPERTY;
-            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SUPER_MARIO_BROS_IN_SUPER_MARIO_MAKER_2_PROPERTY;
+            if (isInSuperMario3DWorldStyle === null)
+                return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SMB_IN_SMM1_PROPERTY;
+            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SMB_IN_SMM2_PROPERTY;
         }
-        if (!isInSuperMarioBrosStyle && isInSuperMarioBros3Style && !isInSuperMarioWorldStyle && !isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle === false)
-            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SUPER_MARIO_BROS_3_PROPERTY;
-        if (!isInSuperMarioBrosStyle && !isInSuperMarioBros3Style && isInSuperMarioWorldStyle && !isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle === false)
-            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SUPER_MARIO_WORLD_PROPERTY;
-        if (!isInSuperMarioBrosStyle && !isInSuperMarioBros3Style && !isInSuperMarioWorldStyle && isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle === false)
-            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_NEW_SUPER_MARIO_BROS_U_PROPERTY;
+        if (!isInSuperMarioBrosStyle && isInSuperMarioBros3Style && !isInSuperMarioWorldStyle && !isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle !== true)
+            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SMB3_PROPERTY;
+        if (!isInSuperMarioBrosStyle && !isInSuperMarioBros3Style && isInSuperMarioWorldStyle && !isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle !== true)
+            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SMW_PROPERTY;
+        if (!isInSuperMarioBrosStyle && !isInSuperMarioBros3Style && !isInSuperMarioWorldStyle && isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle !== true)
+            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_NSMBU_PROPERTY;
         if (!isInSuperMarioBrosStyle && !isInSuperMarioBros3Style && !isInSuperMarioWorldStyle && !isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle === true)
-            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SUPER_MARIO_3D_WORLD_PROPERTY;
+            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SM3DW_PROPERTY;
         //endregion ----- Exclusive for 1 game style -----
         //region ----- Exclusive for 2 game styles -----
-        if (isInSuperMarioBrosStyle && isInSuperMarioBros3Style && !isInSuperMarioWorldStyle && !isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle === false)
-            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SUPER_MARIO_BROS_AND_SUPER_MARIO_BROS_3_PROPERTY;
-        if (!isInSuperMarioBrosStyle && !isInSuperMarioBros3Style && isInSuperMarioWorldStyle && isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle === false)
-            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SUPER_MARIO_WORLD_AND_NEW_SUPER_MARIO_BROS_U_PROPERTY;
+        if (isInSuperMarioBrosStyle && isInSuperMarioBros3Style && !isInSuperMarioWorldStyle && !isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle !== true)
+            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SMB_AND_SMB3_PROPERTY;
+        if (!isInSuperMarioBrosStyle && !isInSuperMarioBros3Style && isInSuperMarioWorldStyle && isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle !== true)
+            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SMW_AND_NSMBU_PROPERTY;
+        if (!isInSuperMarioBrosStyle && !isInSuperMarioBros3Style && isInSuperMarioWorldStyle && !isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle === true)
+            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SMW_AND_SM3DW_PROPERTY;
         //endregion ----- Exclusive for 2 game styles -----
         //region ----- Exclusive for 3 game styles -----
-        if (isInSuperMarioBrosStyle && isInSuperMarioBros3Style && !isInSuperMarioWorldStyle && isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle === false)
-            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SUPER_MARIO_BROS_AND_SUPER_MARIO_BROS_3_AND_NEW_SUPER_MARIO_BROS_U;
+        if (isInSuperMarioBrosStyle && isInSuperMarioBros3Style && isInSuperMario3DWorldStyle !== true) {
+            if (isInSuperMarioWorldStyle && !isInNewSuperMarioBrosUStyle)
+                return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SMB_AND_SMB3_AND_SMW;
+            if (!isInSuperMarioWorldStyle && isInNewSuperMarioBrosUStyle)
+                return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SMB_AND_SMB3_AND_NSMBU;
+        }
         //endregion ----- Exclusive for 3 game styles -----
+        //region ----- Exclusive for 4 game styles -----
+        if (isInSuperMarioBrosStyle && isInSuperMarioBros3Style && !isInSuperMarioWorldStyle && isInNewSuperMarioBrosUStyle && isInSuperMario3DWorldStyle === true)
+            return IsInGameStylePropertyContainer.__IS_IN_EXCLUSIVE_TO_SMB_AND_SMB3_NSMBU_AND_SM3DW;
+        //endregion ----- Exclusive for 4 game styles -----
 
         return IsInGameStylePropertyContainer.__IS_IN_NO_GAME_STYLES_PROPERTY;
     }
