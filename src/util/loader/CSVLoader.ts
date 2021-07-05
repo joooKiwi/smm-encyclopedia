1c46abf5aa23c02fa2ce4c4ae17a3778a422e575
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-07-04 22:16:53 -0400

    Addition of documentation to most of the public methods on the CSVLoader.ts and stronger type on the the arrays and map returned by the loader (may break at some places where it use it).

--- src/util/loader/CSVLoader.ts
+++ src/util/loader/CSVLoader.ts
@@ -1,11 +1,16 @@
-import {Converter}                                                      from "./converter/Converter";
-import {GenericStringToAnyConverter}                                    from "./converter/GenericStringToAnyConverter";
-import {GenericStringToAnyNullableConverter}                            from "./converter/GenericStringToAnyNullableConverter";
-import {PredefinedConversion, PredefinedConverter, PrimitiveConversion} from "./converter/PredefinedConverter";
+import {Converter}                                                      from './converter/Converter';
+import {GenericStringToAnyConverter}                                    from './converter/GenericStringToAnyConverter';
+import {GenericStringToAnyNullableConverter}                            from './converter/GenericStringToAnyNullableConverter';
+import {PredefinedConversion, PredefinedConverter, PrimitiveConversion} from './converter/PredefinedConverter';
 
-type HeaderTypeOrConvertor = PredefinedConversion
+//region -------------------- CVSLoader types --------------------
+
+export type CustomConversionCallback = (value: string) => Converter<string, any>;
+export type HeaderTypeOrConvertor = PredefinedConversion
     | readonly (PredefinedConversion | string)[]
-    | ((value: string) => Converter<string, any>);
+    | CustomConversionCallback;
+
+//endregion -------------------- CVSLoader types --------------------
 
 export class CSVLoader<T extends any[], U> {
 
@@ -19,14 +24,14 @@ export class CSVLoader<T extends any[], U> {
     readonly #content;
 
     readonly #callbackToCreateObject;
-    #callbackOnFinalObjectCreated?: (finalContent: U, convertedContent: T, originalContent: string[],) => void;
+    #callbackOnFinalObjectCreated?: (finalContent: U, convertedContent: T, originalContent: readonly string[],) => void;
     #callbackOnSingleContentConverted?: (content: keyof T, originalValue: string) => void;
-    #callbackOnInitialisationEnd?: (finalContents: U[], convertedContents: T[], originalContents: string[][],) => void;
+    #callbackOnInitialisationEnd?: (finalContents: readonly U[], convertedContents: readonly T[], originalContents: readonly string[][],) => void;
     #defaultConversion?: PredefinedConversion;
 
     //endregion -------------------- Attributes --------------------
 
-    public constructor(originalContent: string[][], callbackToCreateObject: (convertedContent: T) => U) {
+    public constructor(originalContent: string[][], callbackToCreateObject: (convertedContent: T) => U,) {
         this.#originalContent = originalContent;
         this.#headersToConvert = new Map<string, HeaderTypeOrConvertor>();
         this.#convertedContent = [] as T[];
@@ -34,30 +39,54 @@ export class CSVLoader<T extends any[], U> {
         this.#callbackToCreateObject = callbackToCreateObject;
     }
 
-
     //region -------------------- getter and setter and direct use on private members methods --------------------
 
-    public get originalContent() {
+    /**
+     * The original content received via the constructor.
+     */
+    public get originalContent(): string[][] {
         return this.#originalContent;
     }
 
-    public get convertedContent() {
+    /**
+     * The converted content with the sames structure as the {@link originalContent},
+     * but converted based on the {@link headersToConvert} on each associated <u>header index</u>.
+     */
+    public get convertedContent(): readonly T[] {
         return this.#convertedContent;
     }
 
-    public get headersToConvert() {
+    /**
+     * A map of every headers to convert.
+     * It exclude the other titles where they use the {@link GENERIC_DEFAULT_CONVERSION} instead.
+     */
+    public get headersToConvert(): ReadonlyMap<string, HeaderTypeOrConvertor> {
         return this.#headersToConvert;
     }
 
+    protected get _headersToConvert(): Map<string, HeaderTypeOrConvertor> {
+        return this.#headersToConvert;
+    }
+
+    /**
+     * Load the content only once, and then do nothing.
+     *
+     * @see _initialiseContent
+     */
     public load(): this {
         if (this.#content.length === 0)
             this._initialiseContent();
         return this;
     }
 
-    public get content() {
-        this.load();
-        return this.#content;
+    /**
+     * Get the content stored inside this {@link CSVLoader loader}.
+     * It use the {@link load} method to retrieve the data only once.
+     *
+     * @see load
+     */
+    public get content(): readonly U[] {
+        return this.load().#content;
     }
 
     protected addContent(originalContent: string[], arrayContent: T): this {
@@ -74,7 +103,25 @@ export class CSVLoader<T extends any[], U> {
         return this.#callbackOnFinalObjectCreated;
     }
 
-    public onFinalObjectCreated(callback: (finalContent: U, convertedContent: T, originalContent: string[],) => void): this {
+    /**
+     * <p>
+     *  A trigger called before inserting the content
+     *  and after the {@link callbackToCreateObject callback received in the construction}
+     *  on the {@link addContent addition of content} in the initialisation process.
+     * </p>
+     *
+     * <p>
+     *  It receive as a parameter:
+     *  <ol>
+     *    <li>The final content</li>
+     *    <li>The content converted</li>
+     *    <li>The original content</li>
+     *  </ol>
+     * </p>
+     *
+     * @param callback the callback to set
+     */
+    public onFinalObjectCreated(callback: (finalContent: U, convertedContent: T, originalContent: readonly string[],) => void): this {
         this.#callbackOnFinalObjectCreated = callback;
         return this;
     }
@@ -83,6 +130,22 @@ export class CSVLoader<T extends any[], U> {
         return this.#callbackOnSingleContentConverted;
     }
 
+    /**
+     * <p>
+     *  A trigger called after each individual conversion
+     *  on the {@link _initialiseContent initialisation method}.
+     * </p>
+     *
+     * <p>
+     *  It receive as a parameter:
+     *  <ol>
+     *    <li>A single converted content</li>
+     *    <li>A single original content</li>
+     *  </ol>
+     * </p>
+     *
+     * @param callback the callback to set
+     */
     public onSingleContentConverted(callback: (content: keyof T, originalValue: string) => void): this {
         this.#callbackOnSingleContentConverted = callback;
         return this;
@@ -92,7 +155,24 @@ export class CSVLoader<T extends any[], U> {
         return this.#callbackOnInitialisationEnd;
     }
 
-    public onInitialisationEnd(callback: (finalContents: U[], convertedContents: T[], originalContents: string[][],) => void): this {
+    /**
+     * <p>
+     *   A trigger called at the end of
+     *   the {@link _initialiseContent initialisation method}.
+     * </p>
+     *
+     * <p>
+     *  It receive as a parameter:
+     *  <ol>
+     *    <li>The final array of content</li>
+     *    <li>The array of converted content</li>
+     *    <li>The original content</li>
+     *  </ol>
+     * </p>
+     *
+     * @param callback the callback to set
+     */
+    public onInitialisationEnd(callback: (finalContents: readonly U[], convertedContents: readonly T[], originalContents: readonly string[][],) => void): this {
         this.#callbackOnInitialisationEnd = callback;
         return this;
     }
@@ -102,7 +182,6 @@ export class CSVLoader<T extends any[], U> {
     protected get callbackToCreateObject() {
         return this.#callbackToCreateObject;
     }
-
 
     //region -------------------- default values methods --------------------
 
@@ -114,6 +193,14 @@ export class CSVLoader<T extends any[], U> {
         this.#defaultConversion = value;
     }
 
+    /**
+     * Set the default {@link PredefinedConversion} on the current {@link CSVLoader loader instance}.
+     * If the current default conversion is not defined,
+     * then the {@link GENERIC_DEFAULT_CONVERSION generic default conversion} is used.
+     *
+     * @param defaultConversion
+     * @see GENERIC_DEFAULT_CONVERSION
+     */
     public setDefaultConversion(defaultConversion: PredefinedConversion): this {
         this.defaultConversion = defaultConversion;
         return this;
@@ -152,51 +239,84 @@ export class CSVLoader<T extends any[], U> {
         return this.convertTo('nullable string', ...headers);
     }
 
-    public convertToBooleanAnd(convertor: PredefinedConversion, ...headers: string[]): this
-    public convertToBooleanAnd(validValue: string, ...headers: string[]): this
-    public convertToBooleanAnd(validValueOrConvertor: string | PredefinedConversion, ...headers: string[]): this {
+
+    public convertToBooleanAnd(validValueOrConvertor: string | Exclude<PredefinedConversion, 'boolean'>, ...headers: string[]): this {
         return this.convertTo(['boolean', validValueOrConvertor,], ...headers);
     }
 
-    public convertToNullableBooleanAnd(convertor: PrimitiveConversion, ...headers: string[]): this
-    public convertToNullableBooleanAnd(validValue: string, ...headers: string[]): this
-    public convertToNullableBooleanAnd(validValueOrConvertor: string | PrimitiveConversion, ...headers: string[]): this {
+    public convertToNullableBooleanAnd(validValueOrConvertor: string | Exclude<PrimitiveConversion, 'boolean'>, ...headers: string[]): this {
         return this.convertTo(['nullable boolean', validValueOrConvertor,], ...headers);
     }
 
-
-    public convertToNumberAnd(convertor: PredefinedConversion, ...headers: string[]): this
-    public convertToNumberAnd(validValue: string, ...headers: string[]): this
-    public convertToNumberAnd(validValueOrConvertor: string | PredefinedConversion, ...headers: string[]): this {
+    public convertToNumberAnd(validValueOrConvertor: string | Exclude<PredefinedConversion, 'number'>, ...headers: string[]): this {
         return this.convertTo(['number', validValueOrConvertor,], ...headers);
     }
 
-    public convertToNullableNumberAnd(convertor: PrimitiveConversion, ...headers: string[]): this
-    public convertToNullableNumberAnd(validValue: string, ...headers: string[]): this
-    public convertToNullableNumberAnd(validValueOrConvertor: string | PrimitiveConversion, ...headers: string[]): this {
+    public convertToNullableNumberAnd(validValueOrConvertor: string | Exclude<PrimitiveConversion, 'number'>, ...headers: string[]): this {
         return this.convertTo(['nullable number', validValueOrConvertor,], ...headers);
     }
 
-    public convertToStringAnd(convertor: PredefinedConversion, ...headers: string[]): this
-    public convertToStringAnd(validValue: string, ...headers: string[]): this
-    public convertToStringAnd(validValueOrConvertor: string | PredefinedConversion, ...headers: string[]): this {
+    public convertToStringAnd(validValueOrConvertor: string | Exclude<PredefinedConversion, 'string'>, ...headers: string[]): this {
         return this.convertTo(['string', validValueOrConvertor,], ...headers);
     }
 
-    public convertToEmptyableStringAnd(convertor: PrimitiveConversion, ...headers: string[]): this
-    public convertToEmptyableStringAnd(validValue: string, ...headers: string[]): this
-    public convertToEmptyableStringAnd(validValueOrConvertor: string | PrimitiveConversion, ...headers: string[]): this {
+    /**
+     * <p>
+     *  Convert the headers received into the valid values (any {@link String}),
+     *  the specific value received
+     *  or from a predefined conversion ({@link PrimitiveConversion} excluding the 'string' since it is already referenced).
+     * </p>
+     *
+     * <p>
+     *  It can either be an empty string (''), <u>not</u> the <u><b>null</b></u> or <u><b>undefined</b></u> values,
+     *  any possible predefined value (if a 'boolean' or 'number').
+     *  It also validate if the value can be at least it is the value received (if it's any string).
+     * </p>
+     *
+     * @param validValueOrConvertor
+     * @param headers
+     * @see StringToEmptyableStringConverter
+     * @see ConverterPatterns.EMPTYABLE_STRING_PATTERN
+     */
+    public convertToEmptyableStringAnd(validValueOrConvertor: string | Exclude<PrimitiveConversion, 'string'>, ...headers: string[]): this {
         return this.convertTo(['emptyable string', validValueOrConvertor,], ...headers);
     }
 
-    public convertToNullableStringAnd(convertor: PrimitiveConversion, ...headers: string[]): this
-    public convertToNullableStringAnd(validValue: string, ...headers: string[]): this
-    public convertToNullableStringAnd(validValueOrConvertor: string | PrimitiveConversion, ...headers: string[]): this {
+    /**
+     * <p>
+     *  Convert the headers received into the valid values (any {@link String}),
+     *  the specific value received
+     *  or from a predefined conversion ({@link PrimitiveConversion} excluding the 'string' since it is already referenced).
+     * </p>
+     *
+     * <p>
+     *  It can either be a <u><b>null</b></u> or <u><b>undefined</b></u> ignoring the case or
+     *  any possible predefined value (if a 'boolean' or 'number').
+     *  It also validate if the value can be at least it is the value received (if it's any string).
+     * </p>
+     *
+     * @param validValueOrConvertor
+     * @param headers
+     * @see StringToNullableStringConverter
+     * @see ConverterPatterns.NULLABLE_STRING_PATTERN
+     */
+    public convertToNullableStringAnd(validValueOrConvertor: string | Exclude<PrimitiveConversion, 'string'>, ...headers: string[]): this {
         return this.convertTo(['nullable string', validValueOrConvertor,], ...headers);
     }
 
+    /**
+     * Add a conversion on each headers received with <u>only one</u> of:
+     * <ul>
+     *     <li>a predefined conversion type ({@link PredefinedConversion})</li>
+     *     <li>a custom convertor ({@link CustomConversionCallback})</li>
+     *     <li>an {@link Array array} of any string and / or the {@link PredefinedConversion predefined conversion type}</li>
+     * </ul>
+     *
+     * @param headerTypeOrConvertor <u>the predefined conversion</u>, <u>custom convertor</u> or <u>the array of string and / or predefined conversion</u>
+     * @param headers the headers to have the validation
+     */
     public convertTo(headerTypeOrConvertor: HeaderTypeOrConvertor, ...headers: string[]): this {
-        headers.map(header => header.toLowerCase()).forEach(header => this.headersToConvert.set(header, headerTypeOrConvertor));
+        headers.map(header => header.toLowerCase()).forEach(header => this._headersToConvert.set(header, headerTypeOrConvertor));
         return this;
     }
 
@@ -211,7 +331,7 @@ export class CSVLoader<T extends any[], U> {
                 const convertedValue = headers[index].convertor(content).convertedValue as keyof T;
                 this.callbackOnSingleContentConverted?.(convertedValue, content);
                 return convertedValue;
-            }) as T)
+            }) as T);
         }
         this.callbackOnInitialisationEnd?.(this.content, this.convertedContent, this.originalContent);
         return this;
@@ -229,7 +349,7 @@ export class CSVLoader<T extends any[], U> {
     protected _createAndGetConvertor(header: string, predefinedConversion: PredefinedConversion): (value: string) => Converter<string, any> {
         let convertor: (value: string) => Converter<string, any> =
             value => PredefinedConverter.getValue(predefinedConversion).newStringToConvertor(value);
-        this.headersToConvert.set(header, convertor);
+        this._headersToConvert.set(header, convertor);
         return convertor;
     }
 
@@ -265,7 +385,7 @@ export class CSVLoader<T extends any[], U> {
                     return true;
             }
             return false;
-        }
+        };
         const finalConversionComponentOnConverter = (value: string) => conversionComponentOnConverter[validationComponentOnConverter.findIndex(validationComponent => validationComponent(value))](value);
 
         return containNullable
@@ -274,7 +394,7 @@ export class CSVLoader<T extends any[], U> {
                 value => finalConversionComponentOnConverter(value))
             : value => new GenericStringToAnyConverter(value, typeOnConverter,
                 value => (containNonEmptyString ? value === '' : false) || finalValidationComponentOnConverter(value),
-                value => containNonEmptyString && value === '' ? null : finalConversionComponentOnConverter(value))
+                value => containNonEmptyString && value === '' ? null : finalConversionComponentOnConverter(value));
     }
 
     //endregion -------------------- initialisation methods --------------------
