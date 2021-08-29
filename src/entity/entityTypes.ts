4f686df913845edbf8c3c86cbda630adaaa04e17
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-08-29 15:09:03 -0400

    Small fix on the header name (isInDayTheme & isInNightTheme) to be (inDayTheme & inNightTheme) respectively. And small modification to the EntityLink to be prepared for a more strict string type.

--- src/entity/entityTypes.ts
+++ src/entity/entityTypes.ts
@@ -6,5 +6,6 @@ export type GeneralGlobalEntityLimitType = | boolean | 'Not on track' | null;
 export type ProjectileEntityLimitType = | boolean | '?' | 'Temporary as it comes out' | null;
 export type CustomLimitType = | PossibleEntityLimits | '?' | null;
 
-export type EntityLink = | 'this' | string | `${string | 'this'} / ${'this' | string}`;
+export type SingleEntityName = string;
+export type EntityLink = Exclude<| 'this' | SingleEntityName | `${SingleEntityName | 'this'} / ${'this' | SingleEntityName}`, 'this / this'>;
 export type PossibleLightSource = | '?' | `${'Dim' | 'Full'} light` | 'Project a light in front of them' | 'Variable' | null;
