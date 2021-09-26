2e83dd6dd8f8618623d5897848e611d455332fd8
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-09-26 13:57:59 -0400

    Addition of a default value for the "isInSpan" and multiple return type for the translation callback (string or react component).

--- src/lang/components/ContentTranslationComponent.tsx
+++ src/lang/components/ContentTranslationComponent.tsx
@@ -2,6 +2,6 @@ import type {TranslationProperty} from './TranslationProperty';
 
 import AnyTranslationComponent from './AnyTranslationComponent';
 
-export default function ContentTranslationComponent({translationCallback, isInSpan = false,}: TranslationProperty<'content'>,) {
+export default function ContentTranslationComponent({translationCallback, isInSpan = AnyTranslationComponent.DEFAULT_IS_IN_SPAN,}: TranslationProperty<'content'>,) {
     return <AnyTranslationComponent translationCallback={translationCallback} isInSpan={isInSpan} namespace="content"/>;
 }
--- src/lang/components/LanguageTranslationComponent.tsx
+++ src/lang/components/LanguageTranslationComponent.tsx
@@ -2,6 +2,6 @@ import type {TranslationProperty} from './TranslationProperty';
 
 import AnyTranslationComponent from './AnyTranslationComponent';
 
-export default function LanguageTranslationComponent({translationCallback, isInSpan = false,}: TranslationProperty<'language'>,) {
+export default function LanguageTranslationComponent({translationCallback, isInSpan = AnyTranslationComponent.DEFAULT_IS_IN_SPAN,}: TranslationProperty<'language'>,) {
     return <AnyTranslationComponent translationCallback={translationCallback} isInSpan={isInSpan} namespace="language"/>;
 }
