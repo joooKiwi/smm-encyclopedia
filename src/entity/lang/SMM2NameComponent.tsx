c593d41d5269fd6aac018c03a54d93c221870e82
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-10-05 20:42:02 -0400

    Separation of some section into a sub function to make it clearer to know where it creates what.

--- src/entity/lang/SMM2NameComponent.tsx
+++ src/entity/lang/SMM2NameComponent.tsx
@@ -1,5 +1,5 @@
-import {Popover}        from 'bootstrap';
-import {useTranslation} from 'react-i18next';
+import {Popover}                   from 'bootstrap';
+import {TFunction, useTranslation} from 'react-i18next';
 
 import type {Name}          from '../../lang/name/Name';
 import type {ReactProperty} from '../../util/ReactProperty';
@@ -7,20 +7,35 @@ import type {ReactProperty} from '../../util/ReactProperty';
 import {EveryLanguages} from '../../lang/EveryLanguages';
 import SpanPopover      from '../../bootstrap/popover/SpanPopover';
 
+//region -------------------- SMM2 name component types --------------------
+
+type PopoverOrientation = | 'auto' | 'top' | 'bottom' | 'left' | 'right';
+
 interface SMM2NameProperties
     extends ReactProperty {
 
-    popoverOrientation?: | 'auto' | 'top' | 'bottom' | 'left' | 'right'
+    popoverOrientation?: PopoverOrientation
+
     id: string
+
     name: Name
 
 }
+
+//endregion -------------------- SMM2 name component types --------------------
 
 export default function SMM2NameComponent({popoverOrientation, id, name,}: SMM2NameProperties,): JSX.Element {
     const {t: languageTranslation,} = useTranslation('language');
     const {t: contentTranslation,} = useTranslation('content');
 
     const elementId = id + '_' + name.english.toLowerCase().replace(' ', '_');
+
+    return <SpanPopover elementId={elementId} option={createOption(createContent(name, languageTranslation,), popoverOrientation, contentTranslation,)}>
+        {EveryLanguages.currentLanguage.get(name)}
+    </SpanPopover>;
+}
+
+function createContent(name: Name, languageTranslation: TFunction<'language'>,): string {
     const languagesToDisplay = name.originalLanguages.filter(language => !language.isCurrentLanguage);
     let content = '<ol class="m-0">';
     name.toNameMap().forEach((value, language,) => {
@@ -30,6 +45,10 @@ export default function SMM2NameComponent({popoverOrientation, id, name,}: SMM2N
     });
     content += '</ol>';
 
+    return content;
+}
+
+function createOption(content: string, popoverOrientation: | PopoverOrientation | undefined, contentTranslation: TFunction<'content'>,): Partial<Popover.Options> {
     const option: Partial<Popover.Options> = {
         title: contentTranslation('In other languages'),
         content: content,
@@ -39,7 +58,5 @@ export default function SMM2NameComponent({popoverOrientation, id, name,}: SMM2N
     if (popoverOrientation != null)
         option.placement = popoverOrientation;
 
-    return <SpanPopover elementId={elementId} option={option}>
-        {EveryLanguages.currentLanguage.get(name)}
-    </SpanPopover>;
+    return option;
 }
