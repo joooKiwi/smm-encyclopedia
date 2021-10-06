aeb4d382014550e4aa6fd348bd2403fef2fc4f82
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-10-05 20:41:17 -0400

    Change to the editor limit component to handle a null editor limit as well as the fact that it can be unknown.

--- src/entity/limit/EditorLimitComponent.tsx
+++ src/entity/limit/EditorLimitComponent.tsx
@@ -2,16 +2,23 @@ import type {LimitProperty} from '../properties/limit/LimitProperty';
 import type {ReactProperty} from '../../util/ReactProperty';
 
 import PossiblyKnownTextContainer from '../../app/tools/text/PossiblyKnownTextContainer';
+import SMM2NameComponent          from '../lang/SMM2NameComponent';
 
 interface EditorLimitProperties
     extends ReactProperty {
+
+    index: number
 
     limit: LimitProperty
 
 }
 
-export default function EditorLimitComponent({limit,}: EditorLimitProperties,) {
-    const name = limit.editorLimit?.englishName;
+export default function EditorLimitComponent({index, limit,}: EditorLimitProperties,) {
+    const editorLimit = limit.editorLimit;
 
-    return <PossiblyKnownTextContainer isKnown={!limit.isEditorLimitUnknown} content={name}/>;
+    return limit.isEditorLimitUnknown
+        ? <PossiblyKnownTextContainer isKnown={false} content=""/> :
+        editorLimit == null
+            ? <></>
+            : <SMM2NameComponent id={`entityLimit_name_${index}`} name={editorLimit.reference}/>;
 }
