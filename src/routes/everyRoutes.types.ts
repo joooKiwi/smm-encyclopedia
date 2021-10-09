753aab92e5131b650b289b8f8c115ad13ecdb26f
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-10-09 14:16:20 -0400

    Addition of a type to have every path. Change from a string to the type "EveryPossibleRoutePartialPaths" instead.

--- src/routes/everyRoutes.types.ts
+++ src/routes/everyRoutes.types.ts
@@ -1,6 +1,7 @@
 import type {everySimpleRoutes}               from './everyRoutes';
 import type {PossibleProjectLanguagesAcronym} from '../lang/EveryLanguages.types';
 
-export type EveryPossibleRoutes = `/${PossibleProjectLanguagesAcronym}${typeof everySimpleRoutes[number]['path']}`;
+export type EveryPossibleRoutes = `/${PossibleProjectLanguagesAcronym}${EveryPossibleRoutePartialPaths}`;
 
+export type EveryPossibleRoutePartialPaths = typeof everySimpleRoutes[number]['path'];
 export type EveryPossibleRouteNames = typeof everySimpleRoutes[number]['name'];
