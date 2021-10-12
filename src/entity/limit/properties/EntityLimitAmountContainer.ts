ebe8bf5b095a79edede572dc89e2bff0ac410df4
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-10-12 10:04:49 -0400

    Change from a regular property to a number property

--- src/entity/limit/properties/EntityLimitAmountContainer.ts
+++ src/entity/limit/properties/EntityLimitAmountContainer.ts
@@ -1,12 +1,12 @@
 import type {EntityLimitAmount, PossibleAmount} from './EntityLimitAmount';
 import type {PossibleComment}                   from '../../_properties/ClassWithComment';
 
-import {PropertyWithCommentThatCanBeUnknownContainer} from '../../_properties/PropertyWithCommentThatCanBeUnknownContainer';
+import {NumberPropertyWithCommentThatCanBeUnknownContainer} from '../../_properties/NumberPropertyWithCommentThatCanBeUnknownContainer';
 
 export class EntityLimitAmountContainer<AMOUNT extends PossibleAmount = PossibleAmount,
     IS_UNKNOWN extends boolean = boolean,
     COMMENT extends PossibleComment = PossibleComment, >
-    extends PropertyWithCommentThatCanBeUnknownContainer<AMOUNT, IS_UNKNOWN, COMMENT>
+    extends NumberPropertyWithCommentThatCanBeUnknownContainer<AMOUNT, IS_UNKNOWN, COMMENT>
     implements EntityLimitAmount<AMOUNT, IS_UNKNOWN, COMMENT> {
 
     public constructor(amount: AMOUNT, isUnknown: IS_UNKNOWN, comment: COMMENT,) {
