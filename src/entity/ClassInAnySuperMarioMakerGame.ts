62bc9820fa1ae4344fb58012c1945f051f57838c
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-10-25 14:32:18 -0400

    Removal of the game titles in the translation files since they will not be needed. A new section will be added instead.

---
+++ src/entity/ClassInAnySuperMarioMakerGame.ts
@@ -0,0 +1,7 @@
+export interface ClassInAnySuperMarioMakerGame<SMM1 extends boolean = boolean, SMM2 extends boolean = boolean, > {
+
+    get isInSuperMarioMaker1(): SMM1
+
+    get isInSuperMarioMaker2(): SMM2
+
+}
