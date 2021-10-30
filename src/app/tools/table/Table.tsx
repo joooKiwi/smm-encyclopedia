135bc0ad3b234a656bea41d3239dd4e709803ac4
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-10-30 01:53:12 -0400

    Small fix on the headers color

--- src/app/tools/table/Table.tsx
+++ src/app/tools/table/Table.tsx
@@ -48,7 +48,7 @@ export default class Table
     }
 
     protected get headersColor() {
-        return this.props['headers-color'] ?? Table.DEFAULT_TABLE_COLOR;
+        return this.props['headers-color'] ?? Table.DEFAULT_HEADERS_COLOR;
     }
 
     //endregion -------------------- Getter methods --------------------
