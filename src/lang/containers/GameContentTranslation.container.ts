05616c7200d2f1ecf7b9dd47f8965060466398be
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-10-31 18:45:18 -0400

    Addition of translation property containers.

---
+++ src/lang/containers/GameContentTranslation.container.ts
@@ -0,0 +1,15 @@
+import type {GameContentNamespace, SimpleTranslationProperty, SingleTranslationKey} from '../components/TranslationProperty';
+
+import {AbstractTranslationContainer} from './AbstractTranslation.container';
+
+export class GameContentTranslationContainer
+    extends AbstractTranslationContainer<GameContentNamespace>
+    implements SimpleTranslationProperty<GameContentNamespace> {
+
+    public static readonly NAMESPACE: GameContentNamespace = 'gameContent';
+
+    constructor(translationKey: SingleTranslationKey<GameContentNamespace>,) {
+        super(GameContentTranslationContainer.NAMESPACE, translationKey,);
+    }
+
+}
