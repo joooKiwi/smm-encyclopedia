2683dd5b7b475a75ccf43eca57ba47fae5f246ca
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-10-31 18:37:56 -0400

    Addition of the namespace types.

--- src/lang/components/TranslationProperty.ts
+++ src/lang/components/TranslationProperty.ts
@@ -1,19 +1,25 @@
-import type {Namespace, TFuncKey, TFunction} from 'react-i18next';
+import type {Namespace as OriginalNamespace, TFuncKey, TFunction} from 'react-i18next';
 
 import type {SimpleReactPropertyWithChildren} from '../../util/react/ReactProperty';
 
 export type TranslationReturnType = | string | JSX.Element;
-export type ContentCallback = (translation: TFunction<'content'>,) => TranslationReturnType;
-export type GameContentCallback = (translation: TFunction<'gameContent'>,) => TranslationReturnType;
-export type EntityContentCallback = (translation: TFunction<'entityContent'>,) => TranslationReturnType;
-export type LanguageCallback = (translation: TFunction<'language'>,) => TranslationReturnType;
+export type ContentCallback = (translation: TFunction<ContentNamespace>,) => TranslationReturnType;
+export type GameContentCallback = (translation: TFunction<GameContentNamespace>,) => TranslationReturnType;
+export type EntityContentCallback = (translation: TFunction<EntityContentNamespace>,) => TranslationReturnType;
+export type LanguageCallback = (translation: TFunction<LanguageNamespace>,) => TranslationReturnType;
 
+export type ContentNamespace = 'content';
+export type GameContentNamespace = 'gameContent';
+export type EntityContentNamespace = 'entityContent';
+export type LanguageNamespace = 'language';
+export type Namespace = OriginalNamespace;
 
 export type SingleTranslationKey<N extends Namespace, > = TFuncKey<N> extends infer S ? S : never;
 
 interface _AnyTranslationProperty<N extends Namespace, > {
 
     namespace: N
+
 }
 
 export interface TranslationProperty<N extends Namespace, >
