3b54765e9fa8a8a6db58cc63ed4ad60e818ad3a2
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-11-08 14:18:05 -0500

    Correction on the ordinal for the enums.

--- src/util/enum/enumUtilityMethods.ts
+++ src/util/enum/enumUtilityMethods.ts
@@ -9,6 +9,6 @@ const ordinalMap: Map<EnumerableStatic, number> = new Map();
  */
 export function getLastOrdinalOn<O extends number = number, >(instance: EnumerableStatic<O>,): O {
     return ordinalMap.has(instance)
-        ? ordinalMap.set(instance, 0).get(instance) as O
-        : ordinalMap.set(instance, ordinalMap.get(instance)! + 1).get(instance) as O;
+        ? ordinalMap.set(instance, ordinalMap.get(instance)! + 1).get(instance) as O
+        : ordinalMap.set(instance, 0).get(instance) as O;
 }
