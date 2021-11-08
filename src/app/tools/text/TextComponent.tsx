11a9f124d3114a7916e4b0f45ba839547626f67c
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-11-08 14:44:06 -0500

    Relocation of the "PossiblyKnownTextComponent" into the "TextComponent" instead.

--- src/app/tools/text/TextComponent.tsx
+++ src/app/tools/text/TextComponent.tsx
@@ -1,22 +1,47 @@
+import './TextComponent.scss';
+
 import type {ReactProperty} from '../../../util/react/ReactProperty';
 
-export type PossibleTextContent = | string | number | boolean;
+export type PossibleTextContent = | string | 'N/A' | number | boolean;
 
-export interface TextProperties<T extends PossibleTextContent = PossibleTextContent, >
+interface TextProperties<T extends PossibleTextContent = PossibleTextContent, >
     extends ReactProperty {
 
-    content: | null | undefined | T
+    /**
+     * The content used for the {@link TextComponent}
+     * @see PossibleTextContent
+     */
+    content: | T | null | undefined
 
-    classes?: | null | string[]
+    /**
+     * Tell whenever the value is unknown or not.
+     *
+     * @note It will add a "?" after the content as a CSS attribute (--is-unknown)
+     * @see DEFAULT_IS_UNKNOWN
+     */
+    isUnknown?: boolean | null | undefined
+
+    /**
+     * The classes used for the text component.
+     */
+    classes?: | string[] | null | undefined
 
 }
+
+const DEFAULT_IS_UNKNOWN = false;
 
 /**
  *
  * @param properties
  * @reactComponent
  */
-export default function TextComponent<T extends PossibleTextContent = PossibleTextContent, >({content, classes,}: TextProperties<T>,) {
+export default function TextComponent<T extends PossibleTextContent = PossibleTextContent, >({content, isUnknown = DEFAULT_IS_UNKNOWN, classes,}: TextProperties<T>,) {
+    if (content === 'N/A')
+        return <span className="not-applicable"/>;
+
+    if (isUnknown)
+        (classes ??= []).push('is-unknown');
+
     if (classes == null)
         return <span>{content}</span>;
     return <span className={classes.join(' ')}>{content}</span>;
