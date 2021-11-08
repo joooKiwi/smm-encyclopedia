b0c189b47b0a5a3e9b64023fb946f0a61f20e71b
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-11-08 14:21:06 -0500

    Addition of an error handler over the application in general.

--- src/IndexComponent.tsx
+++ src/IndexComponent.tsx
@@ -11,7 +11,7 @@ import Routes             from './routes/Routes';
  *
  * @reactComponent
  */
-export default function IndexComponent() {
+function FunctionIndexComponent() {
     const [currentLanguage, setCurrentLanguage,] = useState(setAndGetCurrentLanguage());
     EveryLanguages.INTERNATIONALISATION_SET_CURRENT_LANGUAGE = setCurrentLanguage;
 
@@ -25,4 +25,29 @@ export default function IndexComponent() {
 function setAndGetCurrentLanguage(): PossibleProjectLanguagesInternationalAcronym {
     ProjectLanguages.currentLanguage ??= 'en_AM';
     return ProjectLanguages.currentLanguage.internationalAcronym;
+}
+
+export default class IndexComponent
+    extends React.PureComponent<{}, { hasError: boolean, }> {
+
+    public constructor(props: {},) {
+        super(props);
+        this.state = {hasError: false,};
+    }
+
+    public static getDerivedStateFromError(error: Error,) {
+        return {hasError: true,};
+    }
+
+    public componentDidCatch(error: Error, errorInfo: React.ErrorInfo,): void {
+        console.warn(error.message);
+        console.warn(errorInfo.componentStack);
+    }
+
+    public render() {
+        if (this.state.hasError)
+            return <h1>An error happened in the application!</h1>;
+        return <FunctionIndexComponent/>;
+    }
+
 }
