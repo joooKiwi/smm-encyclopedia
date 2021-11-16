f3b9898e167a0fdfeb52fed67b8f96c074d49027
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-11-16 00:58:50 -0500

    Addition of a "replace" attribute on the Every Mystery Mushrooms tab.

--- src/navigation/DisplayTab.tsx
+++ src/navigation/DisplayTab.tsx
@@ -1,5 +1,6 @@
 import ContentTranslationComponent from '../lang/components/ContentTranslationComponent';
 import SingleDisplayTab            from './SingleDisplayTab';
+import {TranslationUtility}        from '../lang/components/TranslationUtility';
 
 /**
  * @reactComponent
@@ -20,7 +21,8 @@ export default function DisplayTab() {
             <SingleDisplayTab routeName="everyThemes" callback={translation => translation('Display every themes')}/>
             <SingleDisplayTab routeName="everySoundEffects" callback={translation => translation('Display every sound effects')}/>
             <SingleDisplayTab routeName="everySoundEffectCategories" callback={translation => translation('Display every sound effect categories')}/>
-            <SingleDisplayTab routeName="everyMysteryMushrooms" callback={translation => translation('Display every Mystery Mushrooms', {pluralName: 'Mystery Mushrooms'},)}/>{/*TODO add Mystery Mushroom but the plural name*/}
+            <SingleDisplayTab routeName="everyMysteryMushrooms" callback={translation => TranslationUtility.replaceAndInterpretTranslation(translation, 'Display every Mystery Mushrooms',
+                {pluralName: <span key="everyMysteryMushrooms_pluralName" className="text-decoration-underline">Mystery Mushrooms</span>})}/>{/*TODO add Mystery Mushroom but the plural name*/}
         </ul>
     </li>;
 }
