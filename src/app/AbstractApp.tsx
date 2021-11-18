71c223d04c48992dd19c8354851714a1742498c8
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-11-18 13:28:43 -0500

    Addition of an id for the main & navigation container and change on the footer container to be similar to the others.

--- src/app/AbstractApp.tsx
+++ src/app/AbstractApp.tsx
@@ -14,14 +14,12 @@ export default abstract class AbstractApp<T = {}>
     extends Component<T>
     implements ReactComponent {
 
-
     protected abstract _mainContent(): JSX.Element;
-
 
     public render() {
         return (<>
             <Navigation/>
-            <main className="pt-3 pb-5 align-bottom container-fluid">
+            <main id="main-container" className="pt-3 pb-5 align-bottom container-fluid">
                 {this._mainContent()}
             </main>
             <Footer/>
