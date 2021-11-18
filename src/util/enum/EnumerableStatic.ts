332aeebd266d05c0ade9a6d61effd30a2cbf69e2
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-11-18 00:17:39 -0500

    Addition of a new instance types utility to have a simpler and standard name for the enum retrieval via "getValue". And Addition of a type to validate the static instance does have the names instead of assuming it has it.

--- src/util/enum/EnumerableStatic.ts
+++ src/util/enum/EnumerableStatic.ts
@@ -1,26 +1,30 @@
-import {Enumerable} from './Enumerable';
+import type {Enumerable}                                                        from './Enumerable';
+import type {EnumByName, EnumByNumber, EnumByOrdinal, EnumByString, SimpleEnum} from './Enum.types';
 
-export interface EnumerableStatic<O extends number = number, N extends string = string, >
+interface _EnumerableStatic<O extends number = number, N extends string = string, E extends Enumerable<O, N> = Enumerable<O, N>, >
     extends Function {
 
     getValue(nullValue: | null | undefined,): null
 
-    getValue(ordinal: O,): this['values'][O]
+    getValue(ordinal: O,): EnumByOrdinal<this['values'], O, E>
 
-    getValue<O extends number = number, >(ordinal: O,): | NonNullable<this['values'][O]> | null
+    getValue<O extends number = number, >(ordinal: O,): EnumByNumber<this['values'], O>
 
-    getValue(name: N,): Enumerable<O, N>
+    getValue(name: N,): EnumByName<N, E>
 
-    getValue(name: string,): Enumerable<O, N> | null
+    getValue<N2 extends string = string, >(name: N2,): EnumByString<N2, N2, N, E>
 
-    getValue<I extends Enumerable<O, N> = Enumerable<O, N>, >(instance: I,): I
+    getValue<I extends E = E, >(instance: I,): I
 
-    getValue<I extends Enumerable<O, N> = Enumerable<O, N>, >(value: | I | Enumerable<O, N> | string | number | null | undefined,): | I | Enumerable<O, N> | null
+    getValue<I extends E = E, >(value: | I | E | string | number | null | undefined,): | I | E | null
 
 
     /**
      * Get every values inside the instance.
      */
-    get values(): readonly Enumerable<O, N>[]
+    get values(): readonly E[]
 
 }
+
+export type EnumerableStatic<O extends number = number, N extends string = string, E extends Enumerable<O, N>
+    = Enumerable<O, N>, > = _EnumerableStatic<O, N, E> & SimpleEnum<N, E>;
