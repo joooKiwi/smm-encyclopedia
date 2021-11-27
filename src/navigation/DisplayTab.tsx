e41488b598e0987dc3a96f969a4ae9afb8fcf5fd
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2021-11-27 00:10:26 -0500

    Addition of the game references to the navigation. And addition of a divider to help organise & see the categories.

--- src/navigation/DisplayTab.tsx
+++ src/navigation/DisplayTab.tsx
@@ -15,14 +15,18 @@ export default function DisplayTab() {
         <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
             <SingleDisplayTab routeName="everyEntities" callback={translation => translation('Display every entities')}/>
             <SingleDisplayTab routeName="everyCategories" callback={translation => translation('Display every entity categories')}/>
-            <SingleDisplayTab routeName="everyGameStyles" callback={translation => translation('Display every game styles')}/>
             <SingleDisplayTab routeName="everyGroups" callback={translation => translation('Display every entity groups')}/>
             <SingleDisplayTab routeName="everyLimits" callback={translation => translation('Display every limits')}/>
             <SingleDisplayTab routeName="everyThemes" callback={translation => translation('Display every themes')}/>
+            <div className="dropdown-divider"/>
             <SingleDisplayTab routeName="everySoundEffects" callback={translation => translation('Display every sound effects')}/>
             <SingleDisplayTab routeName="everySoundEffectCategories" callback={translation => translation('Display every sound effect categories')}/>
+            <div className="dropdown-divider"/>
             <SingleDisplayTab routeName="everyMysteryMushrooms" callback={translation => TranslationUtility.replaceAndInterpretTranslation(translation, 'Display every Mystery Mushrooms',
                 {pluralName: <span key="everyMysteryMushrooms_pluralName" className="text-decoration-underline">Mystery Mushrooms</span>})}/>{/*TODO add Mystery Mushroom but the plural name*/}
+            <div className="dropdown-divider"/>
+            <SingleDisplayTab routeName="everyGameReferences" callback={translation => translation('Display every game references')}/>
+            <SingleDisplayTab routeName="everyGameStyles" callback={translation => translation('Display every game styles')}/>
         </ul>
     </li>;
 }
