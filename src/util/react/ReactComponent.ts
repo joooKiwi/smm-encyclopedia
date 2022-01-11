fb7dcf2d6c6d1957b34a59acd5436e0efe124b66
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2022-01-10 23:50:54 -0500

    Addition of a basic React component with state.

--- src/util/react/ReactComponent.ts
+++ src/util/react/ReactComponent.ts
@@ -1,4 +1,5 @@
-import type {ReactNode} from 'react';
+import type {ReactNode}  from 'react';
+import type {ReactState} from './ReactState';
 
 /**
  * A simplified react component class with a render method.
@@ -7,5 +8,23 @@ import type {ReactNode} from 'react';
 export interface ReactComponent<T extends ReactNode = JSX.Element, > {
 
     render(): T
+
+}
+
+export interface ReactComponentWithState<S extends ReactState, > {
+
+    get state(): Readonly<S>
+
+    /**
+     *
+     * @param state_or_stateCallback
+     * @param callback The callback to call after the current
+     * @see Component.setState
+     * @see https://reactjs.org/docs/react-component.html#setstate
+     */
+    setState<K extends keyof S>(
+        state_or_stateCallback: ((previousState: Readonly<S>, properties: Readonly<{}>) => (Pick<S, K> | S | null)) | (Pick<S, K> | S | null),
+        callback?: () => void
+    ): void
 
 }
