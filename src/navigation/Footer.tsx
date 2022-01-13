02e9c3bb6d2519cab0b230b9b65703a99c49ea38
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2022-01-12 22:07:30 -0500

    Update on the last time edited date.

--- src/navigation/Footer.tsx
+++ src/navigation/Footer.tsx
@@ -21,7 +21,7 @@ export default function Footer() {
                 }</ContentTranslationComponent>
                 <ContentTranslationComponent>{translation =>
                     <span id="copyright_lastEdited" className="text-center text-light small">
-                        {translation('Last update')}: <SimpleDate day={26} month={11} year={2021}/>
+                        {translation('Last update')}: <SimpleDate day={12} month={1} year={2022}/>
                     </span>
                 }</ContentTranslationComponent>
             </div>
