995ed121bb732c8029ef80ddb2d769016417d8e7
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2022-01-17 21:39:13 -0500

    Addition of the version that the entity appear. Addition of the missing instruments for some entities.

--- src/core/instrument/loader.types.ts
+++ src/core/instrument/loader.types.ts
@@ -1,7 +1,8 @@
-type PossibleEnglishName = string;
+export type PossibleEnglishName = string;
 export type PossibleInstrument =
     | 'null'
     | PossibleEnglishName
-    | 'Green → Marimba\nRed → Vibraphone' | 'Green → Dog\nRed → Cat' | 'Chain Chomp → Piano 1\nStump → Mokugyo'
+    | 'Green → Marimba\nRed → Vibraphone' | 'Green → Dog\nRed → Cat' | 'Unchain Chomp → Piano 1\nStump → Mokugyo'
     | 'regular → Cymbal\nsideway → Hi-Hat'
+    | 'Bottom → Hello\nTop → Ok'
     | null;
