d9a6d1840e2fb7d93003b12af7b09d694ee13ab8
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2022-01-22 11:23:38 -0500

    Fix on the property "className" to handle null values.

--- src/bootstrap/modal/element/ModalContainer.tsx
+++ src/bootstrap/modal/element/ModalContainer.tsx
@@ -1,6 +1,6 @@
 import type {HTMLDivProperties}                                              from '../../../util/react/html/HTMLDivProperties';
 import type {ReactElement, ReactProperty, ReactPropertyWithOptionalChildren} from '../../../util/react/ReactProperty';
-import {EMPTY_OBJECT}                                                        from '../../../util/emptyVariables';
+import {EMPTY_OBJECT, EMPTY_STRING}                                          from '../../../util/emptyVariables';
 
 export type PossibleModalSize = | 'sm' | 'md' | 'lg' | 'xl';
 
@@ -27,9 +27,9 @@ const DEFAULT_SIZE: PossibleModalSize = 'md';
  * @reactComponent
  * @param properties
  */
-export default function ModalContainer({id, className, title, children, verticallyCentered = DEFAULT_VERTICALLY_CENTERED, modalSize = DEFAULT_SIZE,
-                                           modalDialogProperties: {className: modalDialogClassName, ...otherModalDialogProperties} = EMPTY_OBJECT,
-                                           modalContentProperties: {className: modalContentClassName, ...otherModalContentProperties} = EMPTY_OBJECT,
+export default function ModalContainer({id, className=EMPTY_STRING, title, children, verticallyCentered = DEFAULT_VERTICALLY_CENTERED, modalSize = DEFAULT_SIZE,
+                                           modalDialogProperties: {className: modalDialogClassName = EMPTY_STRING, ...otherModalDialogProperties} = EMPTY_OBJECT,
+                                           modalContentProperties: {className: modalContentClassName = EMPTY_STRING, ...otherModalContentProperties} = EMPTY_OBJECT,
                                            ...otherProperties}: ReactPropertyWithOptionalChildren<ModalContainerProperties, | ReactElement | readonly ReactElement[]>,) {
     return <div  {...otherProperties} key={`${id} - modal`} className={`modal fade ${className}`} id={id}>
         <div {...otherModalDialogProperties} key={`${id} - modal dialog`} className={`modal-dialog ${verticallyCentered ? 'modal-dialog-centered' : ''} ${modalSize !== DEFAULT_SIZE ? `modal-${modalSize}` : ''} ${modalDialogClassName}`}>
--- src/bootstrap/modal/element/ModalHeader.tsx
+++ src/bootstrap/modal/element/ModalHeader.tsx
@@ -1,9 +1,9 @@
 import type {HTMLDivProperties}           from '../../../util/react/html/HTMLDivProperties';
-import type {HTMLButtonProperties}             from '../../../util/react/html/HTMLButtonProperties';
+import type {HTMLButtonProperties}        from '../../../util/react/html/HTMLButtonProperties';
 import type {HTMLHeadingProperties}       from '../../../util/react/html/HTMLHeadingProperties';
 import type {ReactElement, ReactProperty} from '../../../util/react/ReactProperty';
 
-import {EMPTY_OBJECT}                     from '../../../util/emptyVariables';
+import {EMPTY_OBJECT, EMPTY_STRING} from '../../../util/emptyVariables';
 
 interface ModalHeaderProperties
     extends ReactProperty, HTMLDivProperties {
@@ -21,12 +21,12 @@ interface ModalHeaderProperties
  * @reactComponent
  * @param properties
  */
-export default function ModalHeader({className, modalTitle,
-                                        titleProperties: {className: titleClassName, ...otherTitleProperties} = EMPTY_OBJECT,
-                                        closeButtonProperties: {className: closeButtonClassName, ...otherCloseButtonProperties} = EMPTY_OBJECT,
+export default function ModalHeader({className = EMPTY_STRING, modalTitle,
+                                        titleProperties: {className: titleClassName = EMPTY_STRING, ...otherTitleProperties} = EMPTY_OBJECT,
+                                        closeButtonProperties: {className: closeButtonClassName = EMPTY_STRING, ...otherCloseButtonProperties} = EMPTY_OBJECT,
                                         ...otherProperties}: ModalHeaderProperties,) {
     return <div {...otherProperties} className={`modal-header ${className}`}>
         <h4 {...otherTitleProperties} className={`modal-title w-100 text-center ${titleClassName}`}>{modalTitle}</h4>
-        <button {...otherCloseButtonProperties} type="button" className={`btn-close ${closeButtonClassName}`} data-bs-dismiss="modal" aria-label="Close"/>
+        <button {...otherCloseButtonProperties} type="button" className={`btn-close ${closeButtonClassName ?? ''}`} data-bs-dismiss="modal" aria-label="Close"/>
     </div>;
 }
