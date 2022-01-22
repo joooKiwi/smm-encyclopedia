3e823adde3f264fbd77429b2cf2791a39afedf3d
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2022-01-22 11:11:33 -0500

    Addition of the parameter (to display & hide certain things on the pages)

--- src/app/AbstractApp.tsx
+++ src/app/AbstractApp.tsx
@@ -6,8 +6,13 @@ import type {AppStates}      from './AppStates.types';
 import type {ReactComponent} from '../util/react/ReactComponent';
 import type {ReactElement}   from '../util/react/ReactProperty';
 
-import Navigation from '../navigation/Navigation';
-import Footer     from '../navigation/Footer';
+import ContentTranslationComponent from '../lang/components/ContentTranslationComponent';
+import Footer                      from '../navigation/Footer';
+import ModalBody                   from '../bootstrap/modal/element/ModalBody';
+import ModalContainer              from '../bootstrap/modal/element/ModalContainer';
+import ModalFooter                 from '../bootstrap/modal/element/ModalFooter';
+import ModalHeader                 from '../bootstrap/modal/element/ModalHeader';
+import Navigation                  from '../navigation/Navigation';
 
 /**
  * @reactComponent
@@ -16,14 +21,28 @@ export default abstract class AbstractApp<T = {}, S extends AppStates = AppState
     extends Component<T, S>
     implements ReactComponent {
 
+    public static readonly PARAMETER_ELEMENT_ID = 'parameter-modal-container';
+
     protected abstract _mainContent(): ReactElement;
+
+    protected _optionContent(): ReactElement {
+        //TODO add some general parameter
+        return <></>;
+    }
 
     public render() {
         return (<>
-            <Navigation/>
+            <Navigation parameterId={AbstractApp.PARAMETER_ELEMENT_ID}/>
             <main id="main-container" className="pt-3 pb-5 align-bottom container-fluid">
                 {this._mainContent()}
             </main>
+            <ContentTranslationComponent>{translation =>
+                <ModalContainer id={AbstractApp.PARAMETER_ELEMENT_ID} verticallyCentered modalSize="xl">
+                    <ModalHeader modalTitle={translation('Options')}/>
+                    <ModalBody>{this._optionContent()}</ModalBody>
+                    <ModalFooter successButton={({value: translation('Confirm'),})}/>
+                </ModalContainer>
+            }</ContentTranslationComponent>
             <Footer/>
         </>);
     }
