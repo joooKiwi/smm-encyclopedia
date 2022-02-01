6748e0bd040d1063f8f51b6fb9bf3eb485a32ce6
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2022-01-31 23:01:41 -0500

    Fix on the bob-omb to have their editor images.

--- src/core/entity/Entities.ts
+++ src/core/entity/Entities.ts
@@ -1440,6 +1440,7 @@ export class Entities
 
         protected get _createEditorImage(): PossibleEditorImage {
             return new EditorImageBuilder('Bombhei', 1,)
+                .setAsDifferentInSMBAndSMB3()
                 .setAllGameStyles();
         }
 
