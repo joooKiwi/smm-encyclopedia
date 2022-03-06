207b5a734bbef076ebf5d2b450f1acfc2b34808e
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2022-03-06 18:41:26 -0500

    Change to the "game component" to apply every game.

--- src/core/game/Game.component.tsx
+++ src/core/game/Game.component.tsx
@@ -1,23 +1,25 @@
 import type {GameProperty} from '../entity/properties/GameProperty';
 
-import {AbstractDualEntityPropertyComponent} from '../_component/AbstractDualEntityPropertyComponent';
-import GameContentTranslationComponent       from '../../lang/components/GameContentTranslationComponent';
-import {Games}                               from './Games';
-import Image                                 from '../../app/tools/images/Image';
-import {StringContainer}                     from '../../util/StringContainer';
+import {AbstractEntityPropertyComponent} from '../_component/AbstractEntityPropertyComponent';
+import GameContentTranslationComponent   from '../../lang/components/GameContentTranslationComponent';
+import {Games}                           from './Games';
+import Image                             from '../../app/tools/images/Image';
+import {StringContainer}                 from '../../util/StringContainer';
 
 /**
  * @reactComponent
  */
 export default class GameComponent
-    extends AbstractDualEntityPropertyComponent<GameProperty> {
+    extends AbstractEntityPropertyComponent<GameProperty, Games> {
 
-    protected get _isInAll() {
-        return this.reference.isInSuperMarioMaker1 && this.reference.isInSuperMarioMaker2;
+    protected get map() {
+        return this.reference.toGameMap();
     }
 
-    protected get _isInFirst() {
-        return this.reference.isInSuperMarioMaker1;
+    protected get _isInAll() {
+        return this.reference.isInSuperMarioMaker1
+            && this.reference.isInSuperMarioMakerFor3DS
+            && this.reference.isInSuperMarioMaker2;
     }
 
     protected _renderSingleComponent(game: Games,) {
@@ -32,14 +34,6 @@ export default class GameComponent
         return <Image key={key} id={id} source={game.imagePath} fallbackName={game.englishName} className={`game-image ${gameEnglishNameInHtml}-image`}/>;
     }
 
-    protected _renderFirstComponent() {
-        return this._renderSingleComponent(Games.SUPER_MARIO_MAKER_1);
-    }
-
-    protected _renderSecondComponent() {
-        return this._renderSingleComponent(Games.SUPER_MARIO_MAKER_2);
-    }
-
     protected _renderComponentForAllAsText() {
         return <GameContentTranslationComponent children={translation => <span>{translation('Every games')}</span>}/>;
     }
@@ -47,4 +41,5 @@ export default class GameComponent
     protected _renderComponentForAllAsImages() {
         return <div key={`${this.name.english} (every games)`}>{Games.values.map(game => this._renderSingleComponent(game))}</div>;
     }
+
 }
