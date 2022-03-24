c6cf6cacf9adba30b7fe090d5a00434f96b5a210
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2022-03-24 14:38:55 -0400

    Relocation of the official notification creation to be separated of the Mii costume instead. And fix on some values that didn't include the value 1.

--- src/core/miiCostume/MiiCostume.builder.ts
+++ src/core/miiCostume/MiiCostume.builder.ts
@@ -5,30 +5,18 @@ import type {Name}                       from '../../lang/name/Name';
 import type {ObjectHolder}               from '../../util/holder/ObjectHolder';
 import type {OfficialNotificationHolder} from '../officialNotification/holder/OfficialNotificationHolder';
 
-import {Games}                               from '../game/Games';
-import {DelayedObjectHolderContainer}        from '../../util/holder/DelayedObjectHolder.container';
-import {MiiCostumeCategories}                from '../miiCostumeCategory/MiiCostumeCategories';
-import {MiiCostumeContainer}                 from './MiiCostume.container';
-import {ObjectHolders}                       from '../../util/holder/objectHolders';
-import {TemplateWithNameBuilder}             from '../_template/TemplateWithName.builder';
-import {Versions}                            from '../version/Versions';
-import {MiiCostumeCategory}                  from '../miiCostumeCategory/MiiCostumeCategory';
-import {OfficialNotificationHolderContainer} from '../officialNotification/holder/OfficialNotificationHolder.container';
-import {OfficialNotifications}               from '../officialNotification/OfficialNotifications';
-import {ObjectHolderContainer}               from '../../util/holder/ObjectHolder.container';
-import {EmptyOfficialNotificationHolder}     from '../officialNotification/holder/EmptyOfficialNotificationHolder';
+import {Games}                             from '../game/Games';
+import {DelayedObjectHolderContainer}      from '../../util/holder/DelayedObjectHolder.container';
+import {MiiCostumeCategories}              from '../miiCostumeCategory/MiiCostumeCategories';
+import {MiiCostumeContainer}               from './MiiCostume.container';
+import {ObjectHolders}                     from '../../util/holder/objectHolders';
+import {OfficialNotificationHolderBuilder} from '../officialNotification/holder/OfficialNotificationHolder.builder';
+import {TemplateWithNameBuilder}           from '../_template/TemplateWithName.builder';
+import {Versions}                          from '../version/Versions';
+import {MiiCostumeCategory}                from '../miiCostumeCategory/MiiCostumeCategory';
 
 export class MiiCostumeBuilder
     extends TemplateWithNameBuilder<MiiCostumeTemplate, MiiCostume> {
-
-    //region -------------------- Attributes --------------------
-
-    static #CONTAIN_NUMBER_REGEX = /\d/;
-    static #NUMBER_ONLY_REGEX = /^\d+$/;
-    static #OFFICIAL_NOTIFICATION_SEPARATOR = ' ';
-    static #NULL_OFFICIAL_NOTIFICATION = new ObjectHolderContainer(EmptyOfficialNotificationHolder.get);
-
-    //endregion -------------------- Attributes --------------------
 
     public constructor(templateBuilder_or_template: Builder<MiiCostumeTemplate>,) {
         super(templateBuilder_or_template, Games.SUPER_MARIO_MAKER_2, true,);
@@ -42,30 +30,7 @@ export class MiiCostumeBuilder
 
 
     private static __createOfficialNotification({officialNotification: officialNotificationName,}: MiiCostumeTemplate,): ObjectHolder<OfficialNotificationHolder> {
-        return officialNotificationName == null
-            ? this.#NULL_OFFICIAL_NOTIFICATION
-            : new DelayedObjectHolderContainer(() => {
-                const officialNotification = OfficialNotifications.getValue(officialNotificationName);
-                const englishName = officialNotification.englishName;
-
-                const lastCharacter = englishName.charAt(englishName.length - 1,);
-                if (lastCharacter === '1' || lastCharacter === '2')
-                    return OfficialNotificationHolderContainer.get(officialNotificationName, officialNotification,);//Official notification = "Receive a lot of feedback"
-
-                const numberFoundInOfficialNotification = Number(officialNotificationName.split(this.#OFFICIAL_NOTIFICATION_SEPARATOR).find(value => this.#NUMBER_ONLY_REGEX.test(value)) ?? -1);
-
-                if (!this.#CONTAIN_NUMBER_REGEX.test(officialNotificationName)) {
-                    if (numberFoundInOfficialNotification === 1)
-                        return OfficialNotificationHolderContainer.get(officialNotificationName, officialNotification, numberFoundInOfficialNotification,);//Official notification = notification that is specified to 1
-
-                    return OfficialNotificationHolderContainer.get(officialNotificationName, officialNotification,);//Official notification = notification that is specified to #
-                }
-
-                if (englishName === officialNotificationName)
-                    return OfficialNotificationHolderContainer.get(officialNotificationName, officialNotification,);//Official notification = notification without any number
-
-                return OfficialNotificationHolderContainer.get(officialNotificationName, officialNotification, numberFoundInOfficialNotification,);
-            });
+        return new DelayedObjectHolderContainer(() => new OfficialNotificationHolderBuilder(officialNotificationName).build());
     }
 
     private static __createVersion({version,}: MiiCostumeTemplate,): ObjectHolder<Versions | null> {
