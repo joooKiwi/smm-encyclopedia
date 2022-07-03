f5573290e701fe0124b41aeff760ecf048b94372
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2022-07-03 16:51:18 -0400

    Change on the StringContainer to use a better formatting on some forbidden characters. Optimisation on the html parser to remove the characters in a certain word or replace if they are alone.

--- src/util/StringContainer.ts
+++ src/util/StringContainer.ts
@@ -1,10 +1,25 @@
 export class StringContainer<T extends string, T_HTML extends string = string, > {
 
-    public static readonly REMOVAL_REGEX = /[.:()[\]]|('s)/g;
+    /**
+     * A regex containing the characters that should be removed direclty
+     * <ol>
+     *  <li>:</li>
+     *  <li>(</li>
+     *  <li>)</li>
+     *  <li>[</li>
+     *  <li>]</li>
+     *  <li>-</li>
+     *  <li>|</li>
+     *  <li>'s</li>
+     * </ol>
+     */
+    static readonly #REMOVAL_REGEX = /[.:()[\]]-|('s)/g;
 
-    public static readonly REPLACE_CHARACTERS = new Map([['?', 'questionMark'], ['!', 'exclamationMark'], ['&', 'and',]] as const);
+    static readonly #REPLACE_CHARACTERS = new Map([['?', 'question'], ['!', 'exclamation'], ['&', 'and',], ['|', 'or',],] as const);
 
-    public static readonly WORD_SEPARATOR_REGEX = /[ \-\\/]/g;
+    static readonly #WORD_SEPARATOR_REGEX = /[ \-\\/]/g;
+
+    static readonly #ALREADY_CONVERTED_VALUES = new Map<string, string>();
 
     readonly #value;
     #valueInHtml?: T_HTML;
@@ -21,11 +36,35 @@ export class StringContainer<T extends string, T_HTML extends string = string, >
         return this.#valueInHtml ??= StringContainer.getInHtml(this.get) as T_HTML;
     }
 
+    /**
+     * Convert the value received in an html format string.<br/>
+     *
+     * Example: <u>anExample-withSomething(weird)&fun</u>
+     * will be transformed in <u>anExampleWithSomethingWeirdAndFun</u><br/>
+     *
+     * It also keeps track of the converted values to not reiterate the same values twice.
+     *
+     * @param value The value to convert as a proper HTML value.
+     */
     public static getInHtml(value: string,): string {
-        const splitValues = value.toLowerCase().replaceAll(this.REMOVAL_REGEX, '',).split(this.WORD_SEPARATOR_REGEX).map(stringValue =>
-            this.REPLACE_CHARACTERS.has(stringValue as never) ? this.REPLACE_CHARACTERS.get(stringValue as never)! : stringValue);
+        if (this.#ALREADY_CONVERTED_VALUES.has(value))
+            return this.#ALREADY_CONVERTED_VALUES.get(value)!;
 
-        return splitValues.shift() + splitValues.map(stringValue => stringValue.charAt(0).toUpperCase() + stringValue.substring(1)).join('');
+        const splitValues = value.toLowerCase()
+            .replaceAll(this.#REMOVAL_REGEX, '',)
+            .split(this.#WORD_SEPARATOR_REGEX)
+            .map(stringValue => this.#REPLACE_CHARACTERS.get(stringValue as never) ?? stringValue)
+            .map(stringValue => {
+                this.#REPLACE_CHARACTERS.forEach((replacementText, characterToReplace,) => {
+                    if (stringValue.includes(characterToReplace))
+                        stringValue = stringValue.replace(characterToReplace, '');
+                });
+                return stringValue;
+            }).flat(2);
+
+        const returnedValue = splitValues.shift() + splitValues.map(stringValue => stringValue.charAt(0).toUpperCase() + stringValue.substring(1)).join('');
+        this.#ALREADY_CONVERTED_VALUES.set(value, returnedValue,);
+        return returnedValue;
     }
 
 }
