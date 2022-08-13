a31d6466e24a2f88f1b92b29b60980e40110cd67
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2022-08-13 16:17:16 -0400

    Simple formatting

--- src/app/EverySoundEffectCategoriesApp.tsx
+++ src/app/EverySoundEffectCategoriesApp.tsx
@@ -39,7 +39,7 @@ export default class EverySoundEffectCategoriesApp
 
             //region -------------------- Card list interpreter --------------------
 
-            public createCardListContent(enumerable: SoundEffectCategories): ReactElement {
+            public createCardListContent(enumerable: SoundEffectCategories,): ReactElement {
                 return <Image source={enumerable.imagePath} fallbackName={`${enumerable.englishName} - image`}/>;
             }
 
