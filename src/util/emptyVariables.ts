c9a83d3d112b3430ebd4bc3ab97cad6df6c1f498
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2022-09-04 15:15:18 -0400

    Simple change over the variables to be frozen instead of by themselves.
    And small change over the types used for the ObjectHolders.

--- src/util/emptyVariables.ts
+++ src/util/emptyVariables.ts
@@ -1,20 +1,19 @@
-/**
- * An empty string
- */
-export const EMPTY_STRING = '';
+/** An empty string */
+export const EMPTY_STRING: '' = Object.freeze('') as '';
 
 /**
  * An empty array with no values that is not modifiable.
  *
  * @note It is an empty array to ensure any possible values is possible.
  */
-export const EMPTY_ARRAY: readonly [] = [];
+export const EMPTY_ARRAY: readonly [] = Object.freeze([]) as readonly [];
 
 /**
  * An empty {@link Map} with any values that should always return undefined as a value.
  *
  * @note It use "any" as a type to enable every values
  */
-export const EMPTY_MAP: ReadonlyMap<any, never> = new Map<any, never>();
+export const EMPTY_MAP: ReadonlyMap<any, never> = Object.freeze(new Map<any, never>());
 
-export const EMPTY_OBJECT: Readonly<{}> = {};
+/** An empty object with nothing (and not modifiable) */
+export const EMPTY_OBJECT: Readonly<{}> = Object.freeze({});
