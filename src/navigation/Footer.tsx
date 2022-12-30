c16753c907151b47ad761607ada3c2401b7bcce2
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2022-12-30 17:12:50 -0500

    Addition of a Discord button

--- src/navigation/Footer.tsx
+++ src/navigation/Footer.tsx
@@ -5,6 +5,7 @@ import type {ReactProperties} from 'util/react/ReactProperties'
 
 import {contentTranslation}  from 'lang/components/translationMethods'
 import SimpleDate            from 'lang/date/SimpleDate'
+import DiscordButton         from 'navigation/button/Discord.button'
 import GithubButton          from 'navigation/button/Github.button'
 import LanguageChangerButton from 'navigation/button/LanguageChanger.button'
 
@@ -21,9 +22,10 @@ interface FooterProperties
 export default function Footer({languageChanger,}: FooterProperties,) {
     return <footer id="footer-container" className="bg-dark bg-gradient">
         <div className="container">
-            <div id="helper-container" className="row">
-                <GithubButton/>
-                <LanguageChangerButton {...languageChanger}/>
+            <div id="helper-container" className="row justify-content-around justify-content-sm-center gy-2 gy-sm-0 pb-2">
+                <div id="github-button-container" className="col-auto"><GithubButton/></div>
+                <div id="discord-button-container" className="col-auto"><DiscordButton/></div>
+                <div id="languageChanger-button-container" className="col-auto col-sm-12"><LanguageChangerButton {...languageChanger}/></div>
             </div>
             <div className="row">
                 <span id="copyright" className="text-center text-light small">
--- src/navigation/button/Github.button.tsx
+++ src/navigation/button/Github.button.tsx
@@ -1,5 +1,3 @@
-import './Github.button.scss'
-
 import Tooltip              from 'bootstrap/tooltip/Tooltip'
 import {contentTranslation} from 'lang/components/translationMethods'
 
@@ -7,7 +5,7 @@ const ID = 'github-button'
 export default function GithubButton() {
     return <Tooltip option={{title: contentTranslation('Go to the GitHub repository'), placement: 'top',}} elementId={ID}>
         <a id={ID} href="https://github.com/joooKiwi/smm-encyclopedia" className="btn btn-lg btn-outline-light rounded-pill bi bi-github">
-            <span>{contentTranslation('GitHub repository')}</span>
+            <span className="d-none d-sm-inline">{contentTranslation('GitHub repository')}</span>
         </a>
     </Tooltip>
 }
