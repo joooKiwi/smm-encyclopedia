b61ac455b68ae0891708701501837fa40797b5e5
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2022-12-30 17:14:01 -0500

    Simplification over the language changer buttons.

--- src/navigation/LanguageChanger.link.tsx
+++ src/navigation/LanguageChanger.link.tsx
@@ -1,7 +1,5 @@
 import './LanguageChanger.link.scss'
 
-import {Fragment} from 'react'
-
 import type {ReactProperties} from 'util/react/ReactProperties'
 
 import {ProjectLanguages}          from 'lang/ProjectLanguages'
@@ -26,17 +24,11 @@ export default function LanguageChangerLink({language, callbackToSetLanguage,}: 
 
     if (childrenLanguages.length === 0)
         return <div key={`language changer (single - ${language.englishName})`} id={`single-language-changer-${englishNameAsId}`}
-                    className="languageChanger-link-container  col-12 col-xl-5">
-            <LanguageChangerSingleLink language={language} callbackToSetLanguage={callbackToSetLanguage} type="button"/>
+                    className="languageChanger-link-container col-12 col-xl-5">
+            <LanguageChangerSingleLink language={language} callbackToSetLanguage={callbackToSetLanguage}/>
         </div>
 
     return <div key={`language changer (double - ${language.language.parent!.englishName})`} id={`double-language-changer-${englishNameAsId}`}
-                className="languageChanger-link-container btn-group btn-group-lg col-12 col-xl-5" role="group">{childrenLanguages.map(language => {
-        const englishNameAsId = StringContainer.getInHtml(language.englishName)
-
-        return <Fragment key={`${language.englishName} (input & label)`}>
-            <input key={`${language.englishName} (input)`} id={`input-${englishNameAsId}`} className="btn-check" autoComplete="off" defaultChecked={language.isCurrentLanguage} type="radio"/>
-            <LanguageChangerSingleLink language={ProjectLanguages.getValueByLanguage(language)!} callbackToSetLanguage={callbackToSetLanguage} type="label"/>
-        </Fragment>
-    })}</div>
+                className="languageChanger-link-container btn-group btn-group-lg col-12 col-xl-5" role="group">{childrenLanguages.map(language =>
+        <LanguageChangerSingleLink key={`${language.englishName} (single button in group)`} language={ProjectLanguages.getValueByLanguage(language)!} callbackToSetLanguage={callbackToSetLanguage}/>)}</div>
 }
