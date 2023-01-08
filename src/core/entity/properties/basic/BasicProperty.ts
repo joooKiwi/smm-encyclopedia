de5a8d3fa324842d8faa549f9caf4ff2506fbaa7
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-01-08 16:07:51 -0500

    Re-addition of a missing file for git

---
+++ src/core/entity/properties/basic/BasicProperty.ts
@@ -0,0 +1,43 @@
+import type {InferredBooleanPropertyThatCanBeNotApplicable, InferredBooleanPropertyThatCanBeNotApplicableWithComment} from 'core/_properties/Property'
+import type {NullOr, NullOrBoolean}                                                                                   from 'util/types/nullable'
+import type {BooleanOrNotApplicable}                                                                                  from 'util/types/variables'
+
+export interface BasicProperty {
+
+    //region -------------------- Has a mushroom variant --------------------
+
+    get hasAMushroomVariantContainer(): HasAMushroomVariant
+
+    get hasAMushroomVariant(): BooleanOrNotApplicable
+
+    //endregion -------------------- Has a mushroom variant --------------------
+    //region -------------------- Can be a in a parachute --------------------
+
+    get canBeInAParachuteContainer(): CanBeInAParachute
+
+    get canBeInAParachute(): NullOr<BooleanOrNotApplicable>
+
+    get canBeInAParachuteComment(): NullOr<LCL_whilePlaying>
+
+    //endregion -------------------- Can be a in a parachute --------------------
+    //region -------------------- Can have wings --------------------
+
+    get canHaveWingsContainer(): CanHaveWings
+
+    get canHaveWings(): NullOr<BooleanOrNotApplicable>
+
+    get canHaveWingsComment(): NullOr<LCL_whilePlaying>
+
+    //endregion -------------------- Can have wings --------------------
+
+}
+
+export type HasAMushroomVariant = InferredBooleanPropertyThatCanBeNotApplicable<PossibleHasAMushroomVariant>
+export type CanBeInAParachute = InferredBooleanPropertyThatCanBeNotApplicableWithComment<PossibleCanBeInAParachute>
+export type CanHaveWings = InferredBooleanPropertyThatCanBeNotApplicableWithComment<PossibleCanHaveWings>
+
+export type LCL_whilePlaying = 'While playing → LCL'
+
+export type PossibleHasAMushroomVariant = NullOrBoolean
+export type PossibleCanBeInAParachute = NullOr<| boolean | LCL_whilePlaying>
+export type PossibleCanHaveWings = NullOr<| boolean | LCL_whilePlaying>
