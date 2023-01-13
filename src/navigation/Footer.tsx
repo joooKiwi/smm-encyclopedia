03e1aa571beb6dee9d8c3c1f189d0218bac9d95f
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-01-12 23:39:08 -0500

    Update on the last time edited date

--- src/navigation/Footer.tsx
+++ src/navigation/Footer.tsx
@@ -41,7 +41,7 @@ export default function Footer({languageChanger,}: FooterProperties,) {
                     Geitje
                 </span>
                 <small id="copyright_lastEdited" className="text-center text-light">
-                    {contentTranslation('Last update')}: <i><SimpleDate day={8} month={1} year={2023}/></i>
+                    {contentTranslation('Last update')}: <i><SimpleDate day={12} month={1} year={2023}/></i>
                 </small>
             </div>
         </div>
