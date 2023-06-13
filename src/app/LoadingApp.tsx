72fbf5b157380ef6438f0f9e0cccc4f981edd5ad
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-06-13 15:16:50 -0400

    Addition of the suspension point

--- src/app/LoadingApp.tsx
+++ src/app/LoadingApp.tsx
@@ -1,12 +1,13 @@
 import AbstractApp          from 'app/AbstractApp'
 import TextComponent        from 'app/tools/text/TextComponent'
 import {contentTranslation} from 'lang/components/translationMethods'
+import {SUSPENSION_POINT}   from 'util/commonVariables'
 
 export default class LoadingApp
     extends AbstractApp {
 
     protected override _mainContent() {
-        return <TextComponent key="Loading text content" content={<>{contentTranslation('Loading the page')}…</>}/>
+        return <TextComponent key="Loading text content" content={<>{contentTranslation('Loading the page')}{SUSPENSION_POINT}</>}/>
     }
 
 }
