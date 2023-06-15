cd443aff09736146d3d6025b22138ef19ba3da07
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-06-15 17:57:23 -0400

    Removal of the name displayed for the power-up priority components

--- src/app/powerUp/priority/PowerUpPriority.component.tsx
+++ src/app/powerUp/priority/PowerUpPriority.component.tsx
@@ -3,8 +3,7 @@ import {Fragment} from 'react'
 import type {ImagesCallbackByPriority, PowerUpPriority} from 'app/powerUp/priority/PowerUpPriority'
 import type {ReactProperties}                           from 'util/react/ReactProperties'
 
-import NameComponent from 'lang/name/component/Name.component'
-import Image         from 'app/tools/images/Image'
+import Image from 'app/tools/images/Image'
 
 interface PowerUpPriorityProperties<T extends PowerUpPriority, >
     extends ReactProperties {
@@ -15,11 +14,6 @@ interface PowerUpPriorityProperties<T extends PowerUpPriority, >
 
     images?: ImagesCallbackByPriority<T>
 
-    /**
-     * @temporaryProperty
-     */
-    displayName?: boolean
-
 }
 
 const DEFAULT_CALLBACK = (priority: PowerUpPriority,) => priority.images
@@ -28,7 +22,7 @@ const DEFAULT_CALLBACK = (priority: PowerUpPriority,) => priority.images
  * @param properties
  * @reactComponent
  */
-export default function PowerUpPriorityComponent<T extends PowerUpPriority, >({id, value, images: imagesCallback = DEFAULT_CALLBACK, displayName = false,}: PowerUpPriorityProperties<T>,) {
+export default function PowerUpPriorityComponent<T extends PowerUpPriority, >({id, value, images: imagesCallback = DEFAULT_CALLBACK,}: PowerUpPriorityProperties<T>,) {
     const priorities = value instanceof Array ? value : [value]
     return <div key={`Power-up priority (${id})`} id={id}>
         {priorities.map((priority, index,) => {
@@ -36,7 +30,6 @@ export default function PowerUpPriorityComponent<T extends PowerUpPriority, >({i
                     priorityEnglishName = priorityName.english
 
                 return <Fragment key={`Power-up priority (group - ${priorityEnglishName} #${index + 1})`}>
-                    {displayName ? <NameComponent key={`Power-up priority (name - ${priorityEnglishName} #${index + 1})`} id={`powerUpPriority-${priorityEnglishName}`} name={priorityName}/> : null}
                     {imagesCallback(priority).map((image, index2,) =>
                         <Image key={`Power-up priority (image - ${priorityEnglishName} #${index + 1}-${index2 + 1})`} file={image}/>)}
                 </Fragment>
