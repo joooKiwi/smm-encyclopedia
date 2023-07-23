4c27ac0ba55474689a881e9f075bab2d98327621
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-07-23 18:30:13 -0400

    Removal of some useless type parameter

--- src/core/miiCostume/MiiCostume.template.ts
+++ src/core/miiCostume/MiiCostume.template.ts
@@ -2,10 +2,9 @@ import type {TemplateWithNameTemplate}                                          
 import type {PossibleEnglishName as PossibleEnglishName_Category}                           from 'core/miiCostumeCategory/MiiCostumeCategories.types'
 import type {PossibleEnglishNameWithOnlyAmount as PossibleEnglishName_OfficialNotification} from 'core/officialNotification/OfficialNotifications.types'
 import type {PossibleName_SMM2_Number as PossibleMarioMakerVersion_SMM2_Number}             from 'core/version/Versions.types'
-import type {NameTemplate}                                                                  from 'lang/name/Name.template'
 
 export interface MiiCostumeTemplate
-    extends TemplateWithNameTemplate<NameTemplate> {
+    extends TemplateWithNameTemplate {
 
     officialNotification: NullOr<PossibleEnglishName_OfficialNotification>
 
