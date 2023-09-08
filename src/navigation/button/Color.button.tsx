4c5b8467441742cd493f2c33b7743edcf1ba50ab
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-09-08 16:05:32 -0400

    Addition of the COLOR_MODAL_ID

--- src/navigation/button/Color.button.tsx
+++ src/navigation/button/Color.button.tsx
@@ -1,17 +1,17 @@
-import ModalButton                from 'bootstrap/modal/element/ModalButton'
-import Tooltip                    from 'bootstrap/tooltip/Tooltip'
-import {contentTranslation}       from 'lang/components/translationMethods'
-import {PARAMETER_MODAL_ID}       from 'navigation/button/modalIds'
+import ModalButton          from 'bootstrap/modal/element/ModalButton'
+import Tooltip              from 'bootstrap/tooltip/Tooltip'
+import {contentTranslation} from 'lang/components/translationMethods'
+import {COLOR_MODAL_ID}     from 'navigation/button/modalIds'
 
-const ID = "color-button"
+const ID = 'color-button'
 
 /**
  * @reactComponent
  */
 export default function ColorButton() {
     return <Tooltip elementId={ID} option={({title: contentTranslation('color.plural'), placement: 'left',})}>
-        <ModalButton key={`navigation button (color)`} id={ID} elementToShow={PARAMETER_MODAL_ID} className="btn btn-outline-primary btn-navigation bi bi-palette-fill" disabled aria-disabled>
-                <span key={`navigation text button (color)`} className={`btn-navigation-text d-none d-md-inline-block`}>{contentTranslation('color.plural')}</span>
+        <ModalButton key={`navigation button (color)`} id={ID} elementToShow={COLOR_MODAL_ID} className="btn btn-outline-primary btn-navigation bi bi-palette-fill" disabled aria-disabled>
+            <span key={`navigation text button (color)`} className={`btn-navigation-text d-none d-md-inline-block`}>{contentTranslation('color.plural')}</span>
         </ModalButton>
     </Tooltip>
 }
