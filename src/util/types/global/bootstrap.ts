8120a92e17f1a59723e3b09190d06b5245ddcf7e
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-09-11 17:16:48 -0400

    Addition of other kinds of coloration with Bootstrap

--- src/util/types/global/bootstrap.ts
+++ src/util/types/global/bootstrap.ts
@@ -2,11 +2,17 @@ export {}
 
 declare global {
 
-    type BootstrapColor = | 'primary' | 'secondary'//|'tertiary'
-                          | 'light' | 'dark'
-                          | 'success' | 'info' | 'warning' | 'danger'
-                          | 'white' | 'gray' | 'gray-dark'
-                          | 'blue' | 'indigo' | 'purple' | 'pink' | 'red' | 'orange' | 'yellow' | 'green' | 'teal' | 'cyan'
+    /** Every colour applicable to a Boostrap colour */
+    type BootstrapColor = | BootstrapThemeColor | BootstrapBasicColor
+
+    /** The basic colours used for most Bootstrap coloration when having a variant */
+    type BootstrapThemeColor = | 'primary' | 'secondary'//|'tertiary'
+                               | 'light' | 'dark'
+                               | 'success' | 'info' | 'warning' | 'danger'
+
+    /** The basic colours used by bootstrap for a basic coloration */
+    type BootstrapBasicColor = | 'white' | 'gray' | 'gray-dark'
+                               | 'blue' | 'indigo' | 'purple' | 'pink' | 'red' | 'orange' | 'yellow' | 'green' | 'teal' | 'cyan'
 
     type PossibleTooltipPlacement = | 'top' | 'bottom' | 'left' | 'right'
     type PossibleBootstrapDimension = | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
