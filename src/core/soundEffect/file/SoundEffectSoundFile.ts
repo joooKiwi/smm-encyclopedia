3f851dd3700caa6d6730beaa22e30ef34bcdf0da
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-09-20 17:20:12 -0400

    Fix on the "out" usage on the SoundEffectSoundfile

--- src/core/soundEffect/file/SoundEffectSoundFile.ts
+++ src/core/soundEffect/file/SoundEffectSoundFile.ts
@@ -1,13 +1,12 @@
-import type {PossibleSoundEffectSoundName, PossibleSoundEffectSoundName_SMM1, PossibleSoundEffectSoundName_SMM2} from 'core/soundEffect/sound/types'
-import type {NonRepeatableSoundFile}                                                                             from 'util/file/sound/NonRepeatableSoundFile'
+import type {PossibleSoundEffectSoundName_SMM1, PossibleSoundEffectSoundName_SMM2} from 'core/soundEffect/sound/types'
+import type {NonRepeatableSoundFile}                                               from 'util/file/sound/NonRepeatableSoundFile'
 
 /** A {@link NonRepeatableSoundFile} made to be related to a {@link SoundEffects} */
-export type SoundEffectSoundFile<out NAME extends PossibleSoundEffectSoundName = PossibleSoundEffectSoundName, >
-    = | SMM1SoundEffectSoundFile<NAME> | SMM2SoundEffectSoundFile<NAME>
+export type SoundEffectSoundFile = | SMM1SoundEffectSoundFile | SMM2SoundEffectSoundFile
 
 
-export type SMM1SoundEffectSoundFile<out NAME extends PossibleSoundEffectSoundName_SMM1 = PossibleSoundEffectSoundName_SMM1, >
+export type SMM1SoundEffectSoundFile<NAME extends PossibleSoundEffectSoundName_SMM1 = PossibleSoundEffectSoundName_SMM1, >
     = NonRepeatableSoundFile<'sound/sound effect/SMM1', NAME, 'wav'>
 
-export type SMM2SoundEffectSoundFile<out NAME extends PossibleSoundEffectSoundName_SMM2 = PossibleSoundEffectSoundName_SMM2, >
+export type SMM2SoundEffectSoundFile<NAME extends PossibleSoundEffectSoundName_SMM2 = PossibleSoundEffectSoundName_SMM2, >
     = NonRepeatableSoundFile<'sound/sound effect/SMM2', NAME, 'wav'>
