df5b26450747b615bd406afd8db18346b1230373
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-09-19 23:20:52 -0400

    Change of the structure for the "OfficialNotification" images to only use the "SimpleImageFile"

--- src/core/officialNotification/file/imageFiles.ts
+++ src/core/officialNotification/file/imageFiles.ts
@@ -1,15 +1,14 @@
 import {SimpleImageFile} from 'util/file/image/SimpleImageFile'
-import {MedalImageFile}  from 'core/officialNotification/file/MedalImageFile'
 
 export const LIKE_IMAGE_FILE = new SimpleImageFile('others', 'Heart_00^d', 'tiff', 'Like image',)
 export const STAMP_IMAGE_FILE = new SimpleImageFile('others', 'EventClearListIcon_00^s', 'tiff', 'Stamp image',)
 
 
-export const GOLD_MEDAL_IMAGE_FILE = new MedalImageFile(3, 'Gold medal image',)
-export const SILVER_MEDAL_IMAGE_FILE = new MedalImageFile(4, 'Silver medal image',)
-export const BRONZE_MEDAL_IMAGE_FILE = new MedalImageFile(5, 'Bronze medal image',)
+export const GOLD_MEDAL_IMAGE_FILE = new SimpleImageFile('others/medal', 'BadgeS_03^u', 'tiff', 'Gold medal image',)
+export const SILVER_MEDAL_IMAGE_FILE = new SimpleImageFile('others/medal', 'BadgeS_04^u', 'tiff', 'Silver medal image',)
+export const BRONZE_MEDAL_IMAGE_FILE = new SimpleImageFile('others/medal', 'BadgeS_05^u', 'tiff', 'Bronze medal image',)
 
 
-export const FIRST_PLACE_IMAGE_FILE = new MedalImageFile(0, '1st place image (medal)',)
-export const SECOND_PLACE_IMAGE_FILE = new MedalImageFile(1, '2nd place image (medal)',)
-export const THIRD_PLACE_IMAGE_FILE = new MedalImageFile(2, '3rd place image (medal)',)
+export const FIRST_PLACE_IMAGE_FILE = new SimpleImageFile('others/medal', 'BadgeS_00^u', 'tiff', '1st place image (medal)',)
+export const SECOND_PLACE_IMAGE_FILE = new SimpleImageFile('others/medal', 'BadgeS_01^u', 'tiff', '2nd place image (medal)',)
+export const THIRD_PLACE_IMAGE_FILE = new SimpleImageFile('others/medal', 'BadgeS_02^u', 'tiff', '3rd place image (medal)',)
