0cd9fa65dd092a945c08107280f6a500da863ff8
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-09-19 22:22:53 -0400

    Addition of a new global type "NullOrUndefined"

--- src/util/types/global/nullable.ts
+++ src/util/types/global/nullable.ts
@@ -5,6 +5,7 @@ declare global {
     type UndefinedOr<T, > = | T | undefined
     type NullOr<T, > = | T | null
     type Nullable<T, > = | T | null | undefined
+    type NullOrUndefined = | null | undefined
 
     type UndefinedOrBoolean = UndefinedOr<boolean>
     type NullOrBoolean = NullOr<boolean>
