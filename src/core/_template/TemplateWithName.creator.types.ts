61486dc430c1117f203a57c229291c7e5d91fdff
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-10-07 00:41:18 -0400

    Removal of the unused template files

--- src/core/_template/TemplateWithName.creator.types.ts
+++
@@ -1,4 +0,0 @@
-import type {TemplateWithNameTemplate}                             from 'core/_template/TemplateWithName.template'
-import type {PossibleGameReceived as OriginalPossibleGameReceived} from 'lang/name/Name.builder.types'
-
-export type PossibleGameReceived<T extends TemplateWithNameTemplate, > = | OriginalPossibleGameReceived | ((template: T,) => OriginalPossibleGameReceived)
