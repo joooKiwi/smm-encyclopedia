79caae970da4da65f9df8ace444728ae41942bf5
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-10-09 11:33:04 -0400

    Removal of some lazy references

--- src/core/entity/properties/instrument/EmptyInstrumentProperty.lazy.ts
+++
@@ -1,6 +0,0 @@
-import {lazyOf} from '@joookiwi/lazy'
-
-import {EmptyInstrumentProperty} from 'core/entity/properties/instrument/EmptyInstrumentProperty'
-
-/** A simple {@link import('@joookiwi/lazy').Lazy Lazy} instance for the {@link EmptyInstrumentProperty} */
-export const LAZY_EMPTY_INSTRUMENT_PROPERTY = lazyOf(EmptyInstrumentProperty.get,)
