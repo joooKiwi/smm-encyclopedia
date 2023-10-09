7c244fda0dd52b9f9bdcbaf205750df61240a01b
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-10-08 22:39:16 -0400

    Addition of an EMPTY_SET to the empty variables

--- src/util/emptyVariables.ts
+++ src/util/emptyVariables.ts
@@ -1,22 +1,28 @@
 /** An empty string */
-export const EMPTY_STRING: '' = Object.freeze('') as ''
+export const EMPTY_STRING: '' = Object.freeze('',) as ''
 
 /**
  * An empty array with no values that is not modifiable.
  *
  * @note It is an empty array to ensure any possible values is possible.
  */
-export const EMPTY_ARRAY: readonly [] = Object.freeze([]) as readonly []
+export const EMPTY_ARRAY: readonly [] = Object.freeze([],) as readonly []
+/**
+ * An empty array with no values that is not modifiable.
+ *
+ * @note It is an empty array to ensure any possible values is possible.
+ */
+export const EMPTY_SET: ReadonlySet<never> = Object.freeze(new Set<never>(),)
 
 /**
  * An empty {@link Map} with any values that should always return undefined as a value.
  *
  * @note It use "any" as a type to enable every values
  */
-export const EMPTY_MAP: ReadonlyMap<any, never> = Object.freeze(new Map<any, never>())
+export const EMPTY_MAP: ReadonlyMap<any, never> = Object.freeze(new Map<any, never>(),)
 
 /** An empty object with nothing (and not modifiable) */
-export const EMPTY_OBJECT: Readonly<{}> = Object.freeze({})
+export const EMPTY_OBJECT: Readonly<{}> = Object.freeze({},)
 
 /** An empty callback*/
 export const EMPTY_CALLBACK: () => void = Object.freeze(() => {},)
