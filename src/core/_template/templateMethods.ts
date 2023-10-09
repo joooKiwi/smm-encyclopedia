1076917cf4c0ba9ad6db9123b0bafe6ea51ed7b3
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-10-08 22:39:31 -0400

    Removal of the unused template method

--- src/core/_template/templateMethods.ts
+++ src/core/_template/templateMethods.ts
@@ -1,8 +1,5 @@
-import type {GameContentFromAllGames}        from 'core/game/Loader.types'
-import type {SimpleGameFromAllGamesTemplate} from 'core/game/SimpleGame.template'
-import type {LanguageContent}                from 'core/_template/LanguageContent'
-import type {NameTemplate}                   from 'lang/name/Name.template'
-
+import type {LanguageContent} from 'core/_template/LanguageContent'
+import type {NameTemplate}    from 'lang/name/Name.template'
 
 /**
  * Create a {@link NameTemplate name template} from a {@link content}
@@ -28,19 +25,4 @@ export function createNameTemplate(content: LanguageContent,): NameTemplate {
         ukrainian: content?.ukrainian ?? null,
         greek: content?.greek ?? null,
     }
-}
-
-
-/**
- * Create a game template from a {@link content}
- *
- * It implied that has properties that are {@link boolean}.
- *  1. {@link Games.SUPER_MARIO_MAKER_1 SMM1}
- *  1. {@link Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS SMM3DS}
- *  1. {@link Games.SUPER_MARIO_MAKER_2 SMM2}
- *
- * @param content The {@link GameContentFrom1And2} applicable to a {@link Games} from three arguments
- */
-export function createGameTemplateFromAllGames(content: GameContentFromAllGames,): SimpleGameFromAllGamesTemplate<boolean, boolean, boolean> {
-    return {1: content.isInSuperMarioMaker1, '3DS': content.isInSuperMarioMakerFor3DS, 2: content.isInSuperMarioMaker2,}
 }
