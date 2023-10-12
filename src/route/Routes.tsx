356e7c4dfc31fd36f86662087ad57263194b4d2b
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2023-10-12 16:00:39 -0400

    Addition of the games in a predefined routes available. And less interpreted strings needed for the paths.

--- src/route/Routes.tsx
+++ src/route/Routes.tsx
@@ -21,7 +21,7 @@ const everyRouteInstance = EveryRoutes.CompanionEnum.get.values
 const everyRouteInstanceWithMoreThat1Element = everyRouteInstance.filter(it => it.everyRoute.length !== 1,)
 const everyRouteInstanceWithGameAndViewDisplay = everyRouteInstance.filter(it => it.everyRoute.find(it => it.games == null && it.viewDisplay == null,) == null,)
 const everyRoute = EveryRoutes.CompanionEnum.get.everyRoute
-// const everyGames = Games.GamePossibilitiesCompanion.get.everyFields
+const everyGames = Games.GamePossibilitiesCompanion.get.everyFields
 const everyViewDisplay = ViewDisplays.CompanionEnum.get.values
 /**
  * Every route encapsulated in a hash router (for GitHub).
@@ -73,93 +73,187 @@ const router = createHashRouter([{
         //endregion -------------------- Path from route path --------------------
         //region -------------------- Path from language --------------------
 
-        languages.map<RouteObject>(language => ({
-            path: `/${language.projectAcronym}`,
-            children: [
-                //region -------------------- Path from nothing --------------------
+        languages.map<RouteObject>(language => {
+            const pathFromLanguage = `/${language.projectAcronym}` as const
+            return {
+                path: pathFromLanguage,
+                children: [
+                    //region -------------------- Path from nothing --------------------
 
-                {
-                    path: `/${language.projectAcronym}`,
-                    loader() {
-                        throw redirect(EveryRoutes.HOME.getPath(language, null, null,),)
-                    },
-                },
-
-                //endregion -------------------- Path from nothing --------------------
-                //region -------------------- Path from simple route path --------------------
-
-                everyRouteInstanceWithMoreThat1Element.map<RouteObject>(routeInstance => ({
-                    path: `/${language.projectAcronym}${routeInstance.simplePath}`,
-                    loader() {
-                        throw redirect(routeInstance.getPath(language, null, null,),)
-                    },
-                }),).toArray(),
-
-                //endregion -------------------- Path from simple route path --------------------
-                //region -------------------- Path from route path --------------------
-
-                everyRoute.map<RouteObject>(route => ({
-                    path: `/${language.projectAcronym}${route.path}`,
-                    element: <Suspense fallback={<LoadingApp/>}>{route.renderCallback(route.viewDisplay!, new GameCollection(route.games ?? EMPTY_ARRAY,),)}</Suspense>,
-                    loader: ()=> null,
-                }),),
-
-                //endregion -------------------- Path from route path --------------------
-                //region -------------------- Path from view display --------------------
-
-                everyViewDisplay.map<RouteObject>(viewDisplay => ({
-                    path: `/${language.projectAcronym}/${viewDisplay.urlValue}`,
-                    children: [
-                        //region -------------------- Path from nothing --------------------
-
-                        {
-                            path: `/${language.projectAcronym}/${viewDisplay.urlValue}`,
-                            loader() {
-                                throw redirect(EveryRoutes.HOME.getPath(language, null, viewDisplay,),)
-                            },
+                    {
+                        path: pathFromLanguage,
+                        loader() {
+                            throw redirect(EveryRoutes.HOME.getPath(language, null, null,),)
                         },
+                    },
 
-                        //endregion -------------------- Path from nothing --------------------
-                        //region -------------------- Path from simple route path --------------------
+                    //endregion -------------------- Path from nothing --------------------
+                    //region -------------------- Path from simple route path --------------------
 
-                        everyRouteInstanceWithGameAndViewDisplay.map<RouteObject>(routeInstance => ({
-                            path: `/${language.projectAcronym}/${viewDisplay.urlValue}${routeInstance.simplePath}`,
+                    everyRouteInstanceWithMoreThat1Element.map<RouteObject>(routeInstance => ({
+                        path: `${pathFromLanguage}${routeInstance.simplePath}`,
+                        loader() {
+                            throw redirect(routeInstance.getPath(language, null, null,),)
+                        },
+                    }),).toArray(),
+
+                    //endregion -------------------- Path from simple route path --------------------
+                    //region -------------------- Path from route path --------------------
+
+                    everyRoute.map<RouteObject>(route => ({
+                        path: `${pathFromLanguage}${route.path}`,
+                        element: <Suspense fallback={<LoadingApp/>}>{route.renderCallback(route.viewDisplay!, new GameCollection(route.games ?? EMPTY_ARRAY,),)}</Suspense>,
+                        loader: () => null,
+                    }),),
+
+                    //endregion -------------------- Path from route path --------------------
+                    //region -------------------- Path from game --------------------
+
+                    everyGames.map<RouteObject>(games => {
+                        const pathFromLanguageAndGame = `${pathFromLanguage}/game-${Games.getGroupUrlValue(games,)}` as const
+                        return {
+                            path: pathFromLanguageAndGame,
+                            children: [
+                                //region -------------------- Path from nothing --------------------
+
+                                {
+                                    path: pathFromLanguageAndGame,
+                                    loader() {
+                                        throw redirect(EveryRoutes.HOME.getPath(language, games, null,),)
+                                    },
+                                },
+
+                                //endregion -------------------- Path from nothing --------------------
+                                //region -------------------- Path from view display --------------------
+
+                                everyViewDisplay.map<RouteObject>(viewDisplay => {
+                                    const pathFromLanguageAndGameAndViewDisplay = `${pathFromLanguageAndGame}/${viewDisplay.urlValue}` as const
+                                    return {
+                                        path: pathFromLanguageAndGameAndViewDisplay,
+                                        children: [
+                                            //region -------------------- Path from nothing --------------------
+
+                                            {
+                                                path: pathFromLanguageAndGameAndViewDisplay,
+                                                loader() {
+                                                    throw redirect(EveryRoutes.HOME.getPath(language, games, viewDisplay,),)
+                                                },
+                                            },
+
+                                            //endregion -------------------- Path from nothing --------------------
+                                            //region -------------------- Path from simple route path --------------------
+
+                                            everyRouteInstanceWithGameAndViewDisplay.map<RouteObject>(routeInstance => ({
+                                                path: `${pathFromLanguageAndGameAndViewDisplay}${routeInstance.simplePath}`,
+                                                loader() {
+                                                    throw redirect(routeInstance.getPath(language, games, viewDisplay,),)
+                                                },
+                                            }),).toArray(),
+
+                                            //endregion -------------------- Path from simple route path --------------------
+                                            //region -------------------- Fallback if nothing was found --------------------
+
+                                            {
+                                                path: `${pathFromLanguageAndGameAndViewDisplay}*`,
+                                                loader: loaderArguments => redirectToCorrectPathWithNoRoute2(loaderArguments, language, games, viewDisplay,),
+                                            } satisfies RouteObject,
+
+                                            //endregion -------------------- Fallback if nothing was found --------------------
+                                        ].flat(),
+                                        loader() {
+                                            ViewDisplays.CompanionEnum.get.current = viewDisplay
+                                            return null
+                                        },
+                                    }
+                                },).toArray(),
+
+                                //endregion -------------------- Path from view display --------------------
+                                //region -------------------- Path from simple route path --------------------
+
+                                everyRouteInstanceWithGameAndViewDisplay.map<RouteObject>(routeInstance => ({
+                                    path: `${pathFromLanguageAndGame}${routeInstance.simplePath}`,
+                                    loader() {
+                                        throw redirect(routeInstance.getPath(language, games, null,),)
+                                    },
+                                }),).toArray(),
+
+                                //endregion -------------------- Path from simple route path --------------------
+                                //region -------------------- Fallback if nothing was found --------------------
+
+                                {
+                                    path: `${pathFromLanguageAndGame}*`,
+                                    loader: loaderArguments => redirectToCorrectPathWithNoRoute1(loaderArguments, language, games,),
+                                } satisfies RouteObject,
+
+                                //endregion -------------------- Fallback if nothing was found --------------------
+                            ].flat(),
                             loader() {
-                                throw redirect(routeInstance.getPath(language, null, viewDisplay,),)
+                                Games.setSelected(games,)
+                                return null
                             },
-                        }),).toArray(),
+                        }
+                    },),
 
-                        //endregion -------------------- Path from simple route path --------------------
-                        //region -------------------- Fallback if nothing was found --------------------
+                    //endregion -------------------- Path from game --------------------
+                    //region -------------------- Path from view display --------------------
 
-                        {
-                            path: `/${language.projectAcronym}/${viewDisplay.urlValue}*`,
-                            loader: loaderArguments => redirectToCorrectPathWithNoRouteAndGame2(loaderArguments, language, viewDisplay,),
-                        } satisfies RouteObject,
+                    everyViewDisplay.map<RouteObject>(viewDisplay => {
+                        const pathFromLanguageAndViewDisplay = `${pathFromLanguage}/${viewDisplay.urlValue}` as const
+                        return {
+                            path: pathFromLanguageAndViewDisplay,
+                            children: [
+                                //region -------------------- Path from nothing --------------------
 
-                        //endregion -------------------- Fallback if nothing was found --------------------
-                    ].flat(),
-                    loader() {
-                        ViewDisplays.CompanionEnum.get.current = viewDisplay
-                        return null
-                    }
-                }),).toArray(),
+                                {
+                                    path: pathFromLanguageAndViewDisplay,
+                                    loader() {
+                                        throw redirect(EveryRoutes.HOME.getPath(language, null, viewDisplay,),)
+                                    },
+                                },
 
-                //endregion -------------------- Path from view display --------------------
-                //region -------------------- Fallback if nothing was found --------------------
+                                //endregion -------------------- Path from nothing --------------------
+                                //region -------------------- Path from simple route path --------------------
 
-                {
-                    path: `/${language.projectAcronym}/*`,
-                    loader: loaderArguments => redirectToCorrectPathWithNoRouteAndGame1(loaderArguments, language,),
-                } satisfies RouteObject,
+                                everyRouteInstanceWithGameAndViewDisplay.map<RouteObject>(routeInstance => ({
+                                    path: `/${pathFromLanguageAndViewDisplay}${routeInstance.simplePath}`,
+                                    loader() {
+                                        throw redirect(routeInstance.getPath(language, null, viewDisplay,),)
+                                    },
+                                }),).toArray(),
 
-                //endregion -------------------- Fallback if nothing was found --------------------
-            ].flat(),
-            loader() {
-                ProjectLanguages.CompanionEnum.get.current = language
-                return null
-            },
-        }),),
+                                //endregion -------------------- Path from simple route path --------------------
+                                //region -------------------- Fallback if nothing was found --------------------
+
+                                {
+                                    path: `/${pathFromLanguageAndViewDisplay}*`,
+                                    loader: loaderArguments => redirectToCorrectPathWithNoRouteAndGame2(loaderArguments, language, viewDisplay,),
+                                } satisfies RouteObject,
+
+                                //endregion -------------------- Fallback if nothing was found --------------------
+                            ].flat(),
+                            loader() {
+                                ViewDisplays.CompanionEnum.get.current = viewDisplay
+                                return null
+                            },
+                        }
+                    },).toArray(),
+
+                    //endregion -------------------- Path from view display --------------------
+                    //region -------------------- Fallback if nothing was found --------------------
+
+                    {
+                        path: `${pathFromLanguage}/*`,
+                        loader: loaderArguments => redirectToCorrectPathWithNoRouteAndGame1(loaderArguments, language,),
+                    } satisfies RouteObject,
+
+                    //endregion -------------------- Fallback if nothing was found --------------------
+                ].flat(),
+                loader() {
+                    ProjectLanguages.CompanionEnum.get.current = language
+                    return null
+                },
+            }
+        },),
 
         //endregion -------------------- Path from language --------------------
         //region -------------------- Fallback if nothing was found --------------------
@@ -203,6 +297,43 @@ function redirectToCorrectPathWithNoArguments(loaderArguments: LoaderFunctionArg
  *
  * @param loaderArguments The arguments to retrieve the {@link Request request} {@link Request.url url}
  * @param language The route language
+ * @param games The games in the route
+ * @see redirectToCorrectPathWithEveryArguments
+ * @throws {Response} The route encapsulated in a response
+ */
+function redirectToCorrectPathWithNoRoute1(loaderArguments: LoaderFunctionArgs, language: ProjectLanguages, games: readonly Games[],): never {
+    const url = loaderArguments.request.url
+    throw redirectToCorrectPathWithEveryArguments(
+        EveryRoutes.CompanionEnum.get.getValueInUrl(url,),
+        language,
+        games,
+        ViewDisplays.CompanionEnum.get.getValueInUrl(url,),
+    )
+}
+/**
+ * Redirect to the correct path from a value with only a {@link language}
+ *
+ * @param loaderArguments The arguments to retrieve the {@link Request request} {@link Request.url url}
+ * @param language The route language
+ * @param games The games in the route
+ * @param viewDisplay The view display in the route
+ * @see redirectToCorrectPathWithEveryArguments
+ * @throws {Response} The route encapsulated in a response
+ */
+function redirectToCorrectPathWithNoRoute2(loaderArguments: LoaderFunctionArgs, language: ProjectLanguages, games: readonly Games[], viewDisplay: ViewDisplays,): never {
+    const url = loaderArguments.request.url
+    throw redirectToCorrectPathWithEveryArguments(
+        EveryRoutes.CompanionEnum.get.getValueInUrl(url,),
+        language,
+        games,
+        viewDisplay,
+    )
+}
+/**
+ * Redirect to the correct path from a value with only a {@link language}
+ *
+ * @param loaderArguments The arguments to retrieve the {@link Request request} {@link Request.url url}
+ * @param language The route language
  * @see redirectToCorrectPathWithEveryArguments
  * @throws {Response} The route encapsulated in a response
  */
@@ -220,7 +351,7 @@ function redirectToCorrectPathWithNoRouteAndGame1(loaderArguments: LoaderFunctio
  *
  * @param loaderArguments The arguments to retrieve the {@link Request request} {@link Request.url url}
  * @param language The route language
- * @param viewDisplay The view display
+ * @param viewDisplay The view display in the route
  * @see redirectToCorrectPathWithEveryArguments
  * @throws {Response} The route encapsulated in a response
  */
@@ -238,7 +369,7 @@ function redirectToCorrectPathWithNoRouteAndGame2(loaderArguments: LoaderFunctio
  *
  * @param route The nullable simple route
  * @param language The nullable route language
- * @param viewDisplay The nullable view display
+ * @param viewDisplay The nullable view display in the route
  * @param games The games in the route
  * @throws {Response} The route encapsulated in a response
  */
