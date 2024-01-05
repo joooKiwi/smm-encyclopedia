93654bf4c640484385afaf9b5921c3dfbecbef3c
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-01-05 15:16:01 -0500

    Simplification of the boolean returned for the strict one

--- src/util/collection/GameCollection.ts
+++ src/util/collection/GameCollection.ts
@@ -7,11 +7,11 @@ export class GameCollection<const T extends Games = Games, const REFERENCE exten
 
     //region -------------------- Fields --------------------
 
-    #hasAllGames?: & GameWithSMM1<T> & GameWithSMM3DS<T> & GameWithSMM2<T>
-    #hasSMM1?: GameWithSMM1<T>
-    #hasSMM3DS?: GameWithSMM3DS<T>
-    #hasSMM1Or3DS?: & GameWithSMM1<T> & GameWithSMM3DS<T>
-    #hasSMM2?: GameWithSMM2<T>
+    #hasAllGames?: boolean
+    #hasSMM1?: boolean
+    #hasSMM3DS?: boolean
+    #hasSMM1Or3DS?: boolean
+    #hasSMM2?: boolean
 
     //endregion -------------------- Fields --------------------
     //region -------------------- Getter methods --------------------
@@ -20,34 +20,30 @@ export class GameCollection<const T extends Games = Games, const REFERENCE exten
      * The collection has every game ({@link Games.SUPER_MARIO_MAKER_1 SMM1},
      * {@link Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS SMM3DS} & {@link Games.SUPER_MARIO_MAKER_2 SMM2}) type in its values
      */
-    public get hasAllGames(): & GameWithSMM1<T> & GameWithSMM3DS<T> & GameWithSMM2<T> {
-        return this.#hasAllGames = this.hasAll(Games.SUPER_MARIO_MAKER_1, Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS, Games.SUPER_MARIO_MAKER_2,) as & GameWithSMM1<T> & GameWithSMM3DS<T> & GameWithSMM2<T>
+    public get hasAllGames(): boolean {
+        return this.#hasAllGames = this.hasAll(Games.SUPER_MARIO_MAKER_1, Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS, Games.SUPER_MARIO_MAKER_2,)
     }
 
     /** The collection has the {@link Games.SUPER_MARIO_MAKER_1 SMM1} type in its values */
-    public get hasSMM1(): GameWithSMM1<T> {
-        return this.#hasSMM1 ??= this.hasOne(Games.SUPER_MARIO_MAKER_1) as GameWithSMM1<T>
+    public get hasSMM1(): boolean {
+        return this.#hasSMM1 ??= this.hasOne(Games.SUPER_MARIO_MAKER_1)
     }
 
     /** The collection has the {@link Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS SMM3DS} type in its values */
-    public get hasSMM3DS(): GameWithSMM3DS<T> {
-        return this.#hasSMM3DS ??= this.hasOne(Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS) as GameWithSMM3DS<T>
+    public get hasSMM3DS(): boolean {
+        return this.#hasSMM3DS ??= this.hasOne(Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS)
     }
 
     /** The collection has the {@link Games.SUPER_MARIO_MAKER_1 SMM1} or {@link Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS SMM3DS} type in its values */
-    public get hasSMM1Or3DS(): & GameWithSMM1<T> & GameWithSMM3DS<T> {
-        return this.#hasSMM1Or3DS ??= this.hasOne(Games.SUPER_MARIO_MAKER_1, Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS,) as & GameWithSMM1<T> & GameWithSMM3DS<T>
+    public get hasSMM1Or3DS(): boolean {
+        return this.#hasSMM1Or3DS ??= this.hasOne(Games.SUPER_MARIO_MAKER_1, Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS,)
     }
 
     /** The collection has the {@link Games.SUPER_MARIO_MAKER_2 SMM2} type in its values */
-    public get hasSMM2(): GameWithSMM2<T> {
-        return this.#hasSMM2 ??= this.hasOne(Games.SUPER_MARIO_MAKER_2) as GameWithSMM2<T>
+    public get hasSMM2(): boolean {
+        return this.#hasSMM2 ??= this.hasOne(Games.SUPER_MARIO_MAKER_2)
     }
 
     //endregion -------------------- Getter methods --------------------
 
 }
-
-type GameWithSMM1<T extends Games, > = T extends typeof Games['SUPER_MARIO_MAKER_1'] ? true : false
-type GameWithSMM3DS<T extends Games, > = T extends typeof Games['SUPER_MARIO_MAKER_FOR_NINTENDO_3DS'] ? true : false
-type GameWithSMM2<T extends Games, > = T extends typeof Games['SUPER_MARIO_MAKER_2'] ? true : false
