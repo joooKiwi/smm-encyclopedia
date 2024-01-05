329e3d510b06b2f8d38745c677518e25b87da2f1
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-01-05 16:51:18 -0500

    Removal of the type on the SimpleRoute since it is used nowhere

--- src/route/SimpleRoute.ts
+++ src/route/SimpleRoute.ts
@@ -11,23 +11,20 @@ import type {RouteCallback} from 'route/EveryRoutes.types'
  *
  * @see EveryRoutes
  */
-export class SimpleRoute<const out NAME extends string = string,
-    const out PATH extends string = string,
-    const out GAMES extends NullOr<readonly Games[]> = NullOr<readonly Games[]>,
-    const out VIEW_DISPLAY extends NullOr<ViewDisplays> = NullOr<ViewDisplays>, > {
+export class SimpleRoute {
 
     //region -------------------- Fields --------------------
 
     readonly #name
     readonly #path
-    readonly #viewDisplay: VIEW_DISPLAY//FIXME this type is only there to help typescript (it's not the standard)
-    readonly #games: GAMES//FIXME this type is only there to help typescript (it's not the standard)
+    readonly #viewDisplay
+    readonly #games
     readonly #renderCallback
 
     //endregion -------------------- Fields --------------------
     //region -------------------- Constructor --------------------
 
-    public constructor(name: NAME, path: PATH, games: GAMES, viewDisplay: VIEW_DISPLAY, renderCallback: RouteCallback,) {
+    public constructor(name: string, path: string, games: NullOr<readonly Games[]>, viewDisplay: NullOr<ViewDisplays>, renderCallback: RouteCallback,) {
         this.#name = name
         this.#path = path
         this.#viewDisplay = viewDisplay
@@ -38,19 +35,19 @@ export class SimpleRoute<const out NAME extends string = string,
     //endregion -------------------- Constructor --------------------
     //region -------------------- Getter methods --------------------
 
-    public get name(): NAME {
+    public get name(): string {
         return this.#name
     }
 
-    public get path(): PATH {
+    public get path(): string {
         return this.#path
     }
 
-    public get viewDisplay(): VIEW_DISPLAY {
+    public get viewDisplay(): NullOr<ViewDisplays> {
         return this.#viewDisplay
     }
 
-    public get games(): GAMES {
+    public get games(): NullOr<readonly Games[]> {
         return this.#games
     }
 
