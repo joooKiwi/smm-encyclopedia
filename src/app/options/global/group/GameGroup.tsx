cae12009a059f81ce51c46aaf2f7141d8fd740d8
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-01-17 01:44:10 -0500

    Simple change to the game group to be similar to the game style group

--- src/app/options/global/group/GameGroup.tsx
+++ src/app/options/global/group/GameGroup.tsx
@@ -4,6 +4,7 @@ import {Link, useLocation} from 'react-router-dom'
 
 import type {PossibleRoutePath} from 'route/EveryRoutes.types'
 import type {ReactProperties}   from 'util/react/ReactProperties'
+import type {GameCollection}    from 'util/collection/GameCollection'
 
 import {BootstrapInstanceHandler} from 'bootstrap/BootstrapInstanceHandler'
 import {Games}                    from 'core/game/Games'
@@ -14,40 +15,39 @@ interface GameLinkProperties
 
     readonly game: Games
 
-    readonly gamesInUrl: readonly Games[]
+    readonly selected: GameCollection
 
 }
 
 /** @reactComponent */
 export default function GameGroup() {
-    const pathname = useLocation().pathname
-    const gamesInUrl = Games.CompanionEnum.get.getValueInUrl(pathname,)
+    const gamesInUrl = Games.CompanionEnum.get.selected
 
-    return <div key={`game option container`} id={`games-option-container`} className="btn-group" role="group">
-        <GameLink game={Games.SUPER_MARIO_MAKER_1} gamesInUrl={gamesInUrl}/>
-        <GameLink game={Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS} gamesInUrl={gamesInUrl}/>
-        <GameLink game={Games.SUPER_MARIO_MAKER_2} gamesInUrl={gamesInUrl}/>
+    return <div key="option container (Game)" id="games-option-container" className="btn-group" role="group">
+        <GameLink game={Games.SUPER_MARIO_MAKER_1} selected={gamesInUrl}/>
+        <GameLink game={Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS} selected={gamesInUrl}/>
+        <GameLink game={Games.SUPER_MARIO_MAKER_2} selected={gamesInUrl}/>
     </div>
 }
 
 /** @reactComponent */
-function GameLink({game, gamesInUrl,}: GameLinkProperties,) {
-    const {pathname,} = useLocation()
+function GameLink({game, selected,}: GameLinkProperties,) {
+    const pathname = useLocation().pathname
 
     const id = `gameLink-${game.englishNameInHtml}-button`
 
-    if (gamesInUrl.length === 0) //FIXME: Find a better way to handle this. This is a temporary solution to handle empty arrays
+    if (selected.length === 0) //FIXME: Find a better way to handle this. This is a temporary solution to handle empty arrays
         return <button type="button" id={id} className="btn btn-secondary link-button disabled">
             {game.renderSingleComponent}
         </button>
 
-    if (game.isSelected && gamesInUrl.length === 1)
+    if (game.isSelected && selected.length === 1)
         return <button type="button" id={id} className="btn btn-secondary link-button disabled">
             {game.renderSingleComponent}
         </button>
 
-    const gamesAsUrl = Games.getGroupUrlValue(gamesInUrl,)
-    const gamesFromSelectionAsUrl = game.isSelected && gamesInUrl.length === 1 ? gamesAsUrl : Games.getGroupUrlValue(game.isSelected ? gamesInUrl.filter(it => it !== game) : [...gamesInUrl, game,],)
+    const gamesAsUrl = Games.getGroupUrlValue(selected,)
+    const gamesFromSelectionAsUrl = game.isSelected && selected.length === 1 ? gamesAsUrl : Games.getGroupUrlValue(game.isSelected ? selected.filter(it => it !== game) : [...selected, game,],)
     const newPath = pathname.replace(`game-${gamesAsUrl}`, `game-${gamesFromSelectionAsUrl}`,) as PossibleRoutePath
 
     return <Link type="button" id={id} className={`btn btn-${game.isSelected ? '' : 'outline-'}secondary link-button`} to={newPath}
