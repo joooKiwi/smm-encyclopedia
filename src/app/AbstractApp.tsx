c662ae5b23c4be8803a466e2bf758536c2d6db7e
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-01-22 22:33:15 -0500

    Relocation of the parameter modal into its own independent component

--- src/app/AbstractApp.tsx
+++ src/app/AbstractApp.tsx
@@ -6,14 +6,13 @@ import type {AppProperties}  from 'app/AppProperties.types'
 import type {AppStates}      from 'app/AppStates.types'
 import type {ReactComponent} from 'util/react/ReactComponent'
 
-import GlobalOptionComponent from 'app/options/global/GlobalOption.component'
-import Footer                from 'navigation/Footer'
-import Navigation            from 'navigation/Navigation'
-import LanguageModal         from 'navigation/modal/LanguageModal'
-import ParameterModal        from 'navigation/modal/ParameterModal'
-import DisplayViewModal      from 'navigation/modal/DisplayViewModal'
-import SearchModal           from 'navigation/modal/SearchModal'
-import ColorModal            from 'navigation/modal/ColorModal'
+import Footer           from 'navigation/Footer'
+import Navigation       from 'navigation/Navigation'
+import LanguageModal    from 'navigation/modal/LanguageModal'
+import ParameterModal   from 'navigation/modal/ParameterModal'
+import DisplayViewModal from 'navigation/modal/DisplayViewModal'
+import SearchModal      from 'navigation/modal/SearchModal'
+import ColorModal       from 'navigation/modal/ColorModal'
 
 /** @reactComponent */
 export default abstract class AbstractApp<const out T extends AppProperties = AppProperties,
@@ -23,15 +22,11 @@ export default abstract class AbstractApp<const out T extends AppProperties = Ap
 
     protected abstract _mainContent(): ReactElement
 
-    protected _parameterContent(): NonNullable<ReactElement> {
-        return <GlobalOptionComponent/>
-    }
-
     public override render(): ReactJSXElement {
         return <>
             <aside id="modal-container">
                 <LanguageModal/>
-                <ParameterModal reference={this._parameterContent()}/>
+                <ParameterModal/>
                 <DisplayViewModal/>
                 <SearchModal/>
                 <ColorModal/>
--- src/navigation/modal/ParameterModal.tsx
+++ src/navigation/modal/ParameterModal.tsx
@@ -1,20 +1,12 @@
 import {useRef} from 'react'
 
-import type {ReactProperties} from 'util/react/ReactProperties'
-
-import Modal                from 'bootstrap/modal/Modal'
-import {contentTranslation} from 'lang/components/translationMethods'
-import {PARAMETER_MODAL_ID} from 'navigation/button/modalIds'
-
-interface ParameterModalProperties
-    extends ReactProperties {
-
-    reference: NonNullable<ReactElement>
-
-}
+import GlobalOptionComponent from 'app/options/global/GlobalOption.component'
+import Modal                 from 'bootstrap/modal/Modal'
+import {contentTranslation}  from 'lang/components/translationMethods'
+import {PARAMETER_MODAL_ID}  from 'navigation/button/modalIds'
 
 /** @reactComponent */
-export default function ParameterModal({reference}: ParameterModalProperties,) {
+export default function ParameterModal() {
     const modal = useRef<HTMLDivElement>(null,)
 
     return <>
@@ -26,7 +18,7 @@ export default function ParameterModal({reference}: ParameterModalProperties,) {
                         <h4 className="modal-title w-100 text-center">{contentTranslation('Options')}</h4>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label={contentTranslation('Close')}/>
                     </div>
-                    <div className="modal-body">{reference}</div>
+                    <div className="modal-body"><GlobalOptionComponent/></div>
                     {/*<div className="modal-footer">*/}
                     {/*    <button type="button" className="btn btn-success">{contentTranslation('Confirm')}</button>*/}
                     {/*    <button type="button" className="btn btn-danger" data-bs-dismiss="modal">{contentTranslation('Cancel')}</button>*/}
