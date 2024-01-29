6e324bca75de5dc4294e53da7507db9993d79a85
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-01-29 17:12:45 -0500

    Change to the date container to have two different inputs, one from a date and another from the year, month and day

--- src/lang/date/SimpleDate.tsx
+++ src/lang/date/SimpleDate.tsx
@@ -5,15 +5,18 @@ import type {ReactProperties} from 'util/react/ReactProperties'
 
 import {DateDayLanguages} from 'lang/date/DateDayLanguages'
 
-export interface DateTimeFormatProperties
+interface DateTimeFormatFromSeparatedValuesProperties
     extends ReactProperties {
 
-    year: number
+    readonly year: number
+    readonly month: MonthNumber
+    readonly day: DayNumber
 
-    month: MonthNumber
+}
 
-    day: DayNumber
-
+interface DateTimeFormatFromDateProperties
+    extends ReactProperties {
+    readonly date: Date
 }
 
 /**
@@ -23,15 +26,19 @@ export interface DateTimeFormatProperties
  * @reactComponent
  * @see https://formatjs.io/docs/react-intl/components#formatteddate
  */
-export default function SimpleDate({year, month, day,}: DateTimeFormatProperties,) {
-    const date = new Date(year, month - 1, day,)
+export default function SimpleDate(properties: | DateTimeFormatFromSeparatedValuesProperties | DateTimeFormatFromDateProperties,) {
+    if ('date' in properties)
+        return <SimpleDateFromDate date={properties.date}/>
+    return <SimpleDateFromDate date={new Date(properties.year, properties.month - 1, properties.day,)}/>
+}
 
+function SimpleDateFromDate({date,}: DateTimeFormatFromDateProperties,) {
     return <FormattedDateParts
         value={date}
         year="numeric"
         month="long"
         day="numeric">
-        {parts => <>{parts.map(({type, value,}) => type === 'day' ? <Fragment key={`${date} - ${type}`}>{DateDayLanguages.CompanionEnum.get.current.newDayComponent(Number(value,) as DayNumber,)}</Fragment> : value)}</>}
+        {parts => <>{parts.map(({type, value,}) => type === 'day' ?
+            <Fragment key={`${date} - ${type}`}>{DateDayLanguages.CompanionEnum.get.current.newDayComponent(Number(value,) as DayNumber,)}</Fragment> : value)}</>}
     </FormattedDateParts>
 }
-
