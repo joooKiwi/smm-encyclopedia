d764720b2dfcce3ac343fbca52c59c3f54acd97c
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-01 23:03:27 -0500

    Elevation of the property into the "mii costume" class itself for better debugging experience

--- src/core/miiCostume/MiiCostume.loader.ts
+++ src/core/miiCostume/MiiCostume.loader.ts
@@ -5,15 +5,18 @@ import type {MiiCostume}                                                        
 import type {PossibleEnglishName}                                                           from 'core/miiCostume/MiiCostumes.types'
 import type {PossibleMarioMakerVersion}                                                     from 'core/miiCostume/loader.types'
 import type {PossibleEnglishName as PossibleEnglishName_Category}                           from 'core/miiCostumeCategory/MiiCostumeCategories.types'
+import type {MiiCostumeCategory}                                                            from 'core/miiCostumeCategory/MiiCostumeCategory'
 import type {PossibleEnglishNameWithOnlyAmount as PossibleEnglishName_OfficialNotification} from 'core/officialNotification/OfficialNotifications.types'
+import type {CompanionEnumByName}                                                           from 'util/enumerable/companion/CompanionEnumByName'
 import type {Loader}                                                                        from 'util/loader/Loader'
 
-import {isInProduction}                    from 'variables'
-import {MiiCostumeContainer}               from 'core/miiCostume/MiiCostume.container'
-import {MiiCostumeCategoryLoader}          from 'core/miiCostumeCategory/MiiCostumeCategory.loader'
-import {OfficialNotificationHolderBuilder} from 'core/officialNotification/holder/OfficialNotificationHolder.builder'
-import {Versions}                          from 'core/version/Versions'
-import {createNameFromContent}             from 'lang/name/createNameFromContent'
+import {isInProduction}           from 'variables'
+import {MiiCostumeContainer}      from 'core/miiCostume/MiiCostume.container'
+import {MiiCostumeCategoryLoader} from 'core/miiCostumeCategory/MiiCostumeCategory.loader'
+import {OfficialNotifications}    from 'core/officialNotification/OfficialNotifications'
+import {Versions}                 from 'core/version/Versions'
+import {createNameFromContent}    from 'lang/name/createNameFromContent'
+import {NUMBER_ONLY_REGEX, SPACE} from 'util/commonVariables'
 
 /**
  * @dependsOn<{@link MiiCostumeCategoryLoader}>
@@ -64,19 +67,38 @@ export class MiiCostumeLoader
 interface Content
     extends LanguageContent {
 
-    notificationIfUnlocked: NullOr<PossibleEnglishName_OfficialNotification>
+    readonly notificationIfUnlocked: NullOr<PossibleEnglishName_OfficialNotification>
 
-    MM2_version: PossibleMarioMakerVersion
-    category: PossibleEnglishName_Category
+    readonly MM2_version: PossibleMarioMakerVersion
+    readonly category: PossibleEnglishName_Category
 
 }
 
 function createReference(content: Content,): MiiCostume {
     const version = content.MM2_version
+    const notificationIfUnlocked = content.notificationIfUnlocked
 
+    if (notificationIfUnlocked == null)
+        return new MiiCostumeContainer(
+            createNameFromContent(content, 2, true,),
+            null, null,
+            version == null ? null : Versions.CompanionEnum.get.getValueByName(`v${version}`,),
+            MiiCostumeCategoryLoader.get.load().get(content.category,)!,
+        )
+
+    const officialNotification = OfficialNotifications.CompanionEnum.get.getValueByName(notificationIfUnlocked,)
+    if (officialNotification === OfficialNotifications.RECEIVE_A_LOT_OF_FEEDBACK_1 || officialNotification === OfficialNotifications.RECEIVE_A_LOT_OF_FEEDBACK_2)
+        return new MiiCostumeContainer(
+            createNameFromContent(content, 2, true,),
+            officialNotification, null,
+            version == null ? null : Versions.CompanionEnum.get.getValueByName(`v${version}`,),
+            MiiCostumeCategoryLoader.get.load().get(content.category,)!,
+        )
+
+    const numberFoundInOfficialNotificationFound = notificationIfUnlocked.split(SPACE,).find(value => NUMBER_ONLY_REGEX.test(value,),)
     return new MiiCostumeContainer(
         createNameFromContent(content, 2, true,),
-        new OfficialNotificationHolderBuilder(content.notificationIfUnlocked,).build(),
+        officialNotification, numberFoundInOfficialNotificationFound == null ? null : Number(numberFoundInOfficialNotificationFound,),
         version == null ? null : Versions.CompanionEnum.get.getValueByName(`v${version}`,),
         MiiCostumeCategoryLoader.get.load().get(content.category,)!,
     )
--- src/core/officialNotification/holder/OfficialNotificationHolder.ts
+++
@@ -1,9 +0,0 @@
-import type {OfficialNotifications} from 'core/officialNotification/OfficialNotifications'
-import type {ClassWithAmount}       from 'util/ClassWithAmount'
-
-export interface OfficialNotificationHolder
-    extends ClassWithAmount {
-
-    get officialNotification(): NullOr<OfficialNotifications>
-
-}
