95a545c1f8801c6a0bf2bf93ae1132aef33b678f
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-03 01:50:55 -0500

    Addition of some recurrent tests into common helper methods

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -6,16 +6,42 @@ import '@testing-library/jest-dom'
 import 'jest-extended/all'
 
 expect.extend({
-    toBeBooleanOrNull(content) {
+    toBeBooleanOrNull(content: unknown,) {
         if (Object.is(content, null,) || Object.is(content, true,) || Object.is(content, false,))
-            return {
-                message: () => `expected "${content}" to not be a boolean or null`,
-                pass: true,
-            }
+            return {message: () => `expected "${content}" to not be a boolean or null`, pass: true,}
         else
-            return {
-                message: () => `expected "${content}" to be a boolean or null`,
-                pass: false,
-            }
+            return {message: () => `expected "${content}" to be a boolean or null`, pass: false,}
     },
-})
+    toBeBooleanOrNotApplicable(content: unknown,) {
+        if (Object.is(content, true,) || Object.is(content, false,) || Object.is(content, 'N/A',))
+            return {message: () => `expected "${content}" to not be a boolean or N/A`, pass: true,}
+        else
+            return {message: () => `expected "${content}" to be a boolean or N/A`, pass: false,}
+    },
+    toBeBooleanOrUnknown(content: unknown,) {
+        if (Object.is(content, true,) || Object.is(content, false,) || Object.is(content, '?',))
+            return {message: () => `expected "${content}" to not be a boolean or ?`, pass: true,}
+        else
+            return {message: () => `expected "${content}" to be a boolean or ?`, pass: false,}
+    },
+
+    toBeBooleanOrNullOrNotApplicable(content: unknown,) {
+        if (Object.is(content, null,) || Object.is(content, true,) || Object.is(content, false,) || Object.is(content, 'N/A',))
+            return {message: () => `expected "${content}" to not be a boolean, null or N/A`, pass: true,}
+        else
+            return {message: () => `expected "${content}" to be a boolean, null or N/A`, pass: false,}
+    },
+    toBeBooleanOrNullOrUnknown(content: unknown,) {
+        if (Object.is(content, null,) || Object.is(content, true,) || Object.is(content, false,) || Object.is(content, '?',))
+            return {message: () => `expected "${content}" to not be a boolean, null or ?`, pass: true,}
+        else
+            return {message: () => `expected "${content}" to be a boolean, null or ?`, pass: false,}
+    },
+
+    toBeBooleanOrNullOrNotApplicableOrUnknown(content: unknown,) {
+        if (Object.is(content, null,) || Object.is(content, true,) || Object.is(content, false,) || Object.is(content, '?',) || Object.is(content, 'N/A',))
+            return {message: () => `expected "${content}" to not be a boolean, null, N/A or ?`, pass: true,}
+        else
+            return {message: () => `expected "${content}" to be a boolean, null, N/A or ?`, pass: false,}
+    },
+},)
