00ead74a12c7c7f28ace906eb7eac4b5fa094348
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-26 11:58:47 -0500

    Simple change to use the resources more efficiently

--- src/lang/languageHook.ts
+++ src/lang/languageHook.ts
@@ -2,9 +2,11 @@ import {useState} from 'react'
 
 import {EveryLanguages} from 'lang/EveryLanguages'
 
+const LanguageCompanion = EveryLanguages.CompanionEnum.get
+
 /** @reactHook */
 export function useCurrentLanguage(key: string,): NullOr<EveryLanguages> {
-    const [currentValue, setCurrentValue,] = useState(EveryLanguages.CompanionEnum.get.currentOrNull,)
-    EveryLanguages.CompanionEnum.get.setOnCurrentEvent(key, setCurrentValue,)
+    const [currentValue, setCurrentValue,] = useState(LanguageCompanion.currentOrNull,)
+    LanguageCompanion.setOnCurrentEvent(key, setCurrentValue,)
     return currentValue
 }
