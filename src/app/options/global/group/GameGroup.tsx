04162eb03b734fa13d09415bf8336815e52c0077
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-02-26 15:43:10 -0500

    Change to the global group to use the current values (by hook) instead of the selected one

--- src/app/options/global/group/GameGroup.tsx
+++ src/app/options/global/group/GameGroup.tsx
@@ -1,12 +1,13 @@
 import './GameGroup.scss'
 
-import {Link, useLocation} from 'react-router-dom'
+import type {CollectionHolder} from '@joookiwi/collection'
+import {Link, useLocation}     from 'react-router-dom'
 
 import type {ReactProperties} from 'util/react/ReactProperties'
-import type {GameCollection}  from 'util/collection/GameCollection'
 
 import {BootstrapInstanceHandler} from 'bootstrap/BootstrapInstanceHandler'
 import {Games}                    from 'core/game/Games'
+import {useCurrentGames}          from 'core/game/gamesHook'
 import {ProjectLanguages}         from 'lang/ProjectLanguages'
 import {PARAMETER_MODAL_ID}       from 'navigation/button/modalIds'
 
@@ -15,18 +16,22 @@ interface GameLinkProperties
 
     readonly game: Games
 
-    readonly selected: GameCollection
+    readonly selected: CollectionHolder<Games>
 
 }
 
 /** @reactComponent */
 export default function GameGroup() {
-    const gamesInUrl = Games.CompanionEnum.get.selected
+    const games = useCurrentGames('game group',)
+    if (games == null)
+        return null
+    if (games.isEmpty)
+        return null
 
-    return <div key="option container (Game)" id="games-option-container" className="btn-group" role="group">
-        <GameLink game={Games.SUPER_MARIO_MAKER_1} selected={gamesInUrl}/>
-        <GameLink game={Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS} selected={gamesInUrl}/>
-        <GameLink game={Games.SUPER_MARIO_MAKER_2} selected={gamesInUrl}/>
+    return <div key={`option container (Games = ${games.toString()})`} id="games-option-container" className="btn-group" role="group">
+        <GameLink game={Games.SUPER_MARIO_MAKER_1} selected={games}/>
+        <GameLink game={Games.SUPER_MARIO_MAKER_FOR_NINTENDO_3DS} selected={games}/>
+        <GameLink game={Games.SUPER_MARIO_MAKER_2} selected={games}/>
     </div>
 }
 
--- src/app/options/global/group/GameStyleGroup.tsx
+++ src/app/options/global/group/GameStyleGroup.tsx
@@ -1,39 +1,44 @@
 import './GameStyleGroup.scss'
 
-import {Link, useLocation} from 'react-router-dom'
+import type {CollectionHolder} from '@joookiwi/collection'
+import {Link, useLocation}     from 'react-router-dom'
 
-import type {GameStyleCollection} from 'util/collection/GameStyleCollection'
 import type {ReactProperties}     from 'util/react/ReactProperties'
 
 import {BootstrapInstanceHandler} from 'bootstrap/BootstrapInstanceHandler'
 import {GameStyles}               from 'core/gameStyle/GameStyles'
+import {useCurrentGameStyles}     from 'core/gameStyle/gameStylesHook'
 import {Games}                    from 'core/game/Games'
 import {ProjectLanguages}         from 'lang/ProjectLanguages'
 import {PARAMETER_MODAL_ID}       from 'navigation/button/modalIds'
 
 /** @reactComponent */
 export default function GameStyleGroup() {
-    const selected = GameStyles.CompanionEnum.get.selected
+    const gameStyles = useCurrentGameStyles('game style group',)
+    if (gameStyles == null)
+        return null
+    if (gameStyles.isEmpty)
+        return null
     const isSmm2Selected = Games.SUPER_MARIO_MAKER_2.isSelected
 
     return <div key="option container (Game styles)" id="gameStyles-option-container">
         <div className="btn-group-vertical d-lg-none">
             <div className="btn-group" role="group">
-                <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_BROS} selected={selected}/>
-                <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_BROS_3} selected={selected}/>
-                <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_WORLD} selected={selected}/>
+                <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_BROS} selected={gameStyles}/>
+                <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_BROS_3} selected={gameStyles}/>
+                <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_WORLD} selected={gameStyles}/>
             </div>
             <div className="btn-group" role="group">
-                <GameStyleLink gameStyle={GameStyles.NEW_SUPER_MARIO_BROS_U} selected={selected}/>
-                <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_3D_WORLD} selected={selected} disabled={!isSmm2Selected}/>
+                <GameStyleLink gameStyle={GameStyles.NEW_SUPER_MARIO_BROS_U} selected={gameStyles}/>
+                <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_3D_WORLD} selected={gameStyles} disabled={!isSmm2Selected}/>
             </div>
         </div>
         <div className="btn-group d-none d-lg-inline-flex" role="group">
-            <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_BROS} selected={selected}/>
-            <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_BROS_3} selected={selected}/>
-            <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_WORLD} selected={selected}/>
-            <GameStyleLink gameStyle={GameStyles.NEW_SUPER_MARIO_BROS_U} selected={selected}/>
-            <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_3D_WORLD} selected={selected} disabled={!isSmm2Selected}/>
+            <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_BROS} selected={gameStyles}/>
+            <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_BROS_3} selected={gameStyles}/>
+            <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_WORLD} selected={gameStyles}/>
+            <GameStyleLink gameStyle={GameStyles.NEW_SUPER_MARIO_BROS_U} selected={gameStyles}/>
+            <GameStyleLink gameStyle={GameStyles.SUPER_MARIO_3D_WORLD} selected={gameStyles} disabled={!isSmm2Selected}/>
         </div>
     </div>
 }
@@ -46,7 +51,7 @@ interface GameStyleLinkProperties
 
     readonly disabled?: boolean
 
-    readonly selected: GameStyleCollection
+    readonly selected: CollectionHolder<GameStyles>
 
 }
 
@@ -68,7 +73,7 @@ function GameStyleLink({gameStyle, disabled = false, selected,}: GameStyleLinkPr
         newPath = pathname.replace(`game-style-${GameStyleCompanion.getGroupUrlValue(selected,)}`, pathToReplace,)
     else if (pathname.includes('game-1',) || pathname.includes('game-3ds',) || pathname.includes('game-2',)) {
         const GameCompanion = Games.CompanionEnum.get
-        const gamesInPath = `/game-${GameCompanion.getGroupUrlValue(GameCompanion.selected,)}` as const
+        const gamesInPath = `/game-${GameCompanion.getGroupUrlValue(GameCompanion.current,)}` as const
         newPath = pathname.replace(gamesInPath, `${gamesInPath}/${pathToReplace}`,)
     } else {
         const languagesInPath = `/${ProjectLanguages.CompanionEnum.get.current.urlValue}` as const
