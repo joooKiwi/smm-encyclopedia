018ab006af04d5c91e2a5b7c6442c6afec65804c
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-03-02 18:08:06 -0500

    Addition of utility components for a text or a link depending on the route name

---
+++ src/app/tools/text/TextOrLink.tsx
@@ -0,0 +1,26 @@
+import {Link} from 'react-router-dom'
+
+import type {PossibleRouteName}                            from 'route/EveryRoutes.types'
+import type {ReactProperties, ReactPropertiesWithChildren} from 'util/react/ReactProperties'
+
+import {routeFromName} from 'route/route'
+
+interface TextOrLinkProperties
+    extends ReactProperties {
+
+    readonly id: string
+
+    readonly routeName: Nullable<PossibleRouteName>
+
+}
+
+/**
+ * Create a {@link Link} or a text if the {@link TextOrLinkProperties.routeName route name} is <b>null</b>
+ *
+ * @reactComponent
+ */
+export default function TextOrLink({id, routeName, children,}: ReactPropertiesWithChildren<TextOrLinkProperties, ReactElementOrStringOrArray>,) {
+    if (routeName == null)
+        return <span id={id}>{children}</span>
+    return <Link id={id} to={routeFromName(routeName,)}>{children}</Link>
+}
