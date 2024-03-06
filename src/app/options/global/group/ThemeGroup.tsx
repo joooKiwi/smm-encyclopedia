239613679f4d1dd38e2f7b7abe1d738c15b2a84a
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-03-06 10:56:48 -0500

    Elevation of the theme and time group into a functional component

--- src/app/options/global/group/ThemeGroup.tsx
+++ src/app/options/global/group/ThemeGroup.tsx
@@ -1,57 +1,61 @@
 import type {GlobalAppOption}   from 'app/options/global/GlobalAppOption'
 import type {GlobalThemeOption} from 'app/options/global/GlobalThemeOption'
-import type {OnClickCallback}   from 'app/options/global/group/Group.types'
+import type {GroupProperties}   from 'app/options/global/group/Group.types'
 import type {Themes}            from 'core/theme/Themes'
 
-import AbstractGroup from 'app/options/global/group/AbstractGroup'
-import Image         from 'app/tools/images/Image'
-import {Times}       from 'core/time/Times'
+import Image   from 'app/tools/images/Image'
+import {Times} from 'core/time/Times'
 
-/** @reactComponent */
-export default class ThemeGroup
-    extends AbstractGroup<Themes, GlobalThemeOption> {
+/**
+ * @todo Change the theme group to not be global
+ * @todo Change the theme group to be like the game and game style group
+ * @reactComponent
+ */
+export default function ThemeGroup({isHidden = false, elements,}: GroupProperties<Themes, GlobalThemeOption>,) {
+    if (isHidden)
+        return null
+    return <div key={`option container (themes)`} id="themes-option-container" className="container-fluid">{
+        elements.map(([element, option, isDisabled, isHidden, onClickCallback = null,]) => {
+            if (isHidden)
+                return null
 
-    protected override _renderElement(element: Themes, option: GlobalAppOption<GlobalThemeOption>, [hasSMM2Selected, isDisabledNight,]: readonly [boolean, boolean,], onClickCallback: NullOr<OnClickCallback>,): ReactElement {
-        return <div key={`option container (${element.englishName})`} id={`${element.englishNameInHtml}-option-container`} className="btn-group-vertical" role="group">{
-            hasSMM2Selected
-                ? ThemeGroup.#renderElementWithDayAndNight(element, option, isDisabledNight,)
-                : ThemeGroup.#renderElementWithOnlyDay(element, option,)
-        }</div>
-    }
-
-
-    static #renderThemeImage(element: Themes, value: boolean, isDisabled: boolean, onClickCallback: () => void,): ReactElement {
-        return <Image key={`option image (${element.englishName})`} id={`${element.englishNameInHtml}-option-image`}
-                    className={`btn btn${value ? '' : '-outline'}-secondary ${isDisabled ? 'disabled' : ''}`}
-                    file={element.smallImageFile} alt={`option - ${element.englishName}`}
-                    onClick={onClickCallback}/>
-    }
-
-    static #renderElementWithOnlyDay(element: Themes, option: GlobalAppOption<GlobalThemeOption>,): ReactElement {
-        const optionValue = option.get
-        const value = optionValue.dayValue
-
-        return this.#renderThemeImage(element, value, false, () => option.set(optionValue.reverseDayValue),)
-    }
-
-    static #renderElementWithDayAndNight(element: Themes, option: GlobalAppOption<GlobalThemeOption>, isDisabledNight: boolean,): ReactElement {
-        const optionValue = option.get
-        const [value, timeValues,] = [optionValue.dayValue || optionValue.nightValue,
-            [
-                [Times.DAY, optionValue.dayValue, false, () => option.set(optionValue.reverseDayValue),],
-                [Times.NIGHT, optionValue.nightValue, isDisabledNight, () => option.set(optionValue.reverseNightValue),],
-            ],
-        ] as const
-
-        return <>
-            {this.#renderThemeImage(element, value, isDisabledNight, () => option.set(optionValue.set(value)),)}
-            <div key={`option time image (${element.englishName})`} id={`${element.englishNameInHtml}-option-time-image`} className="btn-group btn-group-sm">{
-                timeValues.map(([time, value, isDisabled, callback,]) =>
-                    <Image key={`option image (${element.englishName} - ${time.englishName})`} id={`${element.englishNameInHtml}-${time.englishNameInHtml}-option-image`}
-                         className={`btn btn${value ? '' : '-outline'}-secondary ${isDisabled ? 'disabled' : ''}`}
-                         file={time.imageFile} onClick={callback}/>)
+            const [hasSMM2Selected, isDisabledNight,] = isDisabled == null ? [false, false,] : typeof isDisabled == 'boolean' ? [isDisabled, false,] : isDisabled
+            return <div key={`option container (${element.englishName})`} id={`${element.englishNameInHtml}-option-container`} className="btn-group-vertical" role="group">{
+                hasSMM2Selected
+                    ? renderElementWithDayAndNight(element, option, isDisabledNight,)
+                    : renderElementWithOnlyDay(element, option,)
             }</div>
-        </>
-    }
+        },)
+    }</div>
+}
 
+function renderElementWithDayAndNight(element: Themes, option: GlobalAppOption<GlobalThemeOption>, isDisabledNight: boolean,) {
+    const optionValue = option.get
+    const value = optionValue.dayValue || optionValue.nightValue
+
+    return <>
+        {renderThemeImage(element, value, isDisabledNight, () => option.set(optionValue.set(value)),)}
+        <div key={`option time image (${element.englishName})`} id={`${element.englishNameInHtml}-option-time-image`} className="btn-group btn-group-sm">
+            <Image id={`${element.englishNameInHtml}-day-option-image`}
+                   className={`btn btn${optionValue.dayValue ? '' : '-outline'}-secondary`}
+                   file={Times.DAY.imageFile} onClick={() => option.set(optionValue.reverseDayValue,)}/>
+            <Image id={`${element.englishNameInHtml}-night-option-image`}
+                   className={`btn btn${optionValue.nightValue ? '' : '-outline'}-secondary ${isDisabledNight ? 'disabled' : ''}`}
+                   file={Times.NIGHT.imageFile} onClick={() => option.set(optionValue.reverseNightValue,)}/>
+        </div>
+    </>
+}
+
+function renderElementWithOnlyDay(element: Themes, option: GlobalAppOption<GlobalThemeOption>,) {
+    const optionValue = option.get
+    const value = optionValue.dayValue
+
+    return renderThemeImage(element, value, false, () => option.set(optionValue.reverseDayValue,),)
+}
+
+function renderThemeImage(element: Themes, value: boolean, isDisabled: boolean, onClickCallback: () => void,) {
+    return <Image key={`option image (${element.englishName})`} id={`${element.englishNameInHtml}-option-image`}
+                  className={`btn btn${value ? '' : '-outline'}-secondary ${isDisabled ? 'disabled' : ''}`}
+                  file={element.smallImageFile} alt={`option - ${element.englishName}`}
+                  onClick={onClickCallback}/>
 }
--- src/app/options/global/group/TimeGroup.tsx
+++ src/app/options/global/group/TimeGroup.tsx
@@ -1,9 +1,29 @@
-import type {Times} from 'core/time/Times'
+import type {GroupProperties} from 'app/options/global/group/Group.types'
+import type {Times}           from 'core/time/Times'
 
-import {AbstractGroupWithSingleImage} from 'app/options/global/group/AbstractGroupWithSingleImage'
+import Image from 'app/tools/images/Image'
 
-/** @reactComponent */
-export default class TimeGroup
-    extends AbstractGroupWithSingleImage<Times, boolean> {
+/**
+ * @todo Change the time group to not be global
+ * @todo Change the time group to be like the game and game style group
+ * @reactComponent
+ */
+export default function TimeGroup({isHidden = false, elements,}: GroupProperties<Times, boolean>,) {
+    if (isHidden)
+        return null
+    return <div key={`option container (times)`} id="times-option-container" className="container-fluid">{
+        elements.map(([element, option, isDisabled, isHidden, onClickCallback = null,]) => {
+            if (isHidden)
+                return null
 
+            const isDisabled2 = isDisabled == null ? false : typeof isDisabled == 'boolean' ? isDisabled : isDisabled[0]
+            return <Image key={`option input (${element.englishName})`} id={`optionInput-${element.englishNameInHtml}`}
+                          className={`btn btn${option.get ? '' : '-outline'}-secondary  ${isDisabled2 ? 'disabled' : ''}`} data-bs-toggle="button"
+                          file={element.imageFile}
+                          onClick={() => {
+                              option.set(!option.get,)
+                              onClickCallback?.()
+                          }}/>
+        })
+    }</div>
 }
