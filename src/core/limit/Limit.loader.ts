0fd3e5e8d54a52eccf2fb7f42a55dbf1081a7789
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-04-09 23:37:27 -0400

    Change to the limit amount comment type to not include the "null" type

--- src/core/limit/Limit.loader.ts
+++ src/core/limit/Limit.loader.ts
@@ -89,7 +89,7 @@ interface Content
     readonly limit_SMM1And3DS_isUnknown: NullOrBoolean
     readonly limit_SMM2: NullOr<PossibleLimitAmount_SMM2_Amount>
     readonly limit_SMM2_isUnknown: NullOrBoolean
-    readonly limit_comment: PossibleLimitAmount_Comment
+    readonly limit_comment: NullOr<PossibleLimitAmount_Comment>
 
 }
 
--- src/core/limit/loader.types.ts
+++ src/core/limit/loader.types.ts
@@ -8,4 +8,4 @@ export type PossibleLimitAmount_SMM2_Amount = | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
                                               | 100 | 200 | 300 | 400 | 483 | 500 | 999
                                               | 1500 | 2000 | 4000
 
-export type PossibleLimitAmount_Comment = NullOr<| 'Crash online if met' | 'In a single frame' | `Per ${| 'player' | 'pair' | 'section'}`>
+export type PossibleLimitAmount_Comment = | 'Crash online if met' | 'In a single frame' | `Per ${| 'player' | 'pair' | 'section'}`
