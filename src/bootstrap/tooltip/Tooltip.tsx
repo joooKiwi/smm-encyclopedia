e51713c37822251d18c917882f3fb8d3b1f40eb2
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-07-21 21:19:18 -0400

    Change to the Tooltip implementation to have a ReactHtmlReference as a valid parameter instead of just the id

--- src/bootstrap/tooltip/Tooltip.tsx
+++ src/bootstrap/tooltip/Tooltip.tsx
@@ -1,22 +1,54 @@
-import {useEffect} from 'react'
+import {ReactElement, RefObject, useEffect} from 'react'
 
-import type {TooltipConfiguration}                from 'bootstrap/tooltip/Tooltip.types'
-import type {ReactPropertiesWithOptionalChildren} from 'util/react/ReactProperties'
+import type {TooltipEvents}                             from 'bootstrap/tooltip/TooltipEvents'
+import type {SimpleReactPropertiesWithOptionalChildren} from 'util/react/ReactProperties'
 
-import {BootstrapInstanceHandler} from 'bootstrap/BootstrapInstanceHandler'
-import {TooltipInstance}          from 'bootstrap/tooltip/TooltipInstance'
+import {BootstrapInstanceHandler}        from 'bootstrap/BootstrapInstanceHandler'
+import {TooltipInstance}                 from 'bootstrap/tooltip/TooltipInstance'
+import {default as TooltipFromBootstrap} from 'bootstrap/js/dist/tooltip'
+import {EMPTY_CALLBACK}                  from 'util/emptyVariables'
+
+
+interface TooltipProperties
+    extends SimpleReactPropertiesWithOptionalChildren<Nullable<ReactElement>> {
+
+    readonly option: Partial<TooltipFromBootstrap.Options>
+
+    readonly on?: Nullable<Partial<TooltipEvents<any>>>
+
+    readonly reference: Nullable<| RefObject<HTMLElement> | HTMLElement | string>
+
+}
 
 /**
  * Create a new {@link bootstrap.Tooltip Tooltip} instance
  *
  * @param properties the properties received (containing the content, the option, the triggers & the id)
  * @reactComponent
- * @see https://getbootstrap.com/docs/5.2/components/tooltips
+ * @see https://getbootstrap.com/docs/5.3/components/tooltips
  */
-export default function Tooltip<const T extends ReactElement = ReactElement, >({children, option, on: triggers, elementId,}: ReactPropertiesWithOptionalChildren<TooltipConfiguration, T>,) {
+export default function Tooltip({children, option, on: triggers, reference,}: TooltipProperties,) {
     useEffect(() => {
-        const instance = BootstrapInstanceHandler.get.add(elementId, new TooltipInstance(elementId, option, triggers,),)
+        const referenceFound = getReference(reference,)
+        if (referenceFound == null)
+            return EMPTY_CALLBACK
+        const instance = BootstrapInstanceHandler.get.add(referenceFound, new TooltipInstance(referenceFound, option, triggers,),)
         return () => BootstrapInstanceHandler.get.remove(instance,).destroy()
     },)
     return children ?? null
+}
+
+function getReference(reference: Nullable<| RefObject<HTMLElement> | HTMLElement | string>,) {
+    if (reference == null)
+        return null
+
+    if (typeof reference == 'string')
+        return reference
+    if (reference instanceof HTMLElement)
+        return reference
+
+    const referenceValue = reference.current
+    if (referenceValue == null)
+        return null
+    return referenceValue
 }
