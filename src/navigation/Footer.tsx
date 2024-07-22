88e207ea5e3d631711bd3d8f194f84d59641116c
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-07-21 23:02:00 -0400

    Last edited time update

--- src/navigation/Footer.tsx
+++ src/navigation/Footer.tsx
@@ -26,7 +26,7 @@ export default function Footer() {
                     <small>{contentTranslation('React application made by',)}: </small>JóôòKiwi
                 </span>
                 <small id="copyright_lastEdited" className="text-center text-light">
-                    {contentTranslation('Last update',)}: <em><SimpleDate day={4} month={2} year={2025}/></em>
+                    {contentTranslation('Last update',)}: <em><SimpleDate day={21} month={7} year={2025}/></em>
                 </small>
             </div>
         </div>
