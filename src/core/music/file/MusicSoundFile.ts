34ae4890aa9a0488d7475e8f75b1d8335db5663c
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-07-29 23:24:39 -0400

    Fix on the types to use the new music file alias type

--- src/core/music/file/MusicSoundFile.ts
+++ src/core/music/file/MusicSoundFile.ts
@@ -1,7 +1,6 @@
-import type {PossibleMusicName}           from 'core/music/Music'
-import type {NonRepeatableSoundFile}      from 'util/file/sound/NonRepeatableSoundFile'
-import type {RepeatableAtTheEndSoundFile} from 'util/file/sound/RepeatableAtTheEndSoundFile'
-import {RepeatableDuringThePlaySoundFile} from 'util/file/sound/RepeatableDuringThePlaySoundFile'
+import type {PossibleMusicFileName}  from 'core/music/IndividualMusics.types'
+import type {PossibleMusicName}      from 'core/music/Music'
+import type {NonRepeatableSoundFile} from 'util/file/sound/NonRepeatableSoundFile'
 
 /**
  * A {@link SoundFile} made to be used by a {@link Musics}
@@ -10,9 +9,6 @@ import {RepeatableDuringThePlaySoundFile} from 'util/file/sound/RepeatableDuring
  */
 export type MusicSoundFile<NAME extends PossibleMusicName = PossibleMusicName, > =
     | NonRepeatableMusicSoundFile<NAME>
-    | RepeatableAtTheEndMusicSoundFile<NAME>
-    | RepeatableDuringThePlayMusicSoundFile<NAME>
+    | NonNullable<PossibleMusicFileName>
 
 /**@deprecated The use of a more simplistic {@link IndividualMusics} is used and better*/export type NonRepeatableMusicSoundFile<NAME extends PossibleMusicName = PossibleMusicName, > = NonRepeatableSoundFile<'music/SMM2', NAME, 'wav'>
-/**@deprecated The use of a more simplistic {@link IndividualMusics} is used and better*/type RepeatableAtTheEndMusicSoundFile<NAME extends PossibleMusicName = PossibleMusicName, > = RepeatableAtTheEndSoundFile<'music/SMM2', NAME, 'wav'>
-/**@deprecated The use of a more simplistic {@link IndividualMusics} is used and better*/type RepeatableDuringThePlayMusicSoundFile<NAME extends PossibleMusicName = PossibleMusicName, > = RepeatableDuringThePlaySoundFile<'music/SMM2', NAME, 'wav'>
