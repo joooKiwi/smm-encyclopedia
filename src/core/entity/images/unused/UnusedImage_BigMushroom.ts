44270e9967712c2a1657312917311180adcabb5b
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-08-04 21:46:20 -0400

    Little adaptation of the documentation

--- src/core/entity/images/unused/UnusedImage_BigMushroom.ts
+++ src/core/entity/images/unused/UnusedImage_BigMushroom.ts
@@ -5,8 +5,6 @@ import type {UnusedImage}                    from 'core/entity/images/unused/Unu
  * An unused {@link Entities.BIG_MUSHROOM_CLASSIC} or {@link Entities.BIG_MUSHROOM_MODERN}
  * image for some specific entities.
  *
- * It can only be from a {@link Entities.KOOPA_CLOWN_CAR}, {@link Entities.GOOMBA} or {@link Entities.STRETCH}.
- *
  * It is also only applicable in the {@link GameStyles.SUPER_MARIO_BROS "Super Mario Bros." game style}
  * as well as only in the {@link Games.SUPER_MARIO_MAKER_1 "Super Mario Maker" game}.
  */
