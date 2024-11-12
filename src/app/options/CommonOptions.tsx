c240f486b454df871336a6e8c2b344b01bef54bd
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-11-12 12:57:29 -0500

    Fix on the sound header

--- src/app/options/CommonOptions.tsx
+++ src/app/options/CommonOptions.tsx
@@ -68,7 +68,7 @@ export class CommonOptions {
     }
 
     public get soundHeader(): SimpleReactHeader {
-        return this.#soundHeader ??= {key: 'sound', element: contentTranslation('Sound',),}
+        return this.#soundHeader ??= {key: 'sound', element: contentTranslation('Sound.singular',),}
     }
 
 
