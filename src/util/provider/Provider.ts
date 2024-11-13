3ba44d651961db5cbaa0b6d853d4b01e41b40eb7
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-11-13 12:13:05 -0500

    Removal of the no longer used provider classes

--- src/util/provider/Provider.ts
+++
@@ -1,9 +0,0 @@
-/**
- * The base provider giving a way to retrieve the containers
- * by any possible values (array included)
- */
-export interface Provider<K extends unknown, V> {
-
-    get everyContainers(): Map<K, V>
-
-}
