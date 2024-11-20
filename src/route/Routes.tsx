ef1867494d36328347f132121c627e4cd6c5e09d
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-11-20 12:24:19 -0500

    Change to the route processus of loading the routes from eager to lazy

--- src/route/Routes.tsx
+++ src/route/Routes.tsx
@@ -1,7 +1,9 @@
-import type {RouteObject} from 'react-router/dist'
-import {RouterProvider}   from 'react-router/dist'
-import {createHashRouter} from 'react-router-dom/dist'
-import {Suspense}         from 'react'
+import type {MutableArray, NullOrString} from '@joookiwi/type'
+import type {RouteObject}                from 'react-router/dist'
+import {findFirstOrNullByArray}          from '@joookiwi/collection'
+import {RouterProvider}                  from 'react-router/dist'
+import {createHashRouter}                from 'react-router-dom/dist'
+import {Suspense}                        from 'react'
 
 import PageLayout                    from 'app/_PageLayout'
 import LoadingApp                    from 'app/LoadingApp'
@@ -11,7 +13,6 @@ import {Times}                       from 'core/time/Times'
 import {ProjectLanguages}            from 'lang/ProjectLanguages'
 import {EveryRoutes}                 from 'route/EveryRoutes'
 import {redirectTo, redirectToByUrl} from 'route/redirectionMethods'
-import {StraightFallbackRouteObject} from 'route/StraightFallbackRouteObject'
 import {StraightRouteObject}         from 'route/StraightRouteObject'
 import {GameCollection}              from 'util/collection/GameCollection'
 import {GameStyleCollection}         from 'util/collection/GameStyleCollection'
@@ -28,18 +29,19 @@ const homeRoute = EveryRoutes.HOME
 const languages = LanguageCompanion.values
 const everyRouteInstance = RouteCompanion.values
 const everyRoute = RouteCompanion.everyRoute
+
 // const everyGames = Games.Possibilities.get.everyFields
 // const everyGamesAsUrl = everyGames.map(it => GameCompanion.getGroupUrlValue(it,),)
 // const everyGameStyles = GameStyles.Possibilities.get.everyFields
 // const everyGameStylesAsUrl = everyGameStyles.map(it => GameStyleCompanion.getGroupUrlValue(it,),)
 // const everyViewDisplay = ViewDisplayCompanion.values
 /**
- * All the routes encapculated in a {@link Router HashRouter}.
+ * All the routes encapsulated in a {@link Router HashRouter}.
  * The reason it is not a BrowserRouter is because of GitHub hosting service.
  *
  * At the root it has a "/"
  * and then the {@link ProjectLanguages project language} {@link ProjectLanguages.acronym acronym}
- * in parrallel to the {@link EveryRoutes route instances} {@link SimpleRoute.path basic path}.
+ * in parallel to the {@link EveryRoutes route instances} {@link SimpleRoute.path basic path}.
  *
  * If the path is correct for the {@link ProjectLanguages language},
  * then the {@link EveryRoutes route instances}'s values are processed with the mutation
@@ -50,6 +52,7 @@ const everyRoute = RouteCompanion.everyRoute
 const router = createHashRouter([{
     caseSensitive: false,
     path: '/',
+    id: 'root',
     element: <PageLayout/>,
     children: [
         new StraightRouteObject('/', () => redirectTo(homeRoute,),),
@@ -65,34 +68,7 @@ const router = createHashRouter([{
             const pathFromLanguage = `/${language.projectAcronym}` as const
             return {
                 path: pathFromLanguage,
-                children: [
-                    new StraightRouteObject(pathFromLanguage, () => redirectTo(homeRoute, language,),),
-                    //region -------------------- Path from route path --------------------
-
-                    ...everyRoute.map<RouteObject>(route => {
-                        const viewDisplay = route.viewDisplay!
-                        const games = route.games
-                        const gameStyles = route.gameStyles
-                        const times = route.times
-
-                        return {
-                            path: `${pathFromLanguage}${route.path}`,
-                            element: <Suspense fallback={<LoadingApp/>}>{route.renderCallback(viewDisplay, GameCollection.of(games,), TimeCollection.of(times,), GameStyleCollection.of(gameStyles,),)}</Suspense>,
-                            loader() {
-                                if (games != null)
-                                    GameCompanion.current = games
-                                if (gameStyles != null)
-                                    GameStyleCompanion.current = gameStyles
-                                if (times != null)
-                                    TimeCompanion.current = times
-                                return null
-                            },
-                        }
-                    },),
-
-                    //endregion -------------------- Path from route path --------------------
-                    new StraightFallbackRouteObject(pathFromLanguage, loaderArguments => redirectToByUrl(loaderArguments, language,),),
-                ],
+                id: `language-${language.projectAcronym}`,
                 loader() {
                     LanguageCompanion.current = language
                     return null
@@ -101,13 +77,55 @@ const router = createHashRouter([{
         },),
 
         //endregion -------------------- Path from language --------------------
-        new StraightFallbackRouteObject('/', loaderArguments => redirectToByUrl(loaderArguments,),),
+        new StraightRouteObject('/*', loaderArguments => redirectToByUrl(loaderArguments,),),
     ],
-} satisfies RouteObject,], {basename: '/',},)
+} satisfies RouteObject,], {
+    basename: '/',
+    patchRoutesOnNavigation: it => resolveLazyRoute(it.path, it.patch,),
+},)
 
-// console.debug(router.routes[0].children)
+/**
+ * Add the {@link SimpleRoute route found} if it exists by the {@link path} received
+ *
+ * @param path   The path to find a route
+ * @param action The action to add a {@link RouteObject} for the {@link SimpleRoute route found}
+ */
+function resolveLazyRoute(path: string, action: (routeId: NullOrString, children: MutableArray<RouteObject>,) => void,): void {
+    const route = findFirstOrNullByArray(everyRoute, it => path.endsWith(it.path,),)
+    if (route == null)
+        return
+
+    const language = LanguageCompanion.getValueInUrl(path,)
+    if (language == null)
+        return // We are not expecting to have no language by the url
+    const viewDisplay = route.viewDisplay!
+    const games = route.games
+    const gameStyles = route.gameStyles
+    const times = route.times
+
+    action(`language-${language.projectAcronym}`, [{
+        path: `/${language.projectAcronym}${route.path}`,
+        element: <Suspense fallback={<LoadingApp/>}>{route.renderCallback(viewDisplay, GameCollection.of(games,), GameStyleCollection.of(gameStyles,), TimeCollection.of(times,),)}</Suspense>,
+        loader() {
+            if (games != null)
+                GameCompanion.current = games
+            if (gameStyles != null)
+                GameStyleCompanion.current = gameStyles
+            if (times != null)
+                TimeCompanion.current = times
+            return null
+        },
+    },],)
+}
+
+// console.debug(router.routes[0]!.children)
 
 /** @reactComponent */
 export default function Routes() {
     return <RouterProvider router={router} fallbackElement={<LoadingApp/>}/>//TODO change the loading app to have a different visual than afterward
 }
+
+// @ts-ignore: TODO remove once the application is more complete
+(window.test ??= {}).router = router
+// @ts-ignore: TODO remove once the application is more complete
+window.test.routes = router.routes[0]!.children
