b8a4feb566d00287bf9b3b65545482fb1f4e6e2a
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-11-22 15:23:47 -0500

    Simple reformating

--- src/core/editorVoice/EditorVoiceSound.component.tsx
+++ src/core/editorVoice/EditorVoiceSound.component.tsx
@@ -24,20 +24,22 @@ export default function EditorVoiceSoundComponent({editorVoiceSound, name,}: Edi
     if (editorVoiceSound == null)
         return null
 
-    const {regularSound: regularFile, europeanSound: europeanFile,} = editorVoiceSound
-    return regularFile == null
-        ? null
-        : europeanFile == null
-            ? <div key={`Editor voice sound container (single - ${name})`} className="single-editorVoiceSound-container">
-                <SimpleSoundComponent file={regularFile} title={name}/>
-            </div>
-            : <div key={`Editor voice sound container (double - ${name})`} className="double-editorVoiceSound-container container">
-                <div key={`Editor voice sound container (single #1 - ${name}`} className="single-editorVoiceSound-container">
-                    <SimpleSoundComponent file={regularFile} title={name}/>
-                </div>
-                <div key={`Editor voice sound container (single #2 - ${name})`} className="single-editorVoiceSound-container">
-                    <SimpleSoundComponent file={europeanFile} title={name}/>
-                </div>
-            </div>
+    const regularFile = editorVoiceSound.regularSound
+    if (regularFile == null)
+        return null
+
+    const europeanFile = editorVoiceSound.europeanSound
+    if (europeanFile == null)
+        return <div key={`Editor voice sound container (single - ${name})`} className="single-editorVoiceSound-container">
+            <SimpleSoundComponent file={regularFile} title={name}/>
+        </div>
+    return <div key={`Editor voice sound container (double - ${name})`} className="double-editorVoiceSound-container container">
+        <div key={`Editor voice sound container (single #1 - ${name}`} className="single-editorVoiceSound-container">
+            <SimpleSoundComponent file={regularFile} title={name}/>
+        </div>
+        <div key={`Editor voice sound container (single #2 - ${name})`} className="single-editorVoiceSound-container">
+            <SimpleSoundComponent file={europeanFile} title={name}/>
+        </div>
+    </div>
 
 }
