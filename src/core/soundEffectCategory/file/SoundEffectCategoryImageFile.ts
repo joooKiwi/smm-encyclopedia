f2832a98d1123e6f86da864fa6952c2580412940
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-11-22 17:25:07 -0500

    Fix on the category icons

--- src/core/soundEffectCategory/file/SoundEffectCategoryImageFile.ts
+++ src/core/soundEffectCategory/file/SoundEffectCategoryImageFile.ts
@@ -2,4 +2,4 @@ import type {ImageFile}           from 'util/file/image/ImageFile'
 
 export type PossibleImageNumber = | 4 | 5 | 6 | 7 | 8
 /** An {@link ImageFile} made to be related to a {@link SoundEffectCategories} */
-export type SoundEffectCategoryImageFile<NUMBER extends PossibleImageNumber = PossibleImageNumber, > = ImageFile<'category', `Category_Icon_0${NUMBER}^s`, 'tiff'>
+export type SoundEffectCategoryImageFile<NUMBER extends PossibleImageNumber = PossibleImageNumber, > = ImageFile<'category', `CategoryIcon_0${NUMBER}^s`, 'tiff'>
--- src/core/soundEffectCategory/file/fileCreator.ts
+++ src/core/soundEffectCategory/file/fileCreator.ts
@@ -10,5 +10,5 @@ import {SimpleImageFile} from 'util/file/image/SimpleImageFile'
  * @param englishName The fallback name
  */
 export function soundEffectCategoryImage<const NUMBER extends PossibleImageNumber, >(number: NUMBER, englishName: PossibleEnglishName,): SoundEffectCategoryImageFile<NUMBER> {
-    return new SimpleImageFile('category', `Category_Icon_0${number}^s`, 'tiff', englishName,)
+    return new SimpleImageFile('category', `CategoryIcon_0${number}^s`, 'tiff', englishName,)
 }
