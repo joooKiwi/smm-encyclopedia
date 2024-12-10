9ca4870ab86fa26417347a31829a1e553d0c0586
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-12-10 16:44:41 -0500

    Little change to the instrument component key

--- src/core/instrument/component/InstrumentSound.tsx
+++ src/core/instrument/component/InstrumentSound.tsx
@@ -18,8 +18,8 @@ export default function InstrumentSound({value,}: InstrumentSoundProperties,) {
     const name = value.name
     const englishName = value.englishName
 
-        <SimpleSoundComponent key={`instrument sounds #${i} (${englishName})`} file={it} title={`${name} (instrument #${i})`}/>,)
     const elements = new ArrayAsCollection(value.sounds,).map((it, i,) =>
+        <SimpleSoundComponent key={`instrument sounds #${i} (${englishName})`} file={it} title={`${name} (instrument sound #${i})`}/>,)
     if (elements.isEmpty)
         return <div className="instrumentSound-container individual-instrumentSound-container">{elements}</div>
     return <div className="instrumentSound-container grouped-instrumentSound-container">{elements}</div>
