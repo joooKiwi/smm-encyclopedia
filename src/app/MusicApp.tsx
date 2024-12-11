d76b41af419b30596a0f15d6fd3d42370f247eb0
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-12-10 21:32:09 -0500

    Declaration of global arrays

--- src/app/MusicApp.tsx
+++ src/app/MusicApp.tsx
@@ -1043,33 +1043,6 @@ function getYoshiImage(gameStyle: GameStyles,) {
 //                     | 'link' | 'smb2'
 //                     | 'ground' | 'underground' | 'underwater' | 'desert' | 'snow' | 'sky' | 'forest' | 'ghostHouse' | 'airship' | 'castle'
 
-type ArrayOf1<T, > = | readonly [T,]
-type ArrayOf2<T, > = | readonly [T, T,]
-type ArrayOf3<T, > = | readonly [T, T, T,]
-type ArrayOf4<T, > = | readonly [T, T, T, T,]
-type ArrayOf5<T, > = | readonly [T, T, T, T, T,]
-type ArrayOf6<T, > = | readonly [T, T, T, T, T, T,]
-type ArrayOf7<T, > = | readonly [T, T, T, T, T, T, T,]
-type ArrayOf8<T, > = | readonly [T, T, T, T, T, T, T, T,]
-
-// type ArrayOf1To2<T, > = | ArrayOf1<T> | ArrayOf2<T>
-// type ArrayOf1To3<T, > = | ArrayOf1To2<T> | ArrayOf3<T>
-// type ArrayOf1To4<T, > = | ArrayOf1To3<T> | ArrayOf4<T>
-// type ArrayOf1To5<T, > = | ArrayOf1To4<T> | ArrayOf5<T>
-// type ArrayOf1To6<T, > = | ArrayOf1To5<T> | ArrayOf6<T>
-// type ArrayOf1To7<T, > = | ArrayOf1To6<T> | ArrayOf7<T>
-type ArrayOf1To8<T, > = | ArrayOf1<T> | ArrayOf2<T> | ArrayOf3<T> | ArrayOf4<T> | ArrayOf5<T> | ArrayOf6<T> | ArrayOf7<T> | ArrayOf8<T>
-
-// type ArrayOf1Or7<T, > = | ArrayOf1<T> | ArrayOf7<T>
-// type ArrayOf2Or3<T, > = | ArrayOf2<T> | ArrayOf3<T>
-// type ArrayOf2Or4<T, > = | ArrayOf2<T> | ArrayOf4<T>
-// type ArrayOf2Or5<T, > = | ArrayOf2<T> | ArrayOf5<T>
-// type ArrayOf2Or6<T, > = | ArrayOf2<T> | ArrayOf6<T>
-// type ArrayOf2Or7<T, > = | ArrayOf2<T> | ArrayOf7<T>
-// type ArrayOf1Or4<T, > = | ArrayOf1<T> | ArrayOf4<T>
-// type ArrayOf1Or4Or5<T, > = | ArrayOf1<T> | ArrayOf4<T> | ArrayOf5<T>
-// type ArrayOf1Or2Or7<T, > = | ArrayOf1<T> | ArrayOf2<T> | ArrayOf7<T>
-
 //endregion -------------------- Musics types --------------------
 //region -------------------- Musics --------------------
 
