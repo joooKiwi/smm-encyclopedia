d212f5acc20cba309c38c3795d4d83e8d325da0a
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-12-21 22:07:46 -0500

    Formatting over the global React element types to be readable in a table-like form

--- src/util/types/global/react.ts
+++ src/util/types/global/react.ts
@@ -8,30 +8,31 @@ declare global {
     type ReactElement = Nullable<React.ReactElement>
     type NonNullReactElement = React.ReactElement
 
-    type ArrayReactElement = Array<ReactElement>
-    type ReactElementOrArray = | ReactElement | Array<ReactElement>
+    type ArrayReactElement =        Array<ReactElement>
+    type ArrayNonNullReactElement = Array<NonNullReactElement>
+    type ReactElementOrArray =        | ReactElement        | Array<ReactElement>
     type NonNullReactElementOrArray = | NonNullReactElement | Array<NonNullReactElement>
 
-    type ReactElementOrString = | ReactElement | string
-    type NonNullReactElementOrString = | NonNullReactElement | string
-    type ReactElementOrNumeric = | ReactElement | Numeric
-    type NonNullReactElementOrNumeric = | NonNullReactElement | Numeric
-    type ReactElementOrNumber = | ReactElement | number
-    type NonNullReactElementOrNumber = | NonNullReactElement | number
-    type ReactElementOrStringOrNumeric = | ReactElement | string | Numeric
+    type ReactElementOrString =                 | ReactElement        | string
+    type NonNullReactElementOrString =          | NonNullReactElement | string
+    type ReactElementOrNumeric =                | ReactElement        | Numeric
+    type NonNullReactElementOrNumeric =         | NonNullReactElement | Numeric
+    type ReactElementOrNumber =                 | ReactElement        | number
+    type NonNullReactElementOrNumber =          | NonNullReactElement | number
+    type ReactElementOrStringOrNumeric =        | ReactElement        | string | Numeric
     type NonNullReactElementOrStringOrNumeric = | NonNullReactElement | string | Numeric
-    type ReactElementOrStringOrNumber = | ReactElement | string | number
-    type NonNullReactElementOrStringOrNumber = | NonNullReactElement | string | number
+    type ReactElementOrStringOrNumber =         | ReactElement        | string | number
+    type NonNullReactElementOrStringOrNumber =  | NonNullReactElement | string | number
 
-    type ReactElementOrStringOrArray = | ReactElement | string | Array<| ReactElement | string>
-    type NonNullReactElementOrStringOrArray = | NonNullReactElement | string | Array<| NonNullReactElement | string>
-    type ReactElementOrNumericOrArray = | ReactElement | Numeric | Array<| ReactElement | Numeric>
-    type NonNullReactElementOrNumericOrArray = | NonNullReactElement | Numeric | Array<| NonNullReactElement | Numeric>
-    type ReactElementOrNumberOrArray = | ReactElement | number | Array<| ReactElement | number>
-    type NonNullReactElementOrNumberOrArray = | NonNullReactElement | number | Array<| NonNullReactElement | number>
-    type ReactElementOrStringOrNumericOrArray = | ReactElement | string | Numeric | Array<| ReactElement | string | Numeric>
+    type ReactElementOrStringOrArray =                 | ReactElement        | string           | Array<| ReactElement        | string>
+    type NonNullReactElementOrStringOrArray =          | NonNullReactElement | string           | Array<| NonNullReactElement | string>
+    type ReactElementOrNumericOrArray =                | ReactElement        | Numeric          | Array<| ReactElement        | Numeric>
+    type NonNullReactElementOrNumericOrArray =         | NonNullReactElement | Numeric          | Array<| NonNullReactElement | Numeric>
+    type ReactElementOrNumberOrArray =                 | ReactElement        | number           | Array<| ReactElement        | number>
+    type NonNullReactElementOrNumberOrArray =          | NonNullReactElement | number           | Array<| NonNullReactElement | number>
+    type ReactElementOrStringOrNumericOrArray =        | ReactElement        | string | Numeric | Array<| ReactElement        | string | Numeric>
     type NonNullReactElementOrStringOrNumericOrArray = | NonNullReactElement | string | Numeric | Array<| NonNullReactElement | string | Numeric>
-    type ReactElementOrStringOrNumberOrArray = | ReactElement | string | number | Array<| ReactElement | string | number>
-    type NonNullReactElementOrStringOrNumberOrArray = | NonNullReactElement | string | number | Array<| NonNullReactElement | string | number>
+    type ReactElementOrStringOrNumberOrArray =         | ReactElement        | string | number  | Array<| ReactElement        | string | number>
+    type NonNullReactElementOrStringOrNumberOrArray =  | NonNullReactElement | string | number  | Array<| NonNullReactElement | string | number>
 
 }
