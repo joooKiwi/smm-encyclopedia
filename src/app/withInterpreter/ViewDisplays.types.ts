0c1312201dd67ed077a1f012c0f53a709fd15f27
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2024-12-26 22:42:14 -0500

    Removal of unused types

--- src/app/withInterpreter/ViewDisplays.types.ts
+++ src/app/withInterpreter/ViewDisplays.types.ts
@@ -1,10 +1,6 @@
-import type {ViewDisplays}      from 'app/withInterpreter/ViewDisplays'
-import type {PossibleRouteName} from 'route/EveryRoutes.types'
-
 enum Enum {
 
     TABLE,
-    // NAME_LIST,
     SIMPLE_LIST,
     CARD_LIST,
 
@@ -13,15 +9,5 @@ enum Enum {
 export type Ordinals = typeof Enum[Names]
 export type Names = keyof typeof Enum
 
-//region -------------------- Type --------------------
-
 export type Type = | 'table' | `${| 'simple' | 'card'}-list`
-// export type Type = | 'table' | `${| 'name' | 'simple' | 'card'}-list`
-
 export type PossibleUrlValue = | 'table' | 'list' | 'card'
-// export type PossibleUrlValue = | 'table' | 'name' | 'list' | 'card'
-
-//endregion -------------------- Type --------------------
-
-/** A tuple of 2 containing the display and route */
-export type ViewAndRouteName = readonly [ViewDisplays, PossibleRouteName,]
