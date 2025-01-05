f50cfc17501405477eb3d9f38c83e35eb957c5ff
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-01-04 22:09:38 -0500

    Fix on the "language to parameter" button icon

--- src/navigation/button/LanguageToParameter.button.tsx
+++ src/navigation/button/LanguageToParameter.button.tsx
@@ -1,6 +1,5 @@
 import {useRef} from 'react'
 
-import TranslateIcon                                   from 'bootstrap/icon/TranslateIcon'
 import ModalButton                                     from 'bootstrap/modal/element/ModalButton'
 import Tooltip                                         from 'bootstrap/tooltip/Tooltip'
 import {contentTranslation}                            from 'lang/components/translationMethods'
@@ -10,8 +9,6 @@ export default function LanguageToParameterButton() {
     const htmlElement = useRef<HTMLButtonElement>(null,)
 
     return <Tooltip option={{title: contentTranslation('Option',), placement: 'right',}} reference={htmlElement}>
-        <ModalButton ref={htmlElement} id="languageChanger-to-parameter-button" className="btn btn-outline-primary rounded-pill" elementToShow={PARAMETER_MODAL_ID} elementToHide={LANGUAGE_CHANGER_MODAL_ID}>
-            <TranslateIcon/>
-        </ModalButton>
+        <ModalButton ref={htmlElement} id="languageChanger-to-parameter-button" className="btn btn-outline-primary bi bi-gear-fill rounded-pill" elementToShow={PARAMETER_MODAL_ID} elementToHide={LANGUAGE_CHANGER_MODAL_ID}/>
     </Tooltip>
 }
