a0aca141d92b4d67f99a60719885d08771cf29f7
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2025-01-09 15:26:50 -0500

    Addition of the missing file

---
+++ src/display/ViewDisplays.types.ts
@@ -0,0 +1,14 @@
+enum Enum {
+
+    TABLE,
+    CARD,
+    // NAME,
+    LIST,
+
+}
+
+export type Ordinals = typeof Enum[Names]
+export type Names = keyof typeof Enum
+
+export type Type = | 'none' | 'table' | 'card' | 'list'
+// export type Type = | 'none' | 'table' | 'card' | 'name' | 'list'
