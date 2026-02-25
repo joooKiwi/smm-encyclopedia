a1a47604fec60e00732bf3dda8583ea3f139b52a
JoooKiwi <66183590+joooKiwi@users.noreply.github.com>
2026-02-25 12:55:18 -0500

    Addition of a debug message (only in development) for the non-present music audio

--- src/core/track/component/Track.tsx
+++ src/core/track/component/Track.tsx
@@ -1,7 +1,8 @@
 import type {Tracks}          from 'core/track/Tracks'
 import type {ReactProperties} from 'util/react/ReactProperties'
 
-import StandaloneSound from 'util/file/sound/component/StandaloneSound'
+import {isInDevelopment} from 'variables'
+import StandaloneSound   from 'util/file/sound/component/StandaloneSound'
 
 interface TrackProperties
     extends ReactProperties {
@@ -15,11 +16,13 @@ export default function Track(properties: TrackProperties,) {
     const value = properties.value
     const file = value.file
     if (file == null)
-        return <NonPresentMusicAudio/> //FIXME: Remove once the file are all present
+        return <NonPresentMusicAudio value={value}/> //FIXME: Remove once the file are all present
     return <StandaloneSound file={file} title={value.titleName}/>
 }
 
 /** @reactComponent */
-function NonPresentMusicAudio() {
+function NonPresentMusicAudio({value,}: {readonly value: Tracks,},) {
+    if (isInDevelopment)
+        console.debug(`No music audio was present for “${value.titleName}”.`, value,)
     return <div className="nonPresent-music-audio bg-danger bg-opacity-25 rounded" style={{minWidth: '5rem', height: '2rem',}}/>
 }
